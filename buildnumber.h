ee75be6e1128d6b19b6efcb8eb4959bec5ca9ef6
Paolo Ventafridda <venta@bware.it>
2011-01-09 02:58:10 +0100

    Fixed language issue. This is version 199H
    Signed-off-by: Paolo Ventafridda <venta@bware.it>

--- buildnumber.h
+++ buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 6836
-#define BUILDNUMBER_STR "6836" 
+#define BUILDNUMBER 6842
+#define BUILDNUMBER_STR "6842" 
 
 #endif /* BUILD_NUMBER_H_ */
