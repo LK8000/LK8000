f1c0d63e84e28dfe97ab4dd59706ab7e21fd27db
Paolo Ventafridda <venta@lk8000.it>
2011-01-21 01:02:42 +0100

    ..

--- buildnumber.h
+++ buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 6948
-#define BUILDNUMBER_STR "6948" 
+#define BUILDNUMBER 6949
+#define BUILDNUMBER_STR "6949" 
 
 #endif /* BUILD_NUMBER_H_ */
