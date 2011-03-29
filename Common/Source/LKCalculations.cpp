2006d5127ac1249347f0b743525c27696262fcd3
Paolo Ventafridda <venta@lk8000.it>
2011-03-29 17:12:11 +0200

    First beta of DetectFreeFlying

--- Common/Source/LKCalculations.cpp
+++ Common/Source/LKCalculations.cpp
@@ -1929,64 +1929,102 @@ void MapWindow::LKCalculateWaypointReachable(short multicalc_slot, short numslot
 
 
 /* 
+ * Detect start of free flight (FF) for both towing and winching.
+ * Start of FF may be marked 3 seconds later, max. Small error.
+ * This combined approach is much more accurate than seeyou!
+ * WARNING> the internal IGC interpolator is buggy and will cause problems
+ * while replaying IGC file logged at more than 2s interval.
+ * During testing, all 1s interval logs (which are equivalent to real flight)
+ * worked fine, while we got false positives using >2s interval logs.
+ * We dont care much, since we need real flight FF detection, and LK
+ * is logging always at 1s. So this might actually happen only replaying 
+ * an external log file on LK. 
+ *
  * Called by Calculations thread, each second.
+ *
+ * No message will be given to the user concerning start of free flight.
+ * We should not disturb pilots during critical phases of flight.
  */
-#define FF_ALTITUDELOSS	25		// meters
-#define FF_MAXTOWTIME	900		// 15 minutes
+#define FF_TOWING_ALTLOSS	25		// meters loss for towing
+#define FF_WINCHIN_ALTLOSS	10		// meters loss for winching, careful about GPS H errors.
+#define FF_MAXTOWTIME	1200			// 20 minutes
+
+#define DEBUG_DFF	1		// REMOVE BEFORE FLIGHT!
 
 bool DetectFreeFlying(DERIVED_INFO *Calculated) {
 
-  static bool ffDetected=false;
-  static int lastMaxAltitude=-1000;
+  static bool   ffDetected=false;
+  static int    lastMaxAltitude=-1000;
   static double gndAltitude=0;
-  static bool doinit=true;
+  static bool   doinit=true;
   static double vario[8];
+  static bool   winchdetected=false;
+  static short  wlaunch=0;
+  static int    altLoss=0;
 
   if (doinit) {
-	for (int i=0; i<8; i++) vario[i]=0;
-	doinit=false;
+    for (int i=0; i<8; i++) vario[i]=0;
+    doinit=false;
   }
 
   // reset on ground
   if (Calculated->Flying == FALSE) {
-	Calculated->FreeFlying=false;
-	ffDetected=false;
-	lastMaxAltitude=-1000;
-	gndAltitude=GPS_INFO.Altitude;
-	return false;
+    Calculated->FreeFlying=false;
+    ffDetected=false;
+    lastMaxAltitude=-1000;
+    gndAltitude=GPS_INFO.Altitude;
+    winchdetected=false;
+    wlaunch=0;
+    altLoss=FF_TOWING_ALTLOSS;
+    return false;
   }
 
   if (ISPARAGLIDER) {
-	Calculated->FreeFlying=true;
-	ffDetected=true;
-	return true; // no message for start of FF to paragliders
+    Calculated->FreeFlying=true;
+    ffDetected=true;
+    return true; 
   }
 
   // If flying, and start was already detected, assume valid freeflight.
-  // Put here in the future the EN check for motorplanes
+  // Put here in the future the Engine Noise Detection for motorplanes
   if (ffDetected)
     return true;
 
   // Here we are flying, and the start of free flight must still be detected
 
-  // We shall not consider anything below 400m gain, and before 2.5minutes have passed since takeoff
-  if ( (GPS_INFO.Altitude - gndAltitude)<400) return false;
-  if ( (GPS_INFO.Time - Calculated->TakeOffTime) < 150) return false;
+  // In any case, after this time, force a start. This is to avoid that for any reason, no FF is ever detected.
+  if ((int)GPS_INFO.Time > ( Calculated->TakeOffTime + FF_MAXTOWTIME)) {
+    #if DEBUG_DFF
+    DoStatusMessage(_T("DFF:  TIMEOUT"));
+    #endif
+    goto backtrue;	// unconditionally force start FF
+  }
+
  
-  // A loss of altitude will trigger FF
+  // A loss of altitude will trigger FF 
   lastMaxAltitude = std::max(lastMaxAltitude, (int)GPS_INFO.Altitude);
-  if ((int)GPS_INFO.Altitude <= ( lastMaxAltitude - FF_ALTITUDELOSS))
-    goto backtrue;
+  if ((int)GPS_INFO.Altitude <= ( lastMaxAltitude - altLoss)) {
+    #if DEBUG_DFF
+    if ( (winchdetected) || ((GPS_INFO.Altitude - gndAltitude)>=400) 
+      && ((GPS_INFO.Time - Calculated->TakeOffTime) >= 150))
+      DoStatusMessage(_T("DFF:  ALTITUDE LOSS"));
+    #endif
+    goto lastcheck;
+  }
 
   // If circling we assume that we are in free flight already, using the start of circling time and position.
   // But we must be sure that we are not circling.. while towed. A 12 deg/sec turn rate will make it quite sure.
-  if (Calculated->Circling && ( fabs(Calculated->TurnRate) >=12 ) ) {
+  // Of course nobody can circle while winchlaunched, so in this case FF is immediately detected.
+  if (Calculated->Circling && ( winchdetected || ( fabs(Calculated->TurnRate) >=12 ) ) ) {
 
-	CContestMgr::Instance().Add(new CPointGPS(static_cast<unsigned>(Calculated->ClimbStartTime),
-		Calculated->ClimbStartLat, Calculated->ClimbStartLong,
-		static_cast<unsigned>(Calculated->ClimbStartAlt)));
+    CContestMgr::Instance().Add(new CPointGPS(static_cast<unsigned>(Calculated->ClimbStartTime),
+	Calculated->ClimbStartLat, Calculated->ClimbStartLong,
+	static_cast<unsigned>(Calculated->ClimbStartAlt)));
 
-	goto backtrue;
+    #if DEBUG_DFF
+    DoStatusMessage(_T("DFF:  THERMALLING"));
+    #endif
+    goto backtrue;
   }
 
   vario[7]=vario[6];
@@ -1999,25 +2037,70 @@ bool DetectFreeFlying(DERIVED_INFO *Calculated) {
 
   double newavervario;
   double oldavervario;
+
   newavervario=(vario[1]+vario[2]+vario[3])/3;
-  if (newavervario>0.6) goto lastcheck;
-  if (newavervario<=0) goto backtrue;
+
+  if (newavervario>=10) {
+    wlaunch++;
+  } else {
+    wlaunch=0;
+  }
+  // After (6+2) 8 seconds of consecutive fast climbing (AFTER TAKEOFF DETECTED!), winch launch is detected
+  if (wlaunch==6) {
+    #if DEBUG_DFF
+    DoStatusMessage(_T("DFF:  WINCH LAUNCH"));
+    #endif
+    altLoss=FF_WINCHIN_ALTLOSS;
+    winchdetected=true;
+  }
+    
+  if (newavervario>0.3)
+    return false;
+
+  if (newavervario<=0) {
+    #if DEBUG_DFF
+    if ( (winchdetected) || ((GPS_INFO.Altitude - gndAltitude)>=400) 
+      && ((GPS_INFO.Time - Calculated->TakeOffTime) >= 150))
+      DoStatusMessage(_T("DFF:  VARIO SINK"));
+    #endif
+    goto lastcheck;
+  }
+
   oldavervario=(vario[4]+vario[5]+vario[6]+vario[7])/4;
-  if ( fabs(oldavervario-newavervario)>2.2 ) goto backtrue;
+  // current avervario between 0.0 and 0.3: uncertain situation,  we check what we had in the previous time
+  // Windy situations during towing may lead to false positives if delta is too low. This is the most
+  // difficult part which could lead to false positives or negatives.
+  if ( oldavervario >=4.5 ) {
+    #if DEBUG_DFF
+    StartupStore(_T("..... oldavervario=%.1f newavervario=%.1f current=%.1f\n"),oldavervario,newavervario,Calculated->Vario);
+    if ( (winchdetected) || ((GPS_INFO.Altitude - gndAltitude)>=400) 
+      && ((GPS_INFO.Time - Calculated->TakeOffTime) >= 150))
+      DoStatusMessage(_T("DFF:  DELTA VARIO"));
+    #endif
+    goto lastcheck;
+  }
 
-  lastcheck:
-  // In any case, after this time, force a start
-  if ((int)GPS_INFO.Time > ( Calculated->TakeOffTime + FF_MAXTOWTIME))
-    goto backtrue;
-
-  // else we are not in free flight
+  // No free flight detected
   return false;
 
+  lastcheck: 
+
+  // Unless under a winch launch, we shall not consider anything below 400m gain, 
+  // and before 2.5minutes have passed since takeoff
+  // Anybody releasing tow before 3 minutes will be below 500m QFE, and won't go much around
+  // until a thermal is found. So no problems. 
+
+  if (!winchdetected) {
+    if ( (GPS_INFO.Altitude - gndAltitude)<400)
+      return false;
+    if ( (GPS_INFO.Time - Calculated->TakeOffTime) < 150)
+      return false;
+  }
+
   backtrue:
-
-  // REMOVE THIS MESSAGE FOR PRODUCTION VERSION, NO NEED.
+  #if DEBUG_DFF
   DoStatusMessage(gettext(TEXT("_@M1452_")));  // LKTOKEN  _@M1452_ = "Free flight detected"
-
+  #endif
   ffDetected=true;
   Calculated->FreeFlying=true;
   return true;
