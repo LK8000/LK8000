841a91ab9e0024fea8f0770b6b89ac0cab47130b
Mateusz Pusz <mpusz@lk8000.it>
2011-04-24 09:40:41 +0200

    IMI basic communication tools implemented

--- Common/Header/devIMI.h
+++ Common/Header/devIMI.h
@@ -13,10 +13,112 @@
 
 class CDevIMI : public DevBase
 {
+  typedef unsigned char IMIBYTE;                // 8bit unsigned
+  typedef unsigned short IMIWORD;               // 16bit unsigned
+  typedef unsigned long IMIDWORD;               // 32bit unsigned
+  // typedef unsigned __int64 IMIDDWORD;	//64bit unsigned
+  
+  // typedef signed char IMISBYTE;              //8bit signed
+  typedef short IMISWORD;                       // 16bit unsigned
+  // typedef long IMISDWORD;                    //32bit unsigned
+  // typedef __int64 IMISDDWORD;                //64bit unsigned
+  
+  // typedef unsigned long IMIDATETIMESEC;      //32bit unsigned
+  
+  enum TMsgType {
+    MSG_ACK_SUCCESS      = 0x00,
+    MSG_ACK_FAILURE      = 0x01,
+    MSG_ACK_LOGGING      = 0x02,
+    MSG_ACK_NOTCONFIG    = 0x03,
+    MSG_ACK_INVSTATE     = 0x04,
+    
+    MSG_CFG_HELLO        = 0x10,
+    MSG_CFG_BYE          = 0x11,
+    MSG_CFG_FORCESTOP    = 0x12,
+    MSG_CFG_STARTCONFIG  = 0x13,
+    MSG_CFG_DEVICEINFO   = 0x14,
+    MSG_CFG_KEEPCONFIG   = 0x15,
+    MSG_CFG_CONFIG_ID    = 0x16,
+    MSG_CFG_DEFAULTOZ    = 0x17,
+    
+    MSG_DECLARATION      = 0x20,
+    
+    MSG_FLIGHT_INFO      = 0x40,
+    MSG_FLIGHT_DELETE    = 0x41,
+    MSG_FLIGHT_DELETEALL = 0x42
+  };
+  
+  static const IMIBYTE IMICOMM_SYNC_CHAR1 = 'E';
+  static const IMIBYTE IMICOMM_SYNC_CHAR2 = 'X';
+  static const unsigned IMICOMM_CRC_LEN = 2;
+  static const unsigned COMM_MAX_PAYLOAD_SIZE = 1024;
+  
+  struct TMsg {
+    IMIBYTE syncChar1, syncChar2;
+    IMIWORD sn;
+    IMIBYTE msgID, parameter1;
+    IMIWORD parameter2;
+    IMIWORD parameter3;
+    IMIWORD payloadSize;
+    IMIBYTE payload[COMM_MAX_PAYLOAD_SIZE];
+    IMIWORD crc16;
+  };
+  
+  struct TDeviceInfo {
+    IMIBYTE device;
+    IMIBYTE tampered;
+    IMIBYTE hwVersion;
+    IMIBYTE swVersion;
+    IMIBYTE gps;
+    IMIBYTE sensor;
+    IMIBYTE flash;
+    IMIBYTE eeprom;
+    IMIDWORD flashSize;
+    IMIDWORD eepromSize;
+    IMISWORD sensor0Offset;
+    IMISWORD sensor8kOffset;
+    IMIWORD buildNumber;
+    IMIBYTE reserved[64 - 22];
+  } PACKED;
+  
+#define IMICOMM_MAX_MSG_SIZE (sizeof(TMsg))
+#define IMICOMM_MSG_HEADER_SIZE ((unsigned)(&(((TMsg *)0)->payload)))
+#define IMICOMM_BIGPARAM1(param) ((IMIBYTE)((param) >> 16))
+#define IMICOMM_BIGPARAM2(param) ((IMIWORD)(param))
+  
+  class CMsgParser {
+    enum TState {
+      STATE_NOT_SYNC,
+      STATE_COMM_MSG
+    };
+    
+    TState _state;
+    IMIBYTE _msgBuffer[IMICOMM_MAX_MSG_SIZE];
+    unsigned _msgBufferPos;
+    unsigned _msgLen;
+    
+    bool Check(const TMsg *msg, IMIDWORD size) const;
+    
+  public:
+    void Reset();
+    const TMsg *Parse(const IMIBYTE buffer[], IMIDWORD size);
+  };
+  
+  static bool _connected;
+  static CMsgParser _parser;
+  static TDeviceInfo _info;
+  static IMIWORD _serialNumber;
+  
+  // IMI tools
+  static IMIWORD CRC16Checksum(const void *message, unsigned bytes);
+  static bool Send(PDeviceDescriptor_t d, const TMsg &msg, unsigned errBufSize, TCHAR errBuf[]);
+  static bool Send(PDeviceDescriptor_t d, IMIBYTE msgID, unsigned errBufSize, TCHAR errBuf[], const void *payload = 0, IMIWORD payloadSize = 0, IMIBYTE parameter1 = 0, IMIWORD parameter2 = 0);
+  static const TMsg *Receive(PDeviceDescriptor_t d, unsigned extraTimeout, unsigned expectedPayloadSize, unsigned errBufSize, TCHAR errBuf[]);
+  
   // IMI interface
   static bool Connect(PDeviceDescriptor_t d, unsigned errBufSize, TCHAR errBuf[]);
-  static bool Disconnect(PDeviceDescriptor_t d, unsigned errBufSize, TCHAR errBuf[]);
   static bool DeclarationWrite(PDeviceDescriptor_t d, Declaration_t *decl, unsigned errBufSize, TCHAR errBuf[]);
+  static bool Disconnect(PDeviceDescriptor_t d, unsigned errBufSize, TCHAR errBuf[]);
   
   // LK interface
   static const TCHAR *GetName();
--- Common/Source/devIMI.cpp
+++ Common/Source/devIMI.cpp
@@ -14,15 +14,232 @@
 #include "utils/heapcheck.h"
 
 
-const TCHAR *CDevIMI::GetName()
+bool CDevIMI::_connected;
+CDevIMI::CMsgParser CDevIMI::_parser;
+CDevIMI::TDeviceInfo CDevIMI::_info;
+CDevIMI::IMIWORD CDevIMI::_serialNumber;
+
+
+void CDevIMI::CMsgParser::Reset()
 {
-  return(_T("IMI ERIXX"));
+  _msgLen = 0;
+  _msgBufferPos = 0;
+  _state = STATE_NOT_SYNC;
+}
+
+
+const CDevIMI::TMsg *CDevIMI::CMsgParser::Parse(const IMIBYTE buffer[], IMIDWORD size)
+{
+  const IMIBYTE *ptr = buffer;
+  const TMsg *msg = 0;
+  
+  for(;size; size--) {
+    IMIBYTE byte = *ptr++;
+    
+    if(_state == STATE_NOT_SYNC) {
+      if(byte == IMICOMM_SYNC_CHAR1 && _msgBufferPos == 0) {
+        _msgBuffer[_msgBufferPos++] = byte;
+      }
+      else if(byte == IMICOMM_SYNC_CHAR2 && _msgBufferPos == 1) {
+        _msgBuffer[_msgBufferPos++] = byte;
+        _state = STATE_COMM_MSG;
+      }
+      else
+        _msgBufferPos = 0;
+    }
+    else if(_state == STATE_COMM_MSG) {
+      if(_msgBufferPos < IMICOMM_MSG_HEADER_SIZE)
+        _msgBuffer[_msgBufferPos++] = byte;
+      else {
+        if(_msgBufferPos == IMICOMM_MSG_HEADER_SIZE) {
+          _msgLen = ((TMsg *)_msgBuffer)->payloadSize + IMICOMM_CRC_LEN;
+          
+          if(_msgLen > COMM_MAX_PAYLOAD_SIZE + IMICOMM_CRC_LEN) {
+            // Invalid length
+            Reset();
+            continue;
+          }
+        }
+        
+        _msgLen--;
+        
+        if(_msgBufferPos < sizeof(_msgBuffer)) // Just in case
+          _msgBuffer[_msgBufferPos++] = byte;
+        
+        if(_msgLen == 0) {
+          if(Check((TMsg *)_msgBuffer, _msgBufferPos))
+            msg = (TMsg *)_msgBuffer;
+          
+          Reset();
+        }
+      }
+    }
+  }
+  
+  return msg;
+}
+
+
+bool CDevIMI::CMsgParser::Check(const TMsg *msg, IMIDWORD size) const
+{
+  // minimal size of comm message
+  if(size < IMICOMM_MSG_HEADER_SIZE + IMICOMM_CRC_LEN)
+    return false;
+  
+  // check signature
+  if(msg->syncChar1 != IMICOMM_SYNC_CHAR1 || msg->syncChar2 != IMICOMM_SYNC_CHAR2)
+    return false;
+  
+  // check size
+  if(msg->payloadSize != size - IMICOMM_MSG_HEADER_SIZE - IMICOMM_CRC_LEN)
+    return false;
+  
+  IMIWORD crc1 = CDevIMI::CRC16Checksum(((IMIBYTE*)msg) + 2, IMICOMM_MSG_HEADER_SIZE + msg->payloadSize - 2);
+  IMIWORD crc2 = (IMIWORD)(((IMIBYTE*)msg)[size - 1]) | ((IMIWORD)(((IMIBYTE*)msg)[size - 2]) << 8);
+  if(crc1 != crc2)
+    return false;
+  
+  return true;
+}
+
+
+
+
+CDevIMI::IMIWORD CDevIMI::CRC16Checksum(const void *message, unsigned bytes)
+{
+  const IMIBYTE *pData = (const IMIBYTE *)message;
+  
+  IMIWORD crc = 0xFFFF;
+  for(;bytes; bytes--) {
+    crc  = (IMIBYTE)(crc >> 8) | (crc << 8);
+    crc ^= *pData++;
+    crc ^= (IMIBYTE)(crc & 0xff) >> 4;
+    crc ^= (crc << 8) << 4;
+    crc ^= ((crc & 0xff) << 4) << 1;
+  }
+  
+  if (crc == 0xFFFF)
+    crc = 0xAAAA;
+  
+  return crc;
+}
+
+
+bool CDevIMI::Send(PDeviceDescriptor_t d, const TMsg &msg, unsigned errBufSize, TCHAR errBuf[])
+{
+  return ComWrite(d, &msg, IMICOMM_MSG_HEADER_SIZE + msg.payloadSize + 2, errBufSize, errBuf);
+}
+
+
+bool CDevIMI::Send(PDeviceDescriptor_t d, IMIBYTE msgID, unsigned errBufSize, TCHAR errBuf[], const void *payload /* =0 */, IMIWORD payloadSize /* =0 */, IMIBYTE parameter1 /* =0 */, IMIWORD parameter2 /* =0 */)
+{
+  if(payloadSize > COMM_MAX_PAYLOAD_SIZE)
+    return false;
+  
+  TMsg msg;
+  memset(&msg, 0, sizeof(msg));
+  
+  msg.syncChar1 = IMICOMM_SYNC_CHAR1;
+  msg.syncChar2 = IMICOMM_SYNC_CHAR2;
+  msg.sn = _serialNumber;
+  msg.msgID = msgID;
+  msg.parameter1 = parameter1;
+  msg.parameter2 = parameter2;
+  msg.parameter3 = 0;
+  msg.payloadSize = payloadSize;
+  memcpy(msg.payload, payload, payloadSize);
+  
+  IMIWORD crc = CRC16Checksum(((IMIBYTE*)&msg) + 2, payloadSize + IMICOMM_MSG_HEADER_SIZE - 2); 
+  msg.payload[payloadSize] = (IMIBYTE)(crc >> 8);
+  msg.payload[payloadSize + 1] = (IMIBYTE)crc;
+  
+  return Send(d, msg, errBufSize, errBuf);
+}
+
+
+const CDevIMI::TMsg *CDevIMI::Receive(PDeviceDescriptor_t d, unsigned extraTimeout, unsigned expectedPayloadSize, unsigned errBufSize, TCHAR errBuf[])
+{
+  if(expectedPayloadSize > COMM_MAX_PAYLOAD_SIZE)
+    expectedPayloadSize = COMM_MAX_PAYLOAD_SIZE;
+  
+  // set timeout
+  unsigned timeout = extraTimeout + 10000 * (expectedPayloadSize + sizeof(IMICOMM_MSG_HEADER_SIZE) + 10) / d->Com->GetBaudrate();
+  int orgTimeout;
+  if(!SetRxTimeout(d, timeout, orgTimeout, errBufSize, errBuf))
+    return 0;
+  
+  // read message
+  IMIBYTE buffer[64];
+  IMIDWORD bytesRead = d->Com->Read(buffer, sizeof(buffer));
+  if(bytesRead == 0) {
+    // LKTOKEN  _@M1414_ = "Device not responsive!"
+    _sntprintf(errBuf, errBufSize, _T("%s"), gettext(_T("_@M1414_")));
+    return 0;
+  }
+  const TMsg *msg = 0;
+  const TMsg *lastMsg = _parser.Parse(buffer, bytesRead);
+  if(lastMsg) {
+    if(lastMsg->msgID == MSG_ACK_NOTCONFIG)
+      Disconnect(d, errBufSize, errBuf);
+    else if(lastMsg->msgID != MSG_CFG_KEEPCONFIG)
+      msg = lastMsg;
+  }
+  
+  // restore timeout
+  if(!SetRxTimeout(d, orgTimeout, orgTimeout, errBufSize, errBuf))
+    return 0;
+  
+  return msg;
 }
 
 
 bool CDevIMI::Connect(PDeviceDescriptor_t d, unsigned errBufSize, TCHAR errBuf[])
 {
-  return true;
+  if(_connected)
+    Disconnect(d, errBufSize, errBuf);
+  _connected = false;
+  memset(&_info, 0, sizeof(_info));
+  _serialNumber = 0;
+  _parser.Reset();
+  
+  // check connectivity
+  if(Send(d, MSG_CFG_HELLO, errBufSize, errBuf)) {
+    const TMsg *msg = Receive(d, 100, 0, errBufSize, errBuf);
+    if(msg) {
+      if(msg->msgID == MSG_CFG_HELLO)
+        _serialNumber = msg->sn;
+      else
+        return false;
+    }
+  }
+  
+  // configure baudrate
+  unsigned long baudRate = d->Com->GetBaudrate();
+  if(!Send(d, MSG_CFG_STARTCONFIG, errBufSize, errBuf, 0, 0, IMICOMM_BIGPARAM1(baudRate), IMICOMM_BIGPARAM2(baudRate)))
+    return false;
+  
+  // get device info
+  for(int i = 0; i < 4; i++) {
+    if(Send(d, MSG_CFG_DEVICEINFO, errBufSize, errBuf)) {
+      const TMsg *msg = Receive(d, 300, sizeof(TDeviceInfo), errBufSize, errBuf);
+      if(msg) {
+        if(msg->msgID == MSG_CFG_DEVICEINFO && msg->payloadSize == sizeof(TDeviceInfo)) {
+          memcpy(&_info, msg->payload, sizeof(TDeviceInfo));
+          _connected = true;
+          return true;
+        }
+        else if(msg->msgID == MSG_CFG_DEVICEINFO && msg->payloadSize == 16) {
+          // old version of the structure
+          memset(&_info, 0, sizeof(TDeviceInfo));
+          memcpy(&_info, msg->payload, 16);
+          _connected = true;
+          return true;
+        }
+      }
+    }
+  }
+  
+  return false;
 }
 
 
@@ -34,7 +251,22 @@ bool CDevIMI::DeclarationWrite(PDeviceDescriptor_t d, Declaration_t *decl, unsig
 
 bool CDevIMI::Disconnect(PDeviceDescriptor_t d, unsigned errBufSize, TCHAR errBuf[])
 {
-  return true;
+  if(_connected) {
+    if(Send(d, MSG_CFG_BYE, errBufSize, errBuf)) {
+      _connected = false;
+      memset(&_info, 0, sizeof(_info));
+      _serialNumber = 0;
+      return true;
+    }
+  }
+  return false;
+}
+
+
+
+const TCHAR *CDevIMI::GetName()
+{
+  return(_T("IMI ERIXX"));
 }
 
 
