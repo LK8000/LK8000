a1f64137c85895c75ec831c87ddc0bac5273424d
Paolo Ventafridda <venta@lk8000.it>
2011-06-03 02:13:56 +0200

    Version 2.3a release

--- buildnumber.h
+++ buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 7012
-#define BUILDNUMBER_STR "7012" 
+#define BUILDNUMBER 7016
+#define BUILDNUMBER_STR "7016" 
 
 #endif /* BUILD_NUMBER_H_ */
