b513ee74ba4b41c069fa14bec63a5bf4049b5eff
Paolo Ventafridda <venta@lk8000.it>
2011-06-09 16:17:54 +0200

    This is version 2.3b

--- Common/Header/Defines.h
+++ Common/Header/Defines.h
@@ -90,7 +90,7 @@
 #define LKNAME		"XCSoar"
 #define LKFORK		"LK8000"
 #define LKVERSION	"2"
-#define LKRELEASE	"3b0"
+#define LKRELEASE	"3b"
 
 #define REGKEYNAME	"Software\\COOL\\LK8000"
 #define XCSDATADIR	"LK8000"
--- buildnumber.h
+++ buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 7016
-#define BUILDNUMBER_STR "7016" 
+#define BUILDNUMBER 7020
+#define BUILDNUMBER_STR "7020" 
 
 #endif /* BUILD_NUMBER_H_ */
