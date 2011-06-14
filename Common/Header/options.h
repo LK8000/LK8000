4e03720cc6ec3d262262ba9fe7e6844c29be262f
Paolo Ventafridda <venta@lk8000.it>
2011-06-14 21:46:09 +0200

    (CC) (CodeCleanup) Adopt Debug.h  and clean options.h

--- Common/Header/options.h
+++ Common/Header/options.h
@@ -10,12 +10,10 @@
 
 #include "Debug.h"				// DEBUG OPTIONS FOR EVERYONE
 #define   MONOCHROME_SCREEN     1             // optimize for monochrom screen
-#define   EXPERIMENTAL          0             // ????
 
 #define   LOGGDEVICEINSTREAM    0             // log device in stream
 #define   LOGGDEVCOMMANDLINE    NULL          // device in-stream logger command line
                                               // ie TEXT("-logA=\\Speicherkarte\\logA.log ""-logB=\\SD Card\\logB.log""")
-#define   AIRSPACEUSEBINFILE    0             // use and maintain binary airspace file
 
 // define this to be true for windows PC port
 #if !defined(WINDOWSPC)
@@ -148,25 +146,14 @@
 #define NOIBOX		  // Ibox mode no more available, an important TODO
 #define DSX		  // only an experimental test feature for sms reception
 
+// Very old stuff probably we can clean and remove
+#define   EXPERIMENTAL          0             // unused stuff, such like SMS send
+#define   AIRSPACEUSEBINFILE    0             // use and maintain binary airspace file
+
  */
 
 /*
- * Put here debug defines, so that other developers can activate them if needed.
-
-#define DEBUG_DEVSETTING  // startupstore messages to understand sequence of device and com port init
-#define DRAWLOAD          // show cpu load (set by DEBUG mode)
-#define DEBUG_DBLCLK      // show double click is being pressed
-#define VENTA_DEBUG_EVENT // show key events, actually very few.
-#define VENTA_DEBUG_KEY   // activates scan key codes, so you know what an hardware key is mapped to
-			  // the HW key scan code is displayed on the screen!
-#define DEBUG_ROTARY      // write in DEBUG.TXT located in the same place of .exe , append mode
-#define CPUSTATS          // activate cpu thread profiling stuff also in Cpustat.h
-#define DEBUG_DEV_COM     // log device communication through DevBase class methods
-#define DEBUG_GPS	  //  Parser.cpp will show more debug messages about wrong fixes
-#define DEBUGNPM	  // port monitor and hearthbeats in Parser
-#define DEBUG_BESTALTERNATE	// full bestalternate messages inside DEBUG.TXT in home directory
-#define DEBUG_AIRSPACE	// Airspace code debug messages to runtime.log
-*/
-
+ * All debug options should be put inside Debug.h
+ */
 
 #endif
