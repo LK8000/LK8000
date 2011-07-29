001e3998e1a0fa5c93cb7190a89d0ac323c4d151
Paolo Ventafridda <venta@lk8000.it>
2011-07-29 20:27:32 +0200

    Holux and Royaltek device drivers using their internal DLLs.

--- Common/Source/LKHolux.cpp
+++ Common/Source/LKHolux.cpp
@@ -1,11 +1,3 @@
-/*
-   LK8000 Tactical Flight Computer -  WWW.LK8000.IT
-   Released under GNU/GPL License v.2
-   See CREDITS.TXT file for authors and copyrights
-
-   $Id$
-*/
-
 #ifdef PNA
 
 #include "StdAfx.h"
@@ -15,47 +7,514 @@
 #include "externs.h"
 #include "utils/heapcheck.h"
 
+/*  **********************************************************
+ *
+ *  Holux FunTrek 130 hardware support
+ *  by Paolo Ventafridda for LK8000 Project, (C) July 2011
+ *  With kind permission and support by Eddy Young, Holux Inc.
+ *
+ *  **********************************************************/
+
+//
+// Baro sensor
+//
+typedef BOOL (WINAPI *pGM130_barOpenDevice)(void);
+pGM130_barOpenDevice GM130_barOpenDevice;
+typedef void (WINAPI *pGM130_barCloseDevice)(void);
+pGM130_barCloseDevice GM130_barCloseDevice;
+typedef int (WINAPI *pGM130_barGetAltitude)(void);
+pGM130_barGetAltitude GM130_barGetAltitude;
+typedef float (WINAPI *pGM130_barGetPressure)(void);
+pGM130_barGetPressure GM130_barGetPressure;
+#if 0 // unused
+typedef BOOL (WINAPI *pGM130_barCalibrate)(int CalValue);
+pGM130_barCalibrate GM130_barCalibrate;
+#endif
+
+//
+// Power manager
+//
+typedef BOOL (WINAPI *pGM130_pwrOpenDevice)(void);
+pGM130_pwrOpenDevice GM130_pwrOpenDevice;
+typedef void (WINAPI *pGM130_pwrCloseDevice)(void);
+pGM130_pwrCloseDevice GM130_pwrCloseDevice;
+typedef int (WINAPI *pGM130_pwrGetLevel)(void);
+pGM130_pwrGetLevel GM130_pwrGetLevel;
+typedef int (WINAPI *pGM130_pwrGetStatus)(void);
+pGM130_pwrGetStatus GM130_pwrGetStatus;
+
+//
+// Backlight manager
+//
+typedef BOOL (WINAPI *pGM130_blOpenDevice)(void);
+pGM130_blOpenDevice GM130_blOpenDevice;
+typedef void (WINAPI *pGM130_blCloseDevice)(void);
+pGM130_blCloseDevice GM130_blCloseDevice;
+typedef void (WINAPI *pGM130_blSetLevel)(int);
+pGM130_blSetLevel GM130_blSetLevel;
+typedef void (WINAPI *pGM130_blSetTimeout)(int);
+pGM130_blSetTimeout GM130_blSetTimeout;
+typedef void (WINAPI *pGM130_blEnAutoLevel)(BOOL);
+pGM130_blEnAutoLevel GM130_blEnAutoLevel;
+
+//
+// Buzzer controls
+//
+typedef BOOL (WINAPI *pGM130_buzzOpenDevice)(void);
+pGM130_buzzOpenDevice GM130_buzzOpenDevice;
+typedef void (WINAPI *pGM130_buzzCloseDevice)(void);
+pGM130_buzzCloseDevice GM130_buzzCloseDevice;
+typedef void (WINAPI *pGM130_buzzSetMessageVolume)(int);
+pGM130_buzzSetMessageVolume GM130_buzzSetMessageVolume;
+typedef void (WINAPI *pGM130_buzzMessageMute)(BOOL);
+pGM130_buzzMessageMute GM130_buzzMessageMute;
+
+#if 0 // unused
+typedef void (WINAPI *pGM130_buzzSound)(int, int, int, int);
+pGM130_buzzSound GM130_buzzSound;
+#endif
+
+#if GM130TEMPERATURE
+//
+// Temperature sensor
+//
+typedef BOOL (WINAPI *pGM130_tempOpenDevice)(void);
+pGM130_tempOpenDevice GM130_tempOpenDevice;
+typedef void (WINAPI *pGM130_tempCloseDevice)(void);
+pGM130_tempCloseDevice GM130_tempCloseDevice;
+typedef int (WINAPI *pGM130_tempGetValue)(void);
+pGM130_tempGetValue GM130_tempGetValue;
+#endif 
 
 bool DeviceIsGM130=false;
+HMODULE hapiHandle=NULL;
+
+#define HOLIB	"GM130API.DLL"
+//#define HODEBUG	1
 
 bool Init_GM130(void) {
+
+  if (DeviceIsGM130) {
+	#if HODEBUG
+	StartupStore(_T("... Init_GM130 already initialised%s"),NEWLINE);
+	#endif
+	return true;
+  }
+
+  hapiHandle=LoadLibrary(TEXT(HOLIB));
+  if (hapiHandle==(HMODULE)NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 ERR"));
+	StartupStore(_T("... Init_GM130 LoadLibrary failed%s"),NEWLINE);
+	#endif
+	return false;
+  }
+
+  // Library was found, error messages ok assuming this is an Holux device
+
+  //
+  // Barometer control
+  // 
+  GM130_barOpenDevice = NULL;
+  GM130_barOpenDevice = (pGM130_barOpenDevice) GetProcAddressA(hapiHandle, "barOpenDevice");
+  if (GM130_barOpenDevice == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE barOpen"));
+	StartupStore(_T("... Init_GM130 GetProcAddress barOpen failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  } 
+  GM130_barCloseDevice = NULL;
+  GM130_barCloseDevice = (pGM130_barCloseDevice) GetProcAddress(hapiHandle, TEXT("barCloseDevice"));
+  if (GM130_barCloseDevice == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE barClose"));
+	StartupStore(_T("... Init_GM130 GetProcAddress barClose failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  GM130_barGetAltitude = NULL;
+  GM130_barGetAltitude = (pGM130_barGetAltitude) GetProcAddress(hapiHandle, TEXT("barGetAltitude"));
+  if (GM130_barGetAltitude == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE barGetAltitude"));
+	StartupStore(_T("... Init_GM130 GetProcAddress barGetAltitude failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  GM130_barGetPressure = NULL;
+  GM130_barGetPressure = (pGM130_barGetPressure) GetProcAddress(hapiHandle, TEXT("barGetPressure"));
+  if (GM130_barGetPressure == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE barGetPressure"));
+	StartupStore(_T("... Init_GM130 GetProcAddress barGetPressure failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  #if 0
+  GM130_barCalibrate = NULL;
+  GM130_barCalibrate = (pGM130_barCalibrate) GetProcAddress(hapiHandle, TEXT("barCalibrate"));
+  if (GM130_barCalibrate == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE barCalibrate"));
+	StartupStore(_T("... Init_GM130 GetProcAddress barCalibrate failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  #endif
+
+  //
+  // Power controls
+  //
+  GM130_pwrOpenDevice = NULL;
+  GM130_pwrOpenDevice = (pGM130_pwrOpenDevice) GetProcAddressA(hapiHandle, "pwrOpenDevice");
+  if (GM130_pwrOpenDevice == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE pwrOpen"));
+	StartupStore(_T("... Init_GM130 GetProcAddress pwrOpen failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  } 
+  GM130_pwrCloseDevice = NULL;
+  GM130_pwrCloseDevice = (pGM130_pwrCloseDevice) GetProcAddress(hapiHandle, TEXT("pwrCloseDevice"));
+  if (GM130_pwrCloseDevice == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE pwrClose"));
+	StartupStore(_T("... Init_GM130 GetProcAddress pwrClose failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  GM130_pwrGetLevel = NULL;
+  GM130_pwrGetLevel = (pGM130_pwrGetLevel) GetProcAddress(hapiHandle, TEXT("pwrGetLevel"));
+  if (GM130_pwrGetLevel == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE pwrGetLevel"));
+	StartupStore(_T("... Init_GM130 GetProcAddress pwrGetLevel failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  GM130_pwrGetStatus = NULL;
+  GM130_pwrGetStatus = (pGM130_pwrGetStatus) GetProcAddress(hapiHandle, TEXT("pwrGetStatus"));
+  if (GM130_pwrGetStatus == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE pwrGetStatus"));
+	StartupStore(_T("... Init_GM130 GetProcAddress pwrGetStatus failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+
+  //
+  // Backlight controls
+  //
+  GM130_blOpenDevice = NULL;
+  GM130_blOpenDevice = (pGM130_blOpenDevice) GetProcAddressA(hapiHandle, "blOpenDevice");
+  if (GM130_blOpenDevice == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE blOpen"));
+	StartupStore(_T("... Init_GM130 GetProcAddress blOpen failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  } 
+  GM130_blCloseDevice = NULL;
+  GM130_blCloseDevice = (pGM130_blCloseDevice) GetProcAddress(hapiHandle, TEXT("blCloseDevice"));
+  if (GM130_blCloseDevice == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE blClose"));
+	StartupStore(_T("... Init_GM130 GetProcAddress blClose failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  GM130_blSetLevel = NULL;
+  GM130_blSetLevel = (pGM130_blSetLevel) GetProcAddress(hapiHandle, TEXT("blSetLevel"));
+  if (GM130_blSetLevel == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE blSetLevel"));
+	StartupStore(_T("... Init_GM130 GetProcAddress blSetLevel failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  GM130_blSetTimeout = NULL;
+  GM130_blSetTimeout = (pGM130_blSetTimeout) GetProcAddress(hapiHandle, TEXT("blSetTimeout"));
+  if (GM130_blSetTimeout == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE blSetTimeout"));
+	StartupStore(_T("... Init_GM130 GetProcAddress blSetTimeout failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  GM130_blEnAutoLevel = NULL;
+  GM130_blEnAutoLevel = (pGM130_blEnAutoLevel) GetProcAddress(hapiHandle, TEXT("blEnAutoLevel"));
+  if (GM130_blEnAutoLevel == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE blEnAutoLevel"));
+	StartupStore(_T("... Init_GM130 GetProcAddress blEnAutoLevel failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+
+
+  //
+  // Buzzer controls
+  //
+  GM130_buzzOpenDevice = NULL;
+  GM130_buzzOpenDevice = (pGM130_buzzOpenDevice) GetProcAddressA(hapiHandle, "buzzOpenDevice");
+  if (GM130_buzzOpenDevice == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE buzzOpen"));
+	StartupStore(_T("... Init_GM130 GetProcAddress buzzOpen failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  } 
+  GM130_buzzCloseDevice = NULL;
+  GM130_buzzCloseDevice = (pGM130_buzzCloseDevice) GetProcAddress(hapiHandle, TEXT("buzzCloseDevice"));
+  if (GM130_buzzCloseDevice == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE buzzClose"));
+	StartupStore(_T("... Init_GM130 GetProcAddress buzzClose failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  GM130_buzzSetMessageVolume = NULL;
+  GM130_buzzSetMessageVolume = (pGM130_buzzSetMessageVolume) GetProcAddress(hapiHandle, TEXT("buzzSetMessageVolume"));
+  if (GM130_buzzSetMessageVolume == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE buzzSetMessageVolume"));
+	StartupStore(_T("... Init_GM130 GetProcAddress buzzSetMessageVolume failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  #if 0 // unused
+  GM130_buzzSound = NULL;
+  GM130_buzzSound = (pGM130_buzzSound) GetProcAddress(hapiHandle, TEXT("buzzSound"));
+  if (GM130_buzzSound == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE buzzSound"));
+	StartupStore(_T("... Init_GM130 GetProcAddress buzzSound failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  #endif
+  GM130_buzzMessageMute = NULL;
+  GM130_buzzMessageMute = (pGM130_buzzMessageMute) GetProcAddress(hapiHandle, TEXT("buzzMessageMute"));
+  if (GM130_buzzMessageMute == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE buzzMessageMute"));
+	StartupStore(_T("... Init_GM130 GetProcAddress buzzMessageMute failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+
+  #if GM130TEMPERATURE
+  //
+  // Temperature sensor
+  //
+  GM130_tempOpenDevice = NULL;
+  GM130_tempOpenDevice = (pGM130_tempOpenDevice) GetProcAddressA(hapiHandle, "tempOpenDevice");
+  if (GM130_tempOpenDevice == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE tempOpen"));
+	StartupStore(_T("... Init_GM130 GetProcAddress tempOpen failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  } 
+  GM130_tempCloseDevice = NULL;
+  GM130_tempCloseDevice = (pGM130_tempCloseDevice) GetProcAddress(hapiHandle, TEXT("tempCloseDevice"));
+  if (GM130_tempCloseDevice == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE tempClose"));
+	StartupStore(_T("... Init_GM130 GetProcAddress tempClose failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  GM130_tempGetValue = NULL;
+  GM130_tempGetValue = (pGM130_tempGetValue) GetProcAddress(hapiHandle, TEXT("tempGetValue"));
+  if (GM130_tempGetValue == NULL) {
+	#if HODEBUG
+	DoStatusMessage(_T("INIT GM130 FAILURE tempGetValue"));
+	StartupStore(_T("... Init_GM130 GetProcAddress tempGetValue failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  }
+  #endif
+
+  #if HODEBUG
+  DoStatusMessage(_T("INIT LIB GM130 OK!"));
+  #endif
+  StartupStore(_T(". Init Library GM130 Ok.%s"),NEWLINE);
+
+  if (GM130_barOpenDevice() == FALSE) {
+	StartupStore(_T("... Init_GM130 barOpenDevice failed%s"),NEWLINE);
+	goto _fail;
+  }
+  if (GM130_pwrOpenDevice() == FALSE) {
+	StartupStore(_T("... Init_GM130 pwrOpenDevice failed%s"),NEWLINE);
+	goto _fail;
+  }
+  if (GM130_buzzOpenDevice() == FALSE) {
+	StartupStore(_T("... Init_GM130 buzzOpenDevice failed%s"),NEWLINE);
+	goto _fail;
+  }
+  #if GM130TEMPERATURE
+  if (GM130_tempOpenDevice() == FALSE) {
+	StartupStore(_T("... Init_GM130 tempOpenDevice failed%s"),NEWLINE);
+	goto _fail;
+  }
+  #endif
+
+  DeviceIsGM130=true;
+
+  return true;
+
+_fail:
+  StartupStore(_T(".... Init GM130 failed!%s"),NEWLINE);
+
+  if (GM130_barOpenDevice != NULL) GM130_barCloseDevice();
+  if (GM130_pwrOpenDevice != NULL) GM130_pwrCloseDevice();
+  if (GM130_buzzOpenDevice != NULL) GM130_buzzCloseDevice();
+  #if GM130TEMPERATURE
+  if (GM130_tempOpenDevice != NULL) GM130_tempCloseDevice();
+  #endif
+
+  FreeLibrary(hapiHandle);
+  hapiHandle=NULL;
   DeviceIsGM130=false;
   return false;
+
 }
 
 void DeInit_GM130(void) {
+
   if (!DeviceIsGM130) return;
   DeviceIsGM130=false;
+
+  GM130_barCloseDevice();
+  GM130_pwrCloseDevice();
+  // Backlight was already closed
+  GM130_buzzCloseDevice();
+  #if GM130TEMPERATURE
+  GM130_tempCloseDevice();
+  #endif
+
+  FreeLibrary(hapiHandle);
+  hapiHandle = NULL;
+
   return;
+
 }
 
+//
+// These functions are called only if DeviceIsGM130
+//
+
 int GM130BarAltitude(void) {
-  return 0;
+
+  if (!DeviceIsGM130) return 0;
+  return GM130_barGetAltitude();
+
 }
 
 float GM130BarPressure(void) {
-  return 1013.25;
+
+  if (!DeviceIsGM130) return 0;
+  return GM130_barGetPressure();
+
 }
 
 int GM130PowerLevel(void) {
-  return 0;
+
+  if (!DeviceIsGM130) return 0;
+  return GM130_pwrGetLevel();
+
 }
 
 int GM130PowerStatus(void) {
-  return 0;
+
+  if (!DeviceIsGM130) return 0;
+  int pstatus = GM130_pwrGetStatus();
+  switch (pstatus) {
+	case 0: 
+		return  AC_LINE_OFFLINE; // NOT_CHARGING
+	case 1:
+		return  AC_LINE_ONLINE;  // CHARGING
+	case 2:
+		return  AC_LINE_ONLINE;  // CHARGE_FINISHED
+	case 3:
+		return  AC_LINE_UNKNOWN; // NO_BATTERY
+	case 4:
+		return  AC_LINE_OFFLINE; // BAD_BATTERY
+	case 5:
+		return  AC_LINE_OFFLINE; // BATTERY_DROP
+	default:
+		return  AC_LINE_UNKNOWN; 
+  }
 }
 
 int GM130PowerFlag(void) {
-  return 0;
+
+  if (!DeviceIsGM130) return 0;
+  int pstatus = GM130_pwrGetStatus();
+  switch (pstatus) {
+	case 0: 
+		return  BATTERY_FLAG_HIGH; // NOT_CHARGING
+	case 1:
+		return  BATTERY_FLAG_CHARGING;  // CHARGING
+	case 2:
+		return  BATTERY_FLAG_HIGH;  //CHARGE_FINISHED
+	case 3:
+		return  BATTERY_FLAG_NO_BATTERY; // NO_BATTERY
+	case 4:
+		return  BATTERY_FLAG_CRITICAL; // BAD_BATTERY
+	case 5:
+		return  BATTERY_FLAG_LOW; // BATTERY_DROP
+	default:
+		return  BATTERY_FLAG_UNKNOWN;
+  }
 }
 
 void GM130MaxBacklight(void) {
-  return;
+
+  if (!DeviceIsGM130) return;
+  if (!GM130_blOpenDevice()) return;
+
+  GM130_blSetTimeout(0); // No timeout
+  GM130_blSetLevel(9);   // Max brightness
+  GM130_blEnAutoLevel(FALSE); 
+
+  GM130_blCloseDevice();
+
 }
 
 // There is no real sound, only a buzzzer. A real pity on this jewel.
 void GM130MaxSoundVolume(void) {
-  return;
+
+  if (!DeviceIsGM130) return;
+  GM130_buzzMessageMute(FALSE);
+  GM130_buzzSetMessageVolume(2); // max volume for MessageBeep buzz
+
 }
+
+#if 0 // unused but available
+// Level 0-1-2
+// Duration in ms
+// Count of times
+// Interval in ms between counts
+void GM130PlaySound(int level, int duration, int count, int interval) {
+  // Function ready to be used
+  if (!DeviceIsGM130) return;
+  if (level<0 || level>2) level=2;
+  if (count<1|| count>100) count=1;
+  GM130_buzzSound(level, duration, count, interval);
+}
+#endif
+
+#if GM130TEMPERATURE
+int GM130GetTemperature(void) {
+
+  if (!DeviceIsGM130) return 0;
+  return GM130_tempGetValue();
+
+}
+#endif
+
 
 #endif // PNA only
--- Common/Source/LKRoyaltek3200.cpp
+++ Common/Source/LKRoyaltek3200.cpp
@@ -1,11 +1,3 @@
-/*
-   LK8000 Tactical Flight Computer -  WWW.LK8000.IT
-   Released under GNU/GPL License v.2
-   See CREDITS.TXT file for authors and copyrights
-
-   $Id$
-*/
-
 #ifdef PNA
 
 #include "StdAfx.h"
@@ -15,28 +7,146 @@
 #include "externs.h"
 #include "utils/heapcheck.h"
 
+/*  ********************************************
+ *
+ *  Royaltek hardware support for LK8000
+ *  
+ *  ********************************************/
+
+typedef struct {
+  long OUT_TEMP;
+  long OUT_PRESSURE;
+  long OUT_ALTITUDE;
+} INFO_GET, *pINFO_GET;
+
+typedef enum {
+  __OK,
+  _ALTI_ERROR_1,
+  _ALTI_ERROR_2,
+} API_RET;
+
+static INFO_GET bikeInfo;
+
+//
+// Baro sensor
+//
+typedef API_RET (WINAPI *pRoyaltek3200_GetBikeInfo)(INFO_GET *bikeInfo);
+pRoyaltek3200_GetBikeInfo Royaltek3200_GetBikeInfo;
 
 bool DeviceIsRoyaltek3200=false;
+HMODULE royapiHandle=NULL;
+
+#define ROYLIB	"BIKEINFO.DLL"
+//#define ROYDEBUG	1
 
 bool Init_Royaltek3200(void) {
+
+  if (DeviceIsRoyaltek3200) {
+	#if ROYDEBUG
+	StartupStore(_T("... Init_Royaltek3200 already initialised%s"),NEWLINE);
+	#endif
+	return true;
+  }
+
+  bikeInfo.OUT_TEMP=0;
+  bikeInfo.OUT_PRESSURE=0;
+  bikeInfo.OUT_ALTITUDE=0;
+
+  royapiHandle=LoadLibrary(TEXT(ROYLIB));
+  if (royapiHandle==(HMODULE)NULL) {
+	#if ROYDEBUG
+	DoStatusMessage(_T("INIT ROYALTEK3200 ERR"));
+	StartupStore(_T("... Init_Royaltek3200 LoadLibrary failed%s"),NEWLINE);
+	#endif
+	return false;
+  }
+
+  // Library was found, error messages ok assuming this is a Royaltek device
+
+  //
+  // Barometer control
+  // 
+  Royaltek3200_GetBikeInfo = NULL;
+  Royaltek3200_GetBikeInfo = (pRoyaltek3200_GetBikeInfo) GetProcAddressA(royapiHandle, "GetBikeInfo");
+  if (Royaltek3200_GetBikeInfo == NULL) {
+	#if ROYDEBUG
+	DoStatusMessage(_T("INIT ROYALTEK3200 FAILURE GetBikeInfo"));
+	StartupStore(_T("... Init_Royaltek3200 GetProcAddress GetBikeInfo failed%s"),NEWLINE);
+	#endif
+	goto _fail;
+  } 
+
+  #if ROYDEBUG
+  DoStatusMessage(_T("INIT LIB ROYALTEK3200 OK!"));
+  #endif
+  StartupStore(_T(". Init Library Royaltek3200 Ok.%s"),NEWLINE);
+
+  DeviceIsRoyaltek3200=true;
+
+  return true;
+
+_fail:
+  StartupStore(_T(".... Init Royaltek3200 failed!%s"),NEWLINE);
+
+  FreeLibrary(royapiHandle);
+  royapiHandle=NULL;
   DeviceIsRoyaltek3200=false;
   return false;
+
 }
 
 void DeInit_Royaltek3200(void) {
+
   if (!DeviceIsRoyaltek3200) return;
   DeviceIsRoyaltek3200=false;
+
+  FreeLibrary(royapiHandle);
+  royapiHandle = NULL;
+
   return;
+
 }
 
-int Royaltek3200_ReadBarData(void) {
-  return 0;
+//
+// These functions are called only if DeviceIsRoyaltek3200
+//
+
+
+bool Royaltek3200_ReadBarData(void) {
+
+  int result=Royaltek3200_GetBikeInfo(&bikeInfo);
+  #if ROYDEBUG
+  if (result!=__OK) {
+	StartupStore(_T("...... ReadBikeInfo failed, result=%d\n"),result);
+  } else {
+	StartupStore(_T("...... ReadBikeInfo: Alt=%d Pressure=%d Temp=%d\n"),
+		bikeInfo.OUT_ALTITUDE,
+		bikeInfo.OUT_PRESSURE,
+		bikeInfo.OUT_TEMP);
+  }
+  #endif
+  return (result==__OK);
 }
+
 
 float Royaltek3200_GetPressure(void) {
-  return 1013.25;
+
+  if (!DeviceIsRoyaltek3200) return 0;
+  return (float)(bikeInfo.OUT_PRESSURE/10);
 }
 
+#if 0
+int Royaltek3200_GetAltitude(void) {
 
+  if (!DeviceIsRoyaltek3200) return 0;
+  return (int)(bikeInfo.OUT_ALTITUDE);
+}
+
+float Royaltek3200_GetTemperature(void) {
+
+  if (!DeviceIsRoyaltek3200) return 0;
+  return (float)(bikeInfo.OUT_TEMP/10);
+}
+#endif
 
 #endif // PNA only
