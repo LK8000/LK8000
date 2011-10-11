0a257cd9f07094e9c4ac42e746b7522304f7a3c3
Paolo Ventafridda <venta@lk8000.it>
2011-10-11 14:31:49 +0200

    Fix bug 2.3 Alarms not showing warning message correctly for feet altitude units

--- Common/Source/LKMapWindow.cpp
+++ Common/Source/LKMapWindow.cpp
@@ -1010,7 +1010,7 @@ void MapWindow::DrawLKAlarms(HDC hDC, const RECT rc) {
 		case 2:
 			wsprintf(textalarm,_T("%s %d: %s %d"),
 			gettext(_T("_@M1650_")), currentalarm+1, gettext(_T("_@M1651_")),  // ALARM ALTITUDE
-			LKalarms[currentalarm].triggervalue);
+			((int)((double)LKalarms[currentalarm].triggervalue*ALTITUDEMODIFY)));
 			break;
 		default:
 			break;
