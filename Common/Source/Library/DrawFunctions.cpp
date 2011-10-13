ede4a598419a99ddee88e09ec1daa8ccef13cedd
Paolo Ventafridda <venta@lk8000.it>
2011-10-14 01:05:00 +0200

    (CC) Memory.cpp

--- Common/Source/Library/DrawFunctions.cpp
+++ Common/Source/Library/DrawFunctions.cpp
@@ -391,3 +391,22 @@ BOOL PolygonVisible(const POINT *lpPoints, int nCount, RECT rc)
 }
 
 
+
+#if TOPOFASTLABEL
+bool CheckRectOverlap(const RECT *rc1, const RECT *rc2) {
+  if(rc1->left >= rc2->right) return(false);
+  if(rc1->right <= rc2->left) return(false);
+  if(rc1->top >= rc2->bottom) return(false);
+  if(rc1->bottom <= rc2->top) return(false);
+  return(true);
+}
+#else
+bool CheckRectOverlap(RECT rc1, RECT rc2) {
+  if(rc1.left >= rc2.right) return(false);
+  if(rc1.right <= rc2.left) return(false);
+  if(rc1.top >= rc2.bottom) return(false);
+  if(rc1.bottom <= rc2.top) return(false);
+  return(true);
+}
+#endif
+
--- Makefile
+++ Makefile
@@ -379,6 +379,7 @@ DLGS	:=\
 	$(DLG)/dlgWindSettings.cpp \
 
 SRC_FILES :=\
+	$(SRC)/Memory.cpp \
 	$(SRC)/Sound.cpp \
 	$(SRC)/Oracle.cpp		$(SRC)/Alarms.cpp\
 	$(SRC)/LKInterface.cpp		$(SRC)/LocalPath.cpp\
