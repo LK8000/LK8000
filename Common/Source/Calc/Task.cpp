214d5fd9fb8c6ff14c0fe901e6b23c7dcc4b5027
Paolo Ventafridda <venta@lk8000.it>
2011-12-02 23:42:26 +0100

    Fix bug in task save on PC version when using custom local root folder

--- Common/Source/Calc/Task.cpp
+++ Common/Source/Calc/Task.cpp
@@ -1577,12 +1577,13 @@ void CalculateAATIsoLines(void) {
 void SaveDefaultTask(void) {
   LockTaskData();
     TCHAR buffer[MAX_PATH];
-#if (!defined(WINDOWSPC) || (WINDOWSPC <=0) )
+//#if (!defined(WINDOWSPC) || (WINDOWSPC <=0) )
+#if 1
   LocalPath(buffer,TEXT(LKD_TASKS));
   _tcscat(buffer,TEXT("\\"));
   _tcscat(buffer,_T(LKF_DEFAULTASK)); // 091101
-#else
-  SHGetSpecialFolderPath(hWndMainWindow, buffer, CSIDL_PERSONAL, false);
+#else // REMOVE 
+  SHGetSpecialFolderPath(hWndMainWindow, buffer, CSIDL_PERSONAL, false); // REMOVABLE
   _tcscat(buffer,TEXT("\\"));
   _tcscat(buffer,TEXT(LKDATADIR));
   _tcscat(buffer,_T("\\"));
