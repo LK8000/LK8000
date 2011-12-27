5aa8f99a6915b9a5933ec085d5c01c31f05f13ae
Paolo Ventafridda <venta@lk8000.it>
2011-12-27 03:28:55 +0100

    Fix minor problem with HeadWind display on startup, showing bad value before a valid GPS signal.

--- Common/Source/LKProcess.cpp
+++ Common/Source/LKProcess.cpp
@@ -1221,7 +1221,7 @@ goto_bearing:
 			_stprintf(BufferTitle, TEXT("%s"), Data_Options[lkindex].Title );
 			wsprintf(BufferUnit, TEXT("%s"),Units::GetHorizontalSpeedName());
 
-			if (value==-999) {
+			if (DerivedDrawInfo.HeadWind==-999) {
 				_stprintf(BufferValue,TEXT(NULLMEDIUM)); 
 				break;
 			}
