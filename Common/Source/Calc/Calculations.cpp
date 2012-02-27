df9d72be78fc0be6971d7f586776457dfcec1b1f
Paolo Ventafridda <venta@lk8000.it>
2012-02-27 17:43:53 +0100

    Fix minor problem resetting flightstats when replaying an IGC file crossing 00:00 UTC. This is only a workaround, because replay logger is really setting a basic time which is not considering date change.

--- Common/Source/Calc/Calculations.cpp
+++ Common/Source/Calc/Calculations.cpp
@@ -589,7 +589,10 @@ bool FlightTimes(NMEA_INFO *Basic, DERIVED_INFO *Calculated) {
   static double LastTime = 0;
 
   if ((Basic->Time != 0) && (Basic->Time <= LastTime)) {
-	if ( (LastTime - Basic->Time >30 ) && (!Basic->NAVWarning)) {
+	if ( (LastTime - Basic->Time >30 ) && (!Basic->NAVWarning) && 
+	// replay logger does not consider UTC 00 incrementing by 85000 or whatever the basic time.
+	// Meanwhile, we may also skip the 00 utc because of interpolation. So we consider this.
+	!( ReplayLogger::IsEnabled() && Basic->Time<60)  ) {
 		// Reset statistics.. (probably due to being in IGC replay mode)
 		StartupStore(_T("... Time is in the past! %s%s"), WhatTimeIsIt(),NEWLINE);
 
