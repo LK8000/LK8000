bfe3c825774cc62b43dcdab6344c8a74c2877e2c
Paolo Ventafridda <venta@lk8000.it>
2012-06-04 17:55:46 +0200

    MiniMap automatic selection is yellow painted

--- Common/Source/WindowControls.cpp
+++ Common/Source/WindowControls.cpp
@@ -2802,6 +2802,8 @@ void WndButton::Paint(HDC hDC){
 
   // JMW todo: add icons?
 
+
+  if(GlobalModelType!=MODELTYPE_PNA_MINIMAP  || (GlobalModelType==MODELTYPE_PNA_MINIMAP && !HasFocus()))
   if (mDown){
     DrawFrameControl(hDC, &rc, DFC_BUTTON, DFCS_BUTTONPUSH | DFCS_PUSHED);
   }else{
