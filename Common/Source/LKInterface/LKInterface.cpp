fd314576ea3ae3c0fe47527d5dc2882766d7c31d
Paolo Ventafridda <venta@lk8000.it>
2012-10-10 14:28:55 +0200

    Let MSM_MAPRADAR be visible in TESTBENCH mode even when no flarm available

--- Common/Source/LKInterface/LKInterface.cpp
+++ Common/Source/LKInterface/LKInterface.cpp
@@ -51,7 +51,10 @@ redo:
 #endif
 	if (CURMODE == MSM_WELCOME) goto redo;
 
+#if TESTBENCH
+#else
 	if (CURMODE == MSM_MAPRADAR && !GPS_INFO.FLARM_Available) goto redo;
+#endif
 finish:
 	SelectMapSpace( ModeTable[ModeIndex][CURTYPE] );
 	if (ModeIndex==LKMODE_MAP) MultiMapSound();
@@ -90,8 +93,10 @@ redo:
 	if (CURMODE == MSM_INFO_TRI) goto redo;
 #endif
 	if (CURMODE == MSM_WELCOME) goto redo;
-
+#if TESTBENCH
+#else
 	if (CURMODE == MSM_MAPRADAR && !GPS_INFO.FLARM_Available) goto redo;
+#endif
 finish:
 	SelectMapSpace( ModeTable[ModeIndex][CURTYPE] );
 	if (ModeIndex==LKMODE_MAP) MultiMapSound();
