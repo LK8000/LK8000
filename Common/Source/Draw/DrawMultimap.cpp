644dee57559fd4d1bdf82d5574981bc79d7a2f73
Paolo Ventafridda <venta@lk8000.it>
2012-10-10 17:59:55 +0200

    New overtarget info overlay on MAPTRK and MAPWPT, to be made optionally enabled/disabled and saved to config. Not sure what to do on MAPASP, we may reduce font size and keep it permanently, there. To be decided.

--- Common/Source/Draw/DrawMultimap.cpp
+++ Common/Source/Draw/DrawMultimap.cpp
@@ -13,6 +13,7 @@
 #include "Sideview.h"
 #include "Multimap.h"
 #include "LKInterface.h"
+#include "McReady.h"
 
 extern bool Sonar_IsEnabled;
 extern bool ActiveMap_IsEnabled;
@@ -168,7 +169,8 @@ void MapWindow::DrawMultimap_DynaLabel(const HDC hdc, const RECT rci)
   int midsplit=(long)((double)(rci.bottom-rci.top)*fSplitFact);	 // this is ok
   //int midsplit=Sideview_TopRect_InUse.bottom;	// this SHOULD be ok, but in M3 the TopRect is updated 1s late
 
-  oldFont=(HFONT)SelectObject(hdc, LK8UnitFont);
+  //oldFont=(HFONT)SelectObject(hdc, LK8UnitFont);
+  oldFont=(HFONT)SelectObject(hdc, LK8PanelSmallFont);
 
   GetTextExtentPoint(hdc, _T("Y"), 1, &textSize);
   // move the label on top view when the topview window is big enough
@@ -183,26 +185,75 @@ void MapWindow::DrawMultimap_DynaLabel(const HDC hdc, const RECT rci)
         TCHAR topcenter_txt[80];
         _stprintf(topcenter_txt, TEXT("%s"), Sideview_szNearAS );
 
+	HPEN oldpen=(HPEN)SelectObject(hdc,LKPen_White_N1);
+
         MapWindow::LKWriteBoxedText(hdc,&MapRect,topcenter_txt, rci.right/3, midsplit, 0, WTALIGN_CENTER, RGB_WHITE, RGB_BLACK);
+
+	SelectObject(hdc,oldpen);
 	goto _return;
   }
 
-#if 0
-  if(MapSpaceMode==MSM_MAPWPT)
+  if(MapSpaceMode==MSM_MAPWPT || MapSpaceMode==MSM_MAPTRK)
   {
         TCHAR topcenter_txt[80];
 	int index=GetOvertargetIndex();
 	if (index<0) goto _return;
 
 	TCHAR wpname[NAME_SIZE+1];
+	TCHAR BufferValue[10];
+	TCHAR BufferUnit[10];
+
+	//
+	// Waypoint name, bearing difference and distance
+	//
 	LK_tcsncpy(wpname, WayPointList[index].Name, 8);
+	LKFormatBrgDiff(index, false, BufferValue, BufferUnit);
+        _stprintf(topcenter_txt, TEXT("%s   %s   "),wpname,BufferValue);
+	LKFormatDist(index, false, BufferValue, BufferUnit);
+	_tcscat(topcenter_txt,BufferValue);
+	_tcscat(topcenter_txt,BufferUnit);
 
-        _stprintf(topcenter_txt, TEXT("%s  12.2km reqE=12 +123  "), wpname);
+	HPEN oldpen=(HPEN)SelectObject(hdc,LKPen_White_N1);
+        MapWindow::LKWriteBoxedText(hdc,&MapRect,topcenter_txt, rci.right/4+NIBLSCALE(4), midsplit, 0, WTALIGN_CENTER, RGB_WHITE, RGB_BLACK);
 
-        MapWindow::LKWriteBoxedText(hdc,&MapRect,topcenter_txt, rci.right/4, midsplit, 0, WTALIGN_CENTER, RGB_WHITE, RGB_BLACK);
-	goto _return;
+	//
+	// Efficiency required and Arrival altitude
+	//
+	_tcscpy(topcenter_txt,_T(""));
+	LKFormatGR(index, false, BufferValue, BufferUnit);
+	_tcscat(topcenter_txt,BufferValue);
+	_tcscat(topcenter_txt,_T("   "));
+	LKFormatAltDiff(index, false, BufferValue, BufferUnit);
+	_tcscat(topcenter_txt,BufferValue);
+	_tcscat(topcenter_txt,BufferUnit);
+
+	if (WayPointCalc[index].AltArriv[AltArrivMode]<0) {
+		SelectObject(hdc,LKPen_Red_N1);
+	} else {
+		// Reachable is not available for virtual wps, to be investigated..
+		// So we dont paint green or red for them, when arrival is positive, because
+		// we dont know if there are obstacles.
+		if (index>RESWP_END) {
+			if (WayPointList[index].Reachable )
+				SelectObject(hdc,LKPen_Green_N1);
+			else
+				SelectObject(hdc,LKPen_Red_N1);
+		}
+	}
+
+	extern bool IsSafetyMacCreadyInUse(int val);
+        if (IsSafetyMacCreadyInUse(index)) {
+                _stprintf(BufferValue,_T("   @%.1f"),GlidePolar::SafetyMacCready*LIFTMODIFY);
+        } else {
+                _stprintf(BufferValue,_T("   @%.1f"),MACCREADY);
+	}
+	_tcscat(topcenter_txt,BufferValue);
+
+        MapWindow::LKWriteBoxedText(hdc,&MapRect,topcenter_txt, rci.right-rci.right/4, midsplit, 0, WTALIGN_CENTER, RGB_WHITE, RGB_BLACK);
+
+	SelectObject(hdc,oldpen);
+	//goto _return;
   }
-#endif
 
 _return:
   SelectObject(hdc,oldBrush);
