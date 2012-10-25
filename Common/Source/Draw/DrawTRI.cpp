835273f3d22a78da12c98fe06d28374686d0aea4
Mateusz Pusz <mpusz@lk8000.it>
2012-10-25 19:45:17 +0200

    TRI gload circle scalling fixed

--- Common/Source/Draw/DrawTRI.cpp
+++ Common/Source/Draw/DrawTRI.cpp
@@ -25,7 +25,7 @@ void MapWindow::DrawAcceleration(HDC hDC, const RECT rc)
   POINT Pos;
   Pos.x = (rc.right - rc.left)/2 + (int)(DrawInfo.AccelY * ScaleX);
   Pos.y = (rc.bottom - rc.top)/2 - (int)((DrawInfo.AccelZ - 1) * ScaleY);
-  const double radius = 20 + (int)(DrawInfo.AccelX * ScaleZ);
+  const double radius = NIBLSCALE(15) + (int)(DrawInfo.AccelX * ScaleZ);
   
   const HPEN    oldPen   = (HPEN) SelectObject(hDC, GetStockObject(BLACK_PEN));
   const HBRUSH  oldBrush = (HBRUSH)SelectObject(hDC, LKBrush_Red);
