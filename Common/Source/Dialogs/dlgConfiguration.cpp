06f134ac94d87e81a1e9b7a48dcc5cf02e8108cf
Paolo Ventafridda <venta@lk8000.it>
2012-11-10 16:41:52 +0100

    Workaround strange crash in borderline situation. To replicate: use CAR mode, sim or fly, start moving, detect START. Reduce speed to 0, quickly config config setup aircraft setup, set GLIDER mode. Confirm and exit, you must do this BEFORE landing is detected by the fact speed is now 0. In main map wait for crash happening quite soon.

--- Common/Source/Dialogs/dlgConfiguration.cpp
+++ Common/Source/Dialogs/dlgConfiguration.cpp
@@ -4677,6 +4677,12 @@ void UpdateAircraftConfig(void){
         (wp->GetDataField()->GetAsInteger());
       requirerestart = true;
 	LKSW_ReloadProfileBitmaps=true;
+
+	time_in_flight=0;
+	time_on_ground=0;
+	CALCULATED_INFO.Flying=false;
+	CALCULATED_INFO.FreeFlying=false;
+
         if (ISPARAGLIDER) AATEnabled=TRUE; // NOT SURE THIS IS NEEDED ANYMORE. 
     }
   }
