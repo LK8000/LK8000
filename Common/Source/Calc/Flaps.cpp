35ac184bdbb4c7fb14a2ed26355de907467a8c88
Jakub Kubrynski <jkubrynski@gmail.com>
2012-12-31 00:12:09 +0100

    Fix speed for determining flaps in turn, should be corrected for bank angle. It behaves the same like for example minimum speed rising. 	modified:   ../Common/Source/Calc/Flaps.cpp

--- Common/Source/Calc/Flaps.cpp
+++ Common/Source/Calc/Flaps.cpp
@@ -11,15 +11,18 @@
 
 void Flaps(NMEA_INFO *Basic, DERIVED_INFO *Calculated)
 {	
-	double speed = 0.0;
 	if (GlidePolar::FlapsMass<=0) return; // avoid division by zero crashes
+	double speed = 0.0;
 	if (Basic->AirspeedAvailable) {
 		speed = (int)(Basic->IndicatedAirspeed);
 	} else {
 		speed = (int)(Calculated->IndicatedAirspeedEstimated);
 	}
 
-	LKASSERT(GlidePolar::FlapsMass!=0);
+	// correcting speed for calculated bank angle
+	if (cos(Calculated->BankAngle*DEG_TO_RAD)>0) 
+	speed = speed/sqrt(1/cos(Calculated->BankAngle*DEG_TO_RAD));
+
 	double massCorrectionFactor = sqrt(GlidePolar::GetAUW()/GlidePolar::FlapsMass);
 
 	for (int i=0;i<GlidePolar::FlapsPosCount-1;i++) {
