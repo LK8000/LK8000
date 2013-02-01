12233f70e910e368462c697bd9370582b18289ec
Paolo Ventafridda <venta@lk8000.it>
2013-02-01 10:43:21 +0100

    Draw Bearing line to destination also for M4 	modified:   TopView.cpp

--- Common/Source/Draw/Multimaps/TopView.cpp
+++ Common/Source/Draw/Multimaps/TopView.cpp
@@ -200,7 +200,7 @@ _nomoredeclutter:
 
   // 
   // Stuff for MAPTRK only (M1)
-  if (MapSpaceMode==MSM_MAPTRK) {
+  if (MapSpaceMode==MSM_MAPTRK||MapSpaceMode==MSM_VISUALGLIDE) {
 	if(IsMultimapTerrain() || IsMultimapTopology() ) {
 		if (FinalGlideTerrain && DerivedDrawInfo.TerrainValid)
 			DrawGlideThroughTerrain(hdc, DrawRect); 
