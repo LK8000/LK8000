ebc66c7d38406d5bdf037e4314400150520cec08
Mateusz Pusz <mpusz@lk8000.it>
2013-02-28 19:02:49 +0100

    Target dialog for AAT sector fixed
    Before the change a Target dialog for narrow AAT sector (like 90o - 180o) was
    centered at the center of the circle used to draw the sector and zoomed
    to view 2x its radius. Because of that sector was really hard to manage in
    AAT tasks. Now the same sector is centered on the screen and stretched on full
    screen.

--- Common/Source/Library/NavFunctions.cpp
+++ Common/Source/Library/NavFunctions.cpp
@@ -298,7 +298,7 @@ void xXY_Brg_Rng(double X_1, double Y_1, double X_2, double Y_2, double *Bearing
   double y = (X_2 - X_1);
   double x = (Y_2 - Y_1);
 
-  if (fabs(x)>0.00000001 && fabs(y)>0.00000001){
+  if (fabs(x)>0.00000001 || fabs(y)>0.00000001){
     Rad_Bearing = atan2(y, x);
   } else {
     Rad_Bearing = 0;
