dcc1868e94cdfa50a7d0e1fc7891b61c5eab1644
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2013-04-15 22:15:32 +0200

    new task file format don't work ifwaypoint don't come from cup waypoint file

--- Common/Source/SaveLoadTask/CTaskFileHelper.cpp
+++ Common/Source/SaveLoadTask/CTaskFileHelper.cpp
@@ -395,12 +395,12 @@ bool CTaskFileHelper::LoadTaskPoint(XMLNode node) {
     if (node) {
         unsigned long idx = MAXTASKPOINTS;
         GetAttribute(node, _T("idx"), idx);
-        LPCTSTR szCode = NULL;
-        GetAttribute(node, _T("code"), szCode);
-        if (idx >= MAXTASKPOINTS || szCode == NULL) {
+        LPCTSTR szName = NULL;
+        GetAttribute(node, _T("name"), szName);
+        if (idx >= MAXTASKPOINTS || szName == NULL) {
             return false; // invalide TaskPoint index
         }
-        std::map<std::wstring, size_t>::const_iterator it = mWayPointLoaded.find(szCode);
+        std::map<std::wstring, size_t>::const_iterator it = mWayPointLoaded.find(szName);
         if (it == mWayPointLoaded.end()) {
             return false; // non existing Waypoint
         }
@@ -440,13 +440,13 @@ bool CTaskFileHelper::LoadStartPoint(XMLNode node) {
     if (node) {
         unsigned long idx = MAXSTARTPOINTS;
         GetAttribute(node, _T("idx"), idx);
-        LPCTSTR szCode = NULL;
-        GetAttribute(node, _T("code"), szCode);
+        LPCTSTR szName = NULL;
+        GetAttribute(node, _T("name"), szName);
 
-        if (idx >= MAXSTARTPOINTS || szCode == NULL) {
+        if (idx >= MAXSTARTPOINTS || szName == NULL) {
             return false; // invalide TaskPoint index
         }
-        std::map<std::wstring, size_t>::const_iterator it = mWayPointLoaded.find(szCode);
+        std::map<std::wstring, size_t>::const_iterator it = mWayPointLoaded.find(szName);
         if (it == mWayPointLoaded.end()) {
             return false; // non existing Waypoint
         }
@@ -467,12 +467,6 @@ void CTaskFileHelper::LoadWayPoint(XMLNode node) {
     GetAttribute(node, _T("name"), szAttr);
     if (szAttr) {
         _tcscpy(newPoint.Name, szAttr);
-    }
-    if (_tcslen(newPoint.Name) == 0 && _tcslen(newPoint.Code) > 0) {
-        _tcscpy(newPoint.Name, newPoint.Code);
-    }
-    if (_tcslen(newPoint.Name) > 0 && _tcslen(newPoint.Code) == 0) {
-        _tcscpy(newPoint.Code, newPoint.Name);
     }
 
     GetAttribute(node, _T("latitude"), newPoint.Latitude);
@@ -506,7 +500,7 @@ void CTaskFileHelper::LoadWayPoint(XMLNode node) {
     }
     GetAttribute(node, _T("style"), newPoint.Style);
 
-    mWayPointLoaded[newPoint.Code] = FindOrAddWaypoint(&newPoint);
+    mWayPointLoaded[newPoint.Name] = FindOrAddWaypoint(&newPoint);
 }
 
 bool CTaskFileHelper::Save(const TCHAR* szFileName) {
@@ -818,7 +812,7 @@ bool CTaskFileHelper::SaveWayPointList(XMLNode node) {
 }
 
 bool CTaskFileHelper::SaveTaskPoint(XMLNode node, const TASK_POINT& TaskPt) {
-    SetAttribute(node, _T("code"), WayPointList[TaskPt.Index].Code);
+    SetAttribute(node, _T("name"), WayPointList[TaskPt.Index].Name);
 
     if (AATEnabled || DoOptimizeRoute()) {
 
@@ -861,21 +855,21 @@ bool CTaskFileHelper::SaveTaskPoint(XMLNode node, const TASK_POINT& TaskPt) {
 }
 
 bool CTaskFileHelper::SaveStartPoint(XMLNode node, const START_POINT& StartPt) {
-    SetAttribute(node, _T("code"), WayPointList[StartPt.Index].Code);
+    SetAttribute(node, _T("name"), (LPCTSTR)(WayPointList[StartPt.Index].Name));
 
     mWayPointToSave.insert(StartPt.Index);
     return true;
 }
 
 bool CTaskFileHelper::SaveWayPoint(XMLNode node, const WAYPOINT& WayPoint) {
-    SetAttribute(node, _T("code"), WayPoint.Code)
-    if (_tcslen(WayPoint.Name) > 0) {
-        SetAttribute(node, _T("name"), WayPoint.Name);
-    }
+    SetAttribute(node, _T("name"), WayPoint.Name);
     SetAttribute(node, _T("latitude"), WayPoint.Latitude);
     SetAttribute(node, _T("longitude"), WayPoint.Longitude);
     SetAttribute(node, _T("altitude"), WayPoint.Altitude);
     SetAttribute(node, _T("flags"), WayPoint.Flags);
+    if (_tcslen(WayPoint.Code) > 0) {
+        SetAttribute(node, _T("code"), (LPCTSTR)(WayPoint.Code));
+    }
     if (WayPoint.Comment && _tcslen(WayPoint.Comment) > 0) {
         SetAttribute(node, _T("comment"), WayPoint.Comment);
     }
