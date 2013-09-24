f16ed28749d08ba6d211c34da046491125fb8838
AlphaLima <U.Heynen@web.de>
2013-09-24 19:01:50 +0200

    fixed possible critical bug with string operation

--- Common/Source/Draw/MapWndProc.cpp
+++ Common/Source/Draw/MapWndProc.cpp
@@ -515,19 +515,14 @@ LRESULT CALLBACK MapWindow::MapWndProc (HWND hWnd, UINT uMsg, WPARAM wParam, LPA
 
             if( (mode.Is(Mode::MODE_PAN)) || (mode.Is(Mode::MODE_TARGET_PAN)) )
 		    {
-		   /* 	if ((AATEnabled ) && (CALCULATED_INFO.Flying ))
-		    	{
-		    		dlgTarget(PanTaskEdit);
-		    		PanTaskEdit = -1;
-		    	}
-		    	else*/
 		      {
 		        if(Task[PanTaskEdit].Index != RESWP_PANPOS)
 		        {
 		 	      RealActiceWaypoint = Task[PanTaskEdit].Index;
 		 	      memcpy( &WayPointList[RESWP_PANPOS], &WayPointList[ Task[PanTaskEdit].Index], sizeof(WAYPOINT) );
-		 	      _stprintf( WayPointList[RESWP_PANPOS].Name,TEXT("") );
-		 	      _stprintf( WayPointList[RESWP_PANPOS].Comment,TEXT("temporary Task point moved form %"),WayPointList[ Task[PanTaskEdit].Index]);
+		 	 	  _tcscpy(WayPointList[RESWP_PANPOS].Name, _T(" "));
+		 	      WayPointList[RESWP_PANPOS].Comment = _T(" ");
+
 
 		          Task[PanTaskEdit].Index =RESWP_PANPOS;
 				  RefreshMap();
