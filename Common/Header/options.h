ee15e7d082c3a6be0f00e6dea57439806ffed95f
Paolo Ventafridda <venta@lk8000.it>
2013-10-27 19:14:40 +0100

    options disabled ULLI_PRIVATE for master branch

--- Common/Header/options.h
+++ Common/Header/options.h
@@ -90,7 +90,7 @@
 #define SHOW_FOUND_WAYPOINT    // show best found waypoint on active keyboard
 #define SELECT_FOUND           // preselect best fit waypoint in waypoint list.
 
-#define ULLIS_PRIVATE_FEATURES  // Ulli's individual features
+//#define ULLIS_PRIVATE_FEATURES  // Ulli's individual features
 #ifdef ULLIS_PRIVATE_FEATURES
   #define BUTTONS_MS
   #define OWN_POS_MS
