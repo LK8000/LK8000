d33238a905551b0dc54414b54181ba7496352b5e
Alberto Realis-Luc <admin@alus.it>
2014-01-02 18:29:59 +0100

    Finishing up with GPX tasks

--- Common/Header/Sizes.h
+++ Common/Header/Sizes.h
@@ -65,8 +65,8 @@
 #define DESCRIPTION_SIZE 40			// Infobox descriptions aka LK DataOptions
 #define TITLE_SIZE 20				// Infobox titles  aka LK DataOptions
 #define FORMAT_SIZE 20
-// task points enlarged from 10 to 20 
-#define MAXTASKPOINTS 20
+// task points enlarged from 10 to 20 and then from 20 to 50
+#define MAXTASKPOINTS 50
 #define MAXSTARTPOINTS 20
 
 #define MAX_LOADSTRING 100
--- Common/Source/SaveLoadTask/LoadGpxTask.cpp
+++ Common/Source/SaveLoadTask/LoadGpxTask.cpp
@@ -15,219 +15,6 @@
 #include "utils/stringext.h"
 #include <string>
 
-
-
-typedef std::map<std::wstring,WAYPOINT> mapCode2Waypoint_t;
-
-class GpxObsZoneUpdater {
-public:
-
-	GpxObsZoneUpdater() :
-			mIdx(), mType(), mR1(), mA1(), mR2(), mA2(), mA12(), mLine() {
-	}
-
-	size_t mIdx;
-	int mType;
-	double mR1;
-	double mA1;
-	double mR2;
-	double mA2;
-	double mA12;
-	bool mLine;
-
-	void UpdateTask() {
-		if(mA1==180.0) {
-			if(mIdx==0) {
-				StartLine=0;
-				StartRadius=(DWORD)mR1;
-			} else if(mIdx==(size_t)getFinalWaypoint()) {
-				FinishLine=0;
-				FinishRadius=(DWORD)mR1;
-			} else {
-				Task[mIdx].AATType= CIRCLE;
-				Task[mIdx].AATCircleRadius=mR1;
-			}
-		} else {
-
-			switch(mType){
-				case 0: // - Fixed value,
-					if(mLine) {
-						StartupStore(_T("..Cup Task : \"Fixed\" LINE Turnpoint is not supported%s"),NEWLINE);
-						UpdateFixedLine();
-					} else {
-						UpdateFixedSector();
-					}
-					break;
-				case 1: // - Symmetrical,
-					if(mLine) {
-						StartupStore(_T("..Cup Task : \"Symmetrical\" LINE Turnpoint is not supported%s"),NEWLINE);
-						UpdateSymLine();
-					} else {
-						UpdateSymSector();
-					}
-					break;
-				case 2: // - To next point,
-					if(mLine) {
-						if(mIdx>0) {
-							StartupStore(_T("..Cup Task : \"To next point\" LINE Turnpoint is not supported%s"),NEWLINE);
-						}
-						UpdateToNextLine();
-					} else {
-						UpdateToNextSector();
-					}
-					break;
-				case 3: // - To previous point,
-					if(mLine) {
-						if(mIdx<(size_t)getFinalWaypoint()) {
-							StartupStore(_T("..Cup Task : \"To previous point\" LINE Turnpoint is not supported%s"),NEWLINE);
-						}
-						UpdateToPrevLine();
-					} else {
-						UpdateToPrevSector();
-					}
-					break;
-				case 4: // - To start point
-					if(mLine) {
-						StartupStore(_T("..Cup Task : \"To start point\" LINE Turnpoint is not supported%s"),NEWLINE);
-						UpdateToStartLine();
-					} else {
-						UpdateToStartSector();
-					}
-					break;
-			}
-		}
-	}
-
-	void UpdateFixedLine() {
-		if((mIdx==0)||(mIdx==(size_t)getFinalWaypoint())) {
-			UpdateSymLine();
-		} else {
-			StartupStore(_T("..Cup Task : LINE Turnpoint is only supported for Start or Finish%s"),NEWLINE);
-			mA1=90.0;
-			UpdateFixedSector();
-		}
-	}
-
-	void UpdateFixedSector() {
-		if(mIdx==0) {
-			StartLine=2;
-			StartRadius=(DWORD)mR1;
-		} else if(mIdx==(size_t)getFinalWaypoint()) {
-			FinishLine=2;
-			FinishRadius=(DWORD)mR1;
-		} else {
-			Task[mIdx].AATType= SECTOR;
-			Task[mIdx].AATSectorRadius=mR1;
-			Task[mIdx].AATStartRadial=mA12-180.0-(mA1);
-			Task[mIdx].AATFinishRadial=mA12-180.0+(mA1);
-		}
-	}
-
-	void UpdateSymLine() {
-		if(mIdx==0) {
-			StartLine=1;
-			StartRadius=(DWORD)mR1;
-		} else if(mIdx==(size_t)getFinalWaypoint()) {
-			FinishLine=1;
-			FinishRadius=(DWORD)mR1;
-		} else {
-			StartupStore(_T("..Cup Task : LINE Turnpoint is only supported for Start or Finish%s"),NEWLINE);
-			mA1=90.0;
-			UpdateSymSector();
-		}
-	}
-
-	void UpdateSymSector() {
-		if(mIdx==0) {
-			UpdateToNextSector();
-		} else if(mIdx==((size_t)getFinalWaypoint())) {
-			UpdateToPrevSector();
-		} else {
-			const WAYPOINT *CurrPt=TaskWayPoint(mIdx);
-			const WAYPOINT *PrevPt=TaskWayPoint(mIdx-1);
-			const WAYPOINT *NextPt=TaskWayPoint(mIdx+1);
-			double InB=0;
-			double OutB=0;
-			// bearing to prev
-			DistanceBearing(CurrPt->Latitude,CurrPt->Longitude,PrevPt->Latitude,PrevPt->Longitude,NULL,&InB);
-			// bearing to next
-			DistanceBearing(CurrPt->Latitude,CurrPt->Longitude,NextPt->Latitude,NextPt->Longitude,NULL,&OutB);
-			mA12=BiSector(InB,OutB);
-
-			UpdateFixedSector();
-		}
-	}
-
-	void UpdateToNextLine() {
-		if((mIdx==0)||(mIdx==(size_t)getFinalWaypoint())) {
-			UpdateSymLine();
-		} else {
-			StartupStore(_T("..Cup Task : LINE Turnpoint is only supported for Start or Finish%s"),NEWLINE);
-			mA1=90.0;
-			UpdateToNextSector();
-		}
-	}
-
-	void UpdateToNextSector() {
-		if(ValidTaskPoint(mIdx+1)) {
-			const WAYPOINT *CurrPt=TaskWayPoint(mIdx);
-			const WAYPOINT *NextPt=TaskWayPoint(mIdx+1);
-			// bearing to next
-			DistanceBearing(CurrPt->Latitude,CurrPt->Longitude,NextPt->Latitude,NextPt->Longitude,NULL,&mA12);
-
-			UpdateFixedSector();
-		}
-	}
-
-	void UpdateToPrevLine() {
-		if((mIdx==0)||(mIdx==(size_t)getFinalWaypoint())) {
-			UpdateSymLine();
-		} else {
-			StartupStore(_T("..Cup Task : LINE Turnpoint is only supported for Start or Finish%s"),NEWLINE);
-			mA1=90.0;
-			UpdateToPrevSector();
-		}
-	}
-
-	void UpdateToPrevSector() {
-		if(ValidTaskPoint(mIdx-1)) {
-			const WAYPOINT *CurrPt=TaskWayPoint(mIdx);
-			const WAYPOINT *PrevPt=TaskWayPoint(mIdx-1);
-			// bearing to prev
-			DistanceBearing(CurrPt->Latitude,CurrPt->Longitude,PrevPt->Latitude,PrevPt->Longitude,NULL,&mA12);
-
-			UpdateFixedSector();
-		}
-	}
-
-	void UpdateToStartLine() {
-		if((mIdx==0)||(mIdx==(size_t)getFinalWaypoint())) {
-			UpdateSymLine();
-		} else {
-			StartupStore(_T("..Cup Task : LINE Turnpoint is only supported for Start or Finish%s"),NEWLINE);
-			mA1=90.0;
-			UpdateToStartSector();
-		}
-	}
-
-	void UpdateToStartSector() {
-		if(mIdx>0) {
-			const WAYPOINT *CurrPt=TaskWayPoint(mIdx);
-			const WAYPOINT *StartPt=TaskWayPoint(0);
-			// bearing to prev
-			DistanceBearing(CurrPt->Latitude,CurrPt->Longitude,StartPt->Latitude,StartPt->Longitude,NULL,&mA12);
-
-			UpdateFixedSector();
-		}
-	}
-};
-
-
-
-//#define GetAttribute(node, name, val) FromString(node.getAttribute(name, 0), val)
-
-
-
 bool LoadGpxTask(LPCTSTR szFileName) {
 	LockTaskData();
 	StartupStore(_T(". LoadTask : <%s>%s"), szFileName, NEWLINE);
@@ -242,6 +29,7 @@ bool LoadGpxTask(LPCTSTR szFileName) {
 		if(nRead != size) {
 			fclose(stream);
 			free(buff);
+			UnlockTaskData();
 			return false;
 		}
 		fclose(stream);
@@ -252,109 +40,96 @@ bool LoadGpxTask(LPCTSTR szFileName) {
 		if(rootNode) {
 			if(rootNode.isEmpty()) {
 				free(szXML);
+				UnlockTaskData();
 				return false;
 			}
-			//LoadWayPointList(rootNode.getChildNode(_T("waypoints"), 0));
-			//if (!LoadTaskPointList(rootNode.getChildNode(_T("taskpoints"), 0))) {
-			//	free(szXML);
-			//	return false;
-			//}
-			//if(!LoadStartPointList(rootNode.getChildNode(_T("startpoints"), 0))) {
-			//	free(szXML);
-			//	return false;
-			//}
-
 			//TODO: here we load just the first route may be there are others routes in the GPX file...
 			XMLNode routeNode=rootNode.getChildNode(TEXT("rte"));
 			if(routeNode.isEmpty()) { //ERROR no route found in GPX file
 				free(szXML);
+				UnlockTaskData();
 				return false;
 			}
 			int numWPnodes=routeNode.nChildNode();
 			if(numWPnodes<1) { //ERROR no waypoints found in route in GPX file
 				free(szXML);
+				UnlockTaskData();
 				return false;
 			}
-
-			const TCHAR* text=NULL;
-			//LPCTSTR szAttr = NULL;
-
-
-			XMLNode WPnode;
+			LPCTSTR dataStr=NULL;
+			double lat;
+			XMLNode WPnode,detailNode;
 			WAYPOINT newPoint;
 			for(int i=0,idx=0;i<numWPnodes;i++) {
-				memset(&newPoint, 0, sizeof (newPoint));
-
-				/*
-				GetAttribute(node, _T("code"), szAttr);
-				if(szAttr) {
-					_tcscpy(newPoint.Code, szAttr);
-				}
-				GetAttribute(node, _T("flags"), newPoint.Flags);
-				GetAttribute(node, _T("comment"), szAttr);
-				if(szAttr) {
-					newPoint.Comment = (TCHAR*) malloc((_tcslen(szAttr) + 1) * sizeof (TCHAR));
-					if (newPoint.Comment) {
-						_tcscpy(newPoint.Comment, szAttr);
-					}
-				}
-				GetAttribute(node, _T("details"), szAttr);
-				if(szAttr) {
-					newPoint.Details = (TCHAR*) malloc((_tcslen(szAttr) + 1) * sizeof (TCHAR));
-					if(newPoint.Details) {
-						_tcscpy(newPoint.Details, szAttr);
-					}
-				}
-				GetAttribute(node, _T("format"), newPoint.Format);
-				GetAttribute(node, _T("freq"), szAttr);
-				if(szAttr) {
-					_tcscpy(newPoint.Freq, szAttr);
-				}
-				GetAttribute(node, _T("runwayLen"), newPoint.RunwayLen);
-				GetAttribute(node, _T("runwayDir"), newPoint.RunwayDir);
-				GetAttribute(node, _T("country"), szAttr);
-				if(szAttr) {
-					_tcscpy(newPoint.Country, szAttr);
-				}
-				GetAttribute(node, _T("style"), newPoint.Style);
-				*/
-
-
-
-
-
-				/////////////////////
-
-
 				WPnode=routeNode.getChildNode(i);
 				if(_tcscmp(WPnode.getName(),TEXT("rtept"))==0) {
-					text=WPnode.getAttribute(TEXT("lat"));
-					if(text==NULL) break; //WP without latitude skip it
-					newPoint.Latitude=_tcstod(text,NULL);
-
-
-
-					text=WPnode.getAttribute(TEXT("lon"));
-					if(text==NULL) break; //WP without longitude skip it
-					newPoint.Longitude=_tcstod(text,NULL);
-					idx++;
-					text=WPnode.getAttribute(TEXT("ele"));
-					if(text!=NULL) newPoint.Altitude=_tcstod(text,NULL);
-					else newPoint.Altitude=0; //WP altitude missing we put it to 0
-					text=WPnode.getAttribute(TEXT("name"));
-					if(text!=NULL) {
-						_tcscpy(newPoint.Name, text);
-						//East longitudes are positive according to the GPX standard
-					} else {
-						//wp with no name
-						//newPoint.Name=_tcscpy(newPoint.Name,_T("Unnamed WayPoint"));
+					dataStr=WPnode.getAttribute(TEXT("lat"));
+					if(!dataStr) break; //WP without latitude skip it
+					lat=_tcstod(dataStr,NULL);
+					dataStr=WPnode.getAttribute(TEXT("lon"));
+					if(!dataStr) break; //WP without longitude skip it
+					if(idx>=MAXTASKPOINTS) { //ERROR: too many waypoints
+						free(szXML);
+						ClearTask();
+						UnlockTaskData();
+						return false;
 					}
-					Task[idx].Index = FindOrAddWaypoint(&newPoint);
-				}
-			}
+					memset(&newPoint, 0, sizeof (newPoint));
+					newPoint.Latitude=lat;
+					newPoint.Longitude=_tcstod(dataStr,NULL);
+					detailNode=WPnode.getChildNode(TEXT("ele"),0);
+					if(detailNode) {
+						dataStr=detailNode.getText(0);
+						if(dataStr) newPoint.Altitude=_tcstod(dataStr,NULL);
+					}
+					detailNode=WPnode.getChildNode(TEXT("name"),0);
+					if(detailNode) {
+						dataStr=detailNode.getText(0);
+						if(dataStr) _tcscpy(newPoint.Name, dataStr);
+					}
+					detailNode=WPnode.getChildNode(TEXT("cmt"),0);
+					if(detailNode) {
+						dataStr=detailNode.getText(0);
+						if(dataStr) {
+							newPoint.Comment = (TCHAR*) malloc((_tcslen(dataStr)+1)*sizeof(TCHAR));
+							if(newPoint.Comment) _tcscpy(newPoint.Comment, dataStr);
+						}
+					}
+					detailNode=WPnode.getChildNode(TEXT("desc"),0);
+					if(detailNode) {
+						dataStr=detailNode.getText(0);
+						if(dataStr) {
+							newPoint.Details = (TCHAR*) malloc((_tcslen(dataStr)+1)*sizeof(TCHAR));
+							if(newPoint.Details) _tcscpy(newPoint.Details, dataStr);
+						}
+					}
+					/*TODO: other possible data to get somehow from the GPX file:
+					newPoint.Code
+					newPoint.Flags
+					newPoint.Format
+					newPoint.Freq
+					newPoint.RunwayLen
+					newPoint.RunwayDir
+					newPoint.Country
+					newPoint.Style); */
+					Task[idx++].Index=FindOrAddWaypoint(&newPoint);
+				} //if(rtept)
+			} //for(each rtept)
 		} //if(rootNode)
 		free(szXML);
 	} //if(stream)
+
+	//Set options for GA aircraft
+	AutoAdvance=1; //Auto
+	AATEnabled=false;
+	PGOptimizeRoute=false;
+	StartLine=2; //Sector
+	StartRadius=1500;
+	SectorType=LINE;
+	SectorRadius=2000;
+	FinishLine=0; //Circle
+	FinishRadius=1000;
+	StartHeightRef=1; //ASL
 	RefreshTask();
 	TaskModified = false;
 	TargetModified = false;
