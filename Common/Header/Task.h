LK8000/LK8000

commit 14c9a655a322867ee7f8643a1548078100dc22e4

Change task Requiered Altitude Calculation : if next Tp Requiered Altitude is less than current tp altitude, use current tp altitude for requiered altitude...  - AAT Task : altitude of AAT Target point is used if upper center altitude, otherwise, center altitude is used.  - PG optimized : always use Optimized point altitude.

Common/Header/Task.h
@@ -54,6 +54,7 @@ typedef struct _TASK_POINT
   double AATTargetOffsetRadial;
   double AATTargetLat;
   double AATTargetLon;
+  double AATTargetAltitude;
   POINT	 Target;
   bool   AATTargetLocked;
   bool	 OutCircle;

Common/Header/Waypointparser.h
@@ -23,6 +23,8 @@ void CloseWayPoints(void);
 int dlgWaypointOutOfTerrain(TCHAR *Message);
 void WaypointWriteFiles(void);
 void WaypointAltitudeFromTerrain(WAYPOINT* wpt);
+double AltitudeFromTerrain(double Lat, double Lon);
+void UpdateTargetAltitude(int i);
 WAYPOINT* GrowWaypointList();
 int FindMatchingWaypoint(WAYPOINT *waypoint);
 void InitWayPointCalc(void); 

Common/Source/Archive/Calculations.cpp
@@ -206,7 +206,14 @@ double FAIFinishHeight(NMEA_INFO *Basic, DERIVED_INFO *Calculated, int twp) {
   double wp_alt;
 
   if(ValidTaskPoint(twp)) {
-    wp_alt = WayPointList[Task[twp].Index].Altitude;
+      // for AAT Task, use AATTarget target point Altitude for Turnpoint and TP center for finish.
+      // for PG Optimized task, always use optimized point altitude.
+      if( (AATEnable && twp!=FinalWayPoint) || DoOptimizeRoute() ) {
+          wp_alt = Task[twp].AATTargetAltitude;
+      } else {
+        wp_alt = WayPointList[Task[twp].Index].Altitude;
+      }
+    
     if (!CheckSafetyAltitudeApplies(Task[twp].Index)) safetyaltitudearrival=0;
   } else {
     #if TESTBENCH

Common/Source/Calc/Task/AATDistance.cpp
@@ -10,6 +10,7 @@
 #include "AATDistance.h"
 #include "CalcTask.h"
 #include "Airspace.h"
+#include "Waypointparser.h"
 
 
 #define DISTANCETHRESHOLD 500
@@ -166,6 +167,8 @@ void AATDistance::ShiftTargetOutside(double longitude, double latitude,
                           bearing, 100.0,
                           &Task[taskwaypoint].AATTargetLat,
                           &Task[taskwaypoint].AATTargetLon);
+    
+    UpdateTargetAltitude(taskwaypoint);
     TargetModified = true;
 
   }
@@ -211,6 +214,8 @@ void AATDistance::ShiftTargetFromInFront(double longitude, double latitude,
                         &Task[taskwaypoint].AATTargetLat,
                         &Task[taskwaypoint].AATTargetLon);
   // JMW, distance here was 100m, now changed to speed * 2
+
+  UpdateTargetAltitude(taskwaypoint);
 
   TargetModified = true;
   CalculateAATIsoLines();
@@ -357,6 +362,8 @@ void AATDistance::ShiftTargetFromBehind(double longitude, double latitude,
                           &Task[taskwaypoint].AATTargetLat,
                           &Task[taskwaypoint].AATTargetLon);
 
+    UpdateTargetAltitude(taskwaypoint);
+    
     Task[taskwaypoint].AATTargetOffsetRadius =
       FindInsideAATSectorRange(latitude,
                                longitude,

Common/Source/Calc/Task/OptimizedTargetPos.cpp
@@ -25,14 +25,15 @@ void CalculateOptimizedTargetPos(NMEA_INFO *Basic, DERIVED_INFO *Calculated) {
 
 	for(size_t i=0; i<gPGTask.Count(); ++i) {
 		gPGTask.getOptimized(i, Task[i].AATTargetLat, Task[i].AATTargetLon);
+    
+        UpdateTargetAltitude(i);
 	}
 		
 	int stdwp=Task[ActiveWayPoint].Index;
 
 	WayPointList[RESWP_OPTIMIZED].Latitude = Task[ActiveWayPoint].AATTargetLat;
 	WayPointList[RESWP_OPTIMIZED].Longitude = Task[ActiveWayPoint].AATTargetLon;
-	WayPointList[RESWP_OPTIMIZED].Altitude = WayPointList[stdwp].Altitude;
-	WaypointAltitudeFromTerrain(&WayPointList[RESWP_OPTIMIZED]);
+	WayPointList[RESWP_OPTIMIZED].Altitude = Task[ActiveWayPoint].AATTargetAltitude;
 
 	wsprintf(WayPointList[RESWP_OPTIMIZED].Name, _T("!%s"),WayPointList[stdwp].Name);
 
@@ -56,6 +57,7 @@ void ClearOptimizedTargetPos() {
 	for(int i = 0; ValidWayPoint(Task[i].Index); ++i) {
 		Task[i].AATTargetLat = WayPointList[Task[i].Index].Latitude;
 		Task[i].AATTargetLon = WayPointList[Task[i].Index].Longitude;
+        Task[i].AATTargetAltitude = WayPointList[Task[i].Index].Altitude;
 		Task[i].AATTargetLocked = false;
 	}
 

Common/Source/Calc/Task/RefreshTask/CalculateAATTaskSectors.cpp
@@ -7,6 +7,7 @@
 */
 
 #include "externs.h"
+#include "Waypointparser.h"
 
 
 extern bool TargetDialogOpen;
@@ -148,6 +149,8 @@ void CalculateAATTaskSectors()
                                &Task[i].AATTargetLat,
                                &Task[i].AATTargetLon);
 
+        UpdateTargetAltitude(i);
+
         TargetModified = true;
 
         // }
@@ -160,6 +163,8 @@ void CalculateAATTaskSectors()
                                targetrange,
                                &Task[i].AATTargetLat,
                                &Task[i].AATTargetLon);
+        
+        UpdateTargetAltitude(i);
         TargetModified = true;
         
       }

Common/Source/Calc/Task/TaskAltitudeRequired.cpp
@@ -28,14 +28,15 @@ bool TaskAltitudeRequired(NMEA_INFO *Basic, DERIVED_INFO *Calculated,
 
   bool isfinal=true;
   LegAltitude = 0;
-  double TotalAltitude = 0;
   *TotalTime = 0; *TotalDistance = 0;
   *ifinal = 0;
 
   LockTaskData();
 
-  double height_above_finish = FAIFinishHeight(Basic, Calculated, 0)-
-    FAIFinishHeight(Basic, Calculated, -1);
+  double heightFinal = FAIFinishHeight(Basic, Calculated, -1);
+  double height_above_finish = FAIFinishHeight(Basic, Calculated, 0) - heightFinal;
+  
+  double TotalAltitude = heightFinal;
 
   for(i=MAXTASKPOINTS-2;i>=0;i--) {
 
@@ -50,7 +51,9 @@ bool TaskAltitudeRequired(NMEA_INFO *Basic, DERIVED_INFO *Calculated,
     if (AATEnabled) {
       w1lat = Task[i].AATTargetLat;
       w1lon = Task[i].AATTargetLon;
-      if (!isfinal) {
+      
+      // also use optimized finish point for PG optimized task.
+      if (!isfinal || DoOptimizeRoute()) {
         w0lat = Task[i+1].AATTargetLat;
         w0lon = Task[i+1].AATTargetLon;
       }
@@ -81,6 +84,13 @@ bool TaskAltitudeRequired(NMEA_INFO *Basic, DERIVED_INFO *Calculated,
 
     TotalAltitude += LegAltitude;
 
+    // if required altitude is less than previous turpoint altitude,
+    //   use previous turn point altitude
+    double w1Alt = FAIFinishHeight(Basic, Calculated, i);
+    if(TotalAltitude < w1Alt) {
+        TotalAltitude = w1Alt;
+    }
+
     if (LegTime<0) {
 		retval = false;
 		goto OnExit;
@@ -100,8 +110,6 @@ bool TaskAltitudeRequired(NMEA_INFO *Basic, DERIVED_INFO *Calculated,
     retval = false;
     goto OnExit;
   }
-
-  TotalAltitude += FAIFinishHeight(Basic, Calculated, -1);
 
   if (!ValidTaskPoint(*ifinal)) {
     Calculated->TaskAltitudeRequiredFromStart = TotalAltitude;

Common/Source/Calc/Task/TaskStatistic.cpp
@@ -198,6 +198,9 @@ void TaskStatistics(NMEA_INFO *Basic, DERIVED_INFO *Calculated,
   double final_height = FAIFinishHeight(Basic, Calculated, -1);
   double total_energy_height = Calculated->NavAltitude + Calculated->EnergyHeight;
   double height_above_finish = total_energy_height - final_height;
+
+  TaskAltitudeRequired  = final_height;
+  TaskAltitudeRequired0 = final_height;
   
   // Now add it for remaining waypoints
   int task_index= FinalWayPoint;
@@ -258,6 +261,16 @@ void TaskStatistics(NMEA_INFO *Basic, DERIVED_INFO *Calculated,
 
       TaskAltitudeRequired += LegAltitude;
       TaskAltitudeRequired0 += LegAltitude0;
+
+      // if required altitude is less than previous turpoint altitude,
+      //   use previous turn point altitude
+      double w0Alt = FAIFinishHeight(Basic, Calculated, task_index-1);
+      if(TaskAltitudeRequired < w0Alt) {
+          TaskAltitudeRequired = w0Alt;
+      }
+      if(TaskAltitudeRequired0 < w0Alt) {
+          TaskAltitudeRequired0 = w0Alt;
+      }
       
       Calculated->TaskDistanceToGo += NextLegDistance;
       Calculated->TaskTimeToGo += this_LegTimeToGo;      
@@ -379,9 +392,7 @@ void TaskStatistics(NMEA_INFO *Basic, DERIVED_INFO *Calculated,
 
 
   
-  Calculated->TaskAltitudeRequired = TaskAltitudeRequired + final_height;
-  
-  TaskAltitudeRequired0 += final_height;
+  Calculated->TaskAltitudeRequired = TaskAltitudeRequired;
   
   Calculated->TaskAltitudeDifference = total_energy_height - Calculated->TaskAltitudeRequired; 
   Calculated->TaskAltitudeDifference0 = total_energy_height - TaskAltitudeRequired0;

commit 950ab9b75ddf65d18ade0cc288d35fc06c448428

fix : Old Task file compatibility    old task file loading are broken by commit : 14c9a655a322867ee7f8643a1548078100dc22e4

Common/Header/Task.h
@@ -54,10 +54,12 @@ typedef struct _TASK_POINT
   double AATTargetOffsetRadial;
   double AATTargetLat;
   double AATTargetLon;
-  double AATTargetAltitude;
   POINT	 Target;
   bool   AATTargetLocked;
   bool	 OutCircle;
+  double AATTargetAltitude;
+  // always add new members at the End of this struct
+  //   needed for compatibility with old task file.
 }TASK_POINT;
 
 typedef TASK_POINT Task_t[MAXTASKPOINTS +1];

Common/Source/Dialogs/Task/LoadTaskWaypoints.cpp
@@ -36,31 +36,3 @@ int FindOrAddWaypoint(WAYPOINT *read_waypoint) {
   return waypoint_index;
 }
 
-
-bool LoadTaskWaypoints(HANDLE hFile) {
-  WAYPOINT read_waypoint;
-  DWORD dwBytesRead;
-
-  int i;
-  for(i=0;i<MAXTASKPOINTS;i++) {
-    if(!ReadFile(hFile,&read_waypoint,sizeof(read_waypoint),&dwBytesRead, (OVERLAPPED *)NULL)
-       || (dwBytesRead<sizeof(read_waypoint))) {
-      return false;
-    }
-    if (Task[i].Index != -1) { //  091213 CHECK do not load reserved WP
-      Task[i].Index = FindOrAddWaypoint(&read_waypoint);
-    }
-  }
-  for(i=0;i<MAXSTARTPOINTS;i++) {
-    if(!ReadFile(hFile,&read_waypoint,sizeof(read_waypoint),&dwBytesRead, (OVERLAPPED *)NULL)
-       || (dwBytesRead<sizeof(read_waypoint))) {
-      return false;
-    }
-    if (StartPoints[i].Index != -1) {
-      StartPoints[i].Index = FindOrAddWaypoint(&read_waypoint);
-    }
-  }
-  // managed to load everything
-  return true;
-}
-

Common/Source/SaveLoadTask/LoadNewTask.cpp
@@ -8,10 +8,78 @@
 
 #include "externs.h"
 #include "dlgTools.h"
+#include "Waypointparser.h"
 
 
-bool LoadTaskWaypoints(HANDLE hFile);
 extern bool FullResetAsked;
+
+
+/*******************************************************/
+/* this exist only for compatibility with OLD tsk file */
+
+typedef struct _OLD_TASK_POINT
+{
+  int Index;
+  double InBound;
+  double OutBound;
+  double Bisector;
+  double Leg;
+  double SectorStartLat;
+  double SectorStartLon;
+  double SectorEndLat;
+  double SectorEndLon;
+  POINT	 Start;
+  POINT	 End;		
+  int	 AATType;
+  double AATCircleRadius;
+  double AATSectorRadius;
+  double AATStartRadial;
+  double AATFinishRadial;
+  double AATStartLat;
+  double AATStartLon;
+  double AATFinishLat;
+  double AATFinishLon;
+  POINT	 AATStart;
+  POINT	 AATFinish;
+  double AATTargetOffsetRadius;
+  double AATTargetOffsetRadial;
+  double AATTargetLat;
+  double AATTargetLon;
+  POINT	 Target;
+  bool   AATTargetLocked;
+}OLD_TASK_POINT;
+
+#define OLD_MAXTASKPOINTS 20
+
+/*******************************************************/
+
+bool LoadTaskWaypoints(HANDLE hFile) {
+  WAYPOINT read_waypoint;
+  DWORD dwBytesRead;
+
+  int i;
+  for(i=0;i<OLD_MAXTASKPOINTS;i++) {
+    if(!ReadFile(hFile,&read_waypoint,sizeof(read_waypoint),&dwBytesRead, (OVERLAPPED *)NULL)
+       || (dwBytesRead<sizeof(read_waypoint))) {
+      return false;
+    }
+    if (Task[i].Index != -1) { //  091213 CHECK do not load reserved WP
+      Task[i].Index = FindOrAddWaypoint(&read_waypoint);
+    }
+  }
+  for(i=0;i<MAXSTARTPOINTS;i++) {
+    if(!ReadFile(hFile,&read_waypoint,sizeof(read_waypoint),&dwBytesRead, (OVERLAPPED *)NULL)
+       || (dwBytesRead<sizeof(read_waypoint))) {
+      return false;
+    }
+    if (StartPoints[i].Index != -1) {
+      StartPoints[i].Index = FindOrAddWaypoint(&read_waypoint);
+    }
+  }
+  // managed to load everything
+  return true;
+}
+
 
 // Loads a new task from scratch.
 // This is called on startup by the even manager because in DEFAULT MENU we have a GCE event
@@ -81,21 +149,23 @@ void LoadNewTask(LPCTSTR szFileName)
 		goto goEnd;
 	}
 
-      for(i=0;i<MAXTASKPOINTS;i++)
+      for(i=0;i<OLD_MAXTASKPOINTS;i++)
         {
-          if(!ReadFile(hFile,&Temp,sizeof(TASK_POINT),&dwBytesRead, (OVERLAPPED *)NULL))
+          if(!ReadFile(hFile,&Temp,sizeof(OLD_TASK_POINT),&dwBytesRead, (OVERLAPPED *)NULL))
             {
               TaskInvalid = true;
               break;
             }
-	  memcpy(&Task[i],&Temp, sizeof(TASK_POINT));
+          if(i < MAXTASKPOINTS) {
+            memcpy(&Task[i],&Temp, sizeof(OLD_TASK_POINT));
 
-          if( !ValidNotResWayPoint(Temp.Index) && (Temp.Index != -1) ) { // 091213
-            // Task is only invalid here if the index is out of range
-            // of the waypoints and not equal to -1.
-            // (Because -1 indicates a null task item)
-	        WaypointInvalid = true; 
-	  }
+            if( !ValidNotResWayPoint(Temp.Index) && (Temp.Index != -1) ) { // 091213
+                // Task is only invalid here if the index is out of range
+                // of the waypoints and not equal to -1.
+                // (Because -1 indicates a null task item)
+                WaypointInvalid = true; 
+        	}
+          }
         }
 
       if (!TaskInvalid) {