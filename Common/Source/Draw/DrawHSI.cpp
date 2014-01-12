18d72bb96270ec3685d6be537ea47d06a86c2f81
Alberto Realis-Luc <admin@alus.it>
2014-01-12 15:21:36 +0100

    Calculations optimization

--- Common/Source/Draw/DrawHSI.cpp
+++ Common/Source/Draw/DrawHSI.cpp
@@ -23,31 +23,26 @@
 #include "RGB.h"
 #include "DoInits.h"
 
-struct compassMark {
-	short extX, extY; //coordinates external point
-	short intX, intY; //coordinates internal point
-};
-
 void MapWindow::DrawHSI(HDC hDC, const RECT rc) {
-	static short top = (((rc.bottom-BottomSize-(rc.top + TOPLIMITER)-BOTTOMLIMITER)/PANELROWS)+rc.top+TOPLIMITER)-(rc.top + TOPLIMITER);
-	static short centerX=(rc.right-rc.left)/2;
-	static short centerY=((rc.bottom-BottomSize-top)/2)+top-NIBLSCALE(12);
-	static int radius = NIBLSCALE(80); // gauge size radius
-	static int innerradius = radius - NIBLSCALE(10);
-	static int labelsRadius = radius -NIBLSCALE(20);
-	static int smallMarkRadius = radius - NIBLSCALE(6);
-	static int cdiRadius = radius - NIBLSCALE(35);
-	static int cdiFullScale = radius -NIBLSCALE(30);
+	static short centerX;
+	static short centerY;
+	static short radius; //HSI gauge size radius
+	static short innerradius;
+	static short labelsRadius;
+	static short smallMarkRadius;
+	static short cdiRadius;
+	static short cdiFullScale;
+	static POINT fusA,fusB,winA,winB,taiA,taiB; //coordinates airplane symbol
 	//TCHAR Buffer[LKSIZEBUFFERVALUE];
-	static compassMark compassMarks[72][10];
-	static POINT hdgMark[4]={{centerX,centerY-radius+NIBLSCALE(3)},{centerX-NIBLSCALE(2),centerY-radius-NIBLSCALE(4)},{centerX+NIBLSCALE(2),centerY-radius-NIBLSCALE(4)},{centerX,centerY-radius+NIBLSCALE(3)}};
 
-	//From Wikipedia about Course Deviation Indicator:
-	//When used with a GPS it shows actual distance left or right of the programmed courseline.
-	//Sensitivity is usually programmable or automatically switched, but 5 nautical miles (9.3 km) deviation at full scale is typical for en route operations.
-	//Approach and terminal operations have a higher sensitivity up to frequently .3 nautical miles (0.56 km) at full scale.
-	static const int bigCDIscale=9260; //5 NM
-	static const int smallCDIscale=557; //0.3 NM
+	static struct { //Compass rose marks coordinates matrix: using short's to use less memory
+		short extX, extY; //coordinates external mark point
+		short intX, intY; //coordinates internal mark point
+	} compassMarks[72][10]; //72 compass marks (one every 5 degrees), 10 possible cases
+	static POINT hdgMark[4]; //Coordinates of heading marker (red triangle on the top of compass rose)
+
+	static const int bigCDIscale=9260; //Large Course Deviation Indicator scale: 5 nautical miles (9260 m)
+	static const int smallCDIscale=557; //Narrow (zoomed) CDI scale: 0.3 nautical miles (557 m)
 
 	static const TCHAR* label[]= { //labels of the compass rose
 			TEXT("N"),
@@ -68,7 +63,7 @@ void MapWindow::DrawHSI(HDC hDC, const RECT rc) {
 
 	if(DoInit[MDI_DRAWHSI]) {
 		//all the sizes must be recalculated in case of screen resolution change:
-		top=(((rc.bottom-BottomSize-(rc.top + TOPLIMITER)-BOTTOMLIMITER)/PANELROWS)+rc.top+TOPLIMITER)-(rc.top + TOPLIMITER);
+		short top=(((rc.bottom-BottomSize-(rc.top + TOPLIMITER)-BOTTOMLIMITER)/PANELROWS)+rc.top+TOPLIMITER)-(rc.top + TOPLIMITER);
 		centerX=(rc.right-rc.left)/2;
 		centerY=((rc.bottom-BottomSize-top)/2)+top-NIBLSCALE(12);
 		radius = NIBLSCALE(80);
@@ -80,10 +75,10 @@ void MapWindow::DrawHSI(HDC hDC, const RECT rc) {
 
 		//For the positions of all 72 compass rose marks there are 10 possible cases to be pre-calculated:
 		for(int alpha=0;alpha<10;alpha++) for(int i=0, deg=0, isBig=1; i<72; i++, deg+=5, isBig=!isBig) {
-			compassMarks[i][alpha].extX=centerX+(short)(radius*fastsine(deg+alpha));
-			compassMarks[i][alpha].extY=centerY-(short)(radius*fastcosine(deg+alpha));
-			compassMarks[i][alpha].intX=centerX+(short)((isBig?innerradius:smallMarkRadius)*fastsine(deg+alpha));
-			compassMarks[i][alpha].intY=centerY-(short)((isBig?innerradius:smallMarkRadius)*fastcosine(deg+alpha));
+			compassMarks[i][alpha].extX=centerX+radius*fastsine(deg+alpha);
+			compassMarks[i][alpha].extY=centerY-radius*fastcosine(deg+alpha);
+			compassMarks[i][alpha].intX=centerX+(isBig?innerradius:smallMarkRadius)*fastsine(deg+alpha);
+			compassMarks[i][alpha].intY=centerY-(isBig?innerradius:smallMarkRadius)*fastcosine(deg+alpha);
 		}
 
 		//Initialize the true heading direction marker (triangle)
@@ -95,6 +90,20 @@ void MapWindow::DrawHSI(HDC hDC, const RECT rc) {
 		hdgMark[2].y=hdgMark[1].y;
 		hdgMark[3].x=hdgMark[0].x;
 		hdgMark[3].y=hdgMark[0].y;
+
+		//Initialize coordinates for airplane symbol
+		fusA.x=centerX;
+		fusA.y=centerY-NIBLSCALE(7);
+		fusB.x=centerX;
+		fusB.y=centerY+NIBLSCALE(7);
+		winA.x=centerX-NIBLSCALE(8);
+		winA.y=centerY-NIBLSCALE(3);
+		winB.x=centerX+NIBLSCALE(8);
+		winB.y=centerY-NIBLSCALE(3);
+		taiA.x=centerX-NIBLSCALE(3);
+		taiA.y=centerY+NIBLSCALE(4);
+		taiB.x=centerX+NIBLSCALE(3);
+		taiB.y=centerY+NIBLSCALE(4);
 
 		DoInit[MDI_DRAWHSI]=false;
 	}
@@ -122,11 +131,12 @@ void MapWindow::DrawHSI(HDC hDC, const RECT rc) {
 	SelectObject(hDC, LK8TitleFont);
 	for(int i=0;i<12;i++) {
 		int deg=i*30+angle;
-		int x=centerX+(int)(labelsRadius*fastsine(deg));
-		int y=centerY-(int)(labelsRadius*fastcosine(deg));
+		int x=centerX+labelsRadius*fastsine(deg);
+		int y=centerY-labelsRadius*fastcosine(deg);
 		LKWriteText(hDC,label[i],x,y,0, WTMODE_NORMAL,WTALIGN_CENTER,RGB_WHITE,false);
 	}
 
+	//Draw true heading mark on the top of the compass rose
 	SelectObject(hDC, LKPen_Red_N1);
 	SelectObject(hDC, LKBrush_Red);
 	Polygon(hDC,hdgMark,4);
@@ -138,35 +148,43 @@ void MapWindow::DrawHSI(HDC hDC, const RECT rc) {
 			//DerivedDrawInfo.LegActualTrueCourse
 
 			double rotation=DerivedDrawInfo.LegActualTrueCourse-DrawInfo.TrackBearing;
-			POINT up, down;
-
-			//TODO: optimize calculations...
+			double sin=fastsine(rotation);
+			double cos=fastcosine(rotation);
 
 			//This is the upper side of the course direction arrow
-			up.x=centerX+(long)(labelsRadius*fastsine(rotation));
-			up.y=centerY-(long)(labelsRadius*fastcosine(rotation));
-			down.x=centerX+(long)(cdiRadius*fastsine(rotation));
-			down.y=centerY-(long)(cdiRadius*fastcosine(rotation));
+			long labelsRadiusXsin=labelsRadius*sin;
+			long labelsRadiusXcos=labelsRadius*cos;
+			long cdiRadiusXsin=cdiRadius*sin;
+			long cdiRadiusXcos=cdiRadius*cos;
+			POINT up, down;
+			up.x=centerX+labelsRadiusXsin;
+			up.y=centerY-labelsRadiusXcos;
+			down.x=centerX+cdiRadiusXsin;
+			down.y=centerY-cdiRadiusXcos;
 			_DrawLine(hDC, PS_ENDCAP_SQUARE, NIBLSCALE(2),up,down,RGB_GREEN,rc);
-			long topX=centerX+(long)(innerradius*fastsine(rotation));
-			long topY=centerY-(long)(innerradius*fastcosine(rotation));
-			long rightX=centerX+(long)(labelsRadius*fastsine(rotation))+(long)(NIBLSCALE(4)*fastcosine(rotation));
-			long rightY=centerY-(long)(labelsRadius*fastcosine(rotation))+(long)(NIBLSCALE(4)*fastsine(rotation));
-			long leftX=centerX+(long)(labelsRadius*fastsine(rotation))-(long)(NIBLSCALE(4)*fastcosine(rotation));
-			long leftY=centerY-(long)(labelsRadius*fastcosine(rotation))-(long)(NIBLSCALE(4)*fastsine(rotation));
-			POINT triangle[4] = {{topX,topY},{rightX,rightY},{leftX,leftY},{topX,topY}};
+			long innerradiusXsin=innerradius*sin;
+			long innerradiusXcos=innerradius*cos;
+			long arrowXsin=NIBLSCALE(4)*sin;
+			long arrowXcos=NIBLSCALE(4)*cos;
+			long topX=centerX+innerradiusXsin;
+			long topY=centerY-innerradiusXcos;
+			POINT triangle[4] = {
+					{topX,topY}, //top
+					{centerX+labelsRadiusXsin+arrowXcos,centerY-labelsRadiusXcos+arrowXsin}, //right
+					{centerX+labelsRadiusXsin-arrowXcos,centerY-labelsRadiusXcos-arrowXsin}, //left
+					{topX,topY}}; //top
 			SelectObject(hDC, LKPen_Green_N1);
 			SelectObject(hDC, LKBrush_Green);
 			Polygon(hDC,triangle,4);
 
-			//This is the lower side of the direction arrow
-			up.x=centerX+(long)(innerradius*fastsine(-rotation));
-			up.y=centerY+(long)(innerradius*fastcosine(-rotation));
-			down.x=centerX+(long)(cdiRadius*fastsine(-rotation));
-			down.y=centerY+(long)(cdiRadius*fastcosine(-rotation));
+			//This is the opposite side of the course direction arrow
+			up.x=centerX-innerradiusXsin;
+			up.y=centerY+innerradiusXcos;
+			down.x=centerX-cdiRadiusXsin;
+			down.y=centerY+cdiRadiusXcos;
 			_DrawLine(hDC, PS_ENDCAP_SQUARE, NIBLSCALE(2),up,down,RGB_GREEN,rc);
 
-			//CDI
+			//Course Deviation Indicator
 			double xtd=DerivedDrawInfo.LegCrossTrackError;
 			int dev; //deviation in pixel
 			COLORREF cdiColor=screenModeNormal?RGB_YELLOW:RGB_DARKYELLOW;
@@ -175,23 +193,19 @@ void MapWindow::DrawHSI(HDC hDC, const RECT rc) {
 			if(abs(xtd)<smallCDIscale) { //use small scale of 0.3 NM
 				long tick=cdiFullScale/3; //every mark represents 0.1 NM
 				for(int i=1;i<3;i++) {
-					long x=centerX+(long)(tick*i*fastcosine(rotation));
-					long y=centerY+(long)(tick*i*fastsine(rotation));
-					Circle(hDC,x,y,NIBLSCALE(1),rc,false,false);
-					x=centerX-(long)(tick*i*fastcosine(rotation));
-					y=centerY-(long)(tick*i*fastsine(rotation));
-					Circle(hDC,x,y,NIBLSCALE(1),rc,false,false);
+					long tickXiXsin=tick*i*sin;
+					long tickXiXcos=tick*i*cos;
+					Circle(hDC,centerX+tickXiXcos,centerY+tickXiXsin,NIBLSCALE(1),rc,false,false);
+					Circle(hDC,centerX-tickXiXcos,centerY-tickXiXsin,NIBLSCALE(1),rc,false,false);
 				}
 				dev=-(int)(round((cdiFullScale*xtd)/smallCDIscale));
 			} else { // use big scale of 5 NM
 				long tick=cdiFullScale/5; //every mark represents 1 NM
 				for(int i=1;i<5;i++) {
-					long x=centerX+(long)(tick*i*fastcosine(rotation));
-					long y=centerY+(long)(tick*i*fastsine(rotation));
-					Circle(hDC,x,y,NIBLSCALE(1),rc,false,false);
-					x=centerX-(long)(tick*i*fastcosine(rotation));
-					y=centerY-(long)(tick*i*fastsine(rotation));
-					Circle(hDC,x,y,NIBLSCALE(1),rc,false,false);
+					long tickXiXsin=tick*i*sin;
+					long tickXiXcos=tick*i*cos;
+					Circle(hDC,centerX+tickXiXcos,centerY+tickXiXsin,NIBLSCALE(1),rc,false,false);
+					Circle(hDC,centerX-tickXiXcos,centerY-tickXiXsin,NIBLSCALE(1),rc,false,false);
 				}
 				if(xtd>bigCDIscale) {
 					dev=-cdiFullScale;
@@ -199,33 +213,22 @@ void MapWindow::DrawHSI(HDC hDC, const RECT rc) {
 				} else if(xtd<-bigCDIscale) {
 					dev=cdiFullScale;
 					cdiColor=RGB_RED;
-				} else dev=-(int)(round((cdiFullScale*xtd)/bigCDIscale));
+				} else dev=-round((cdiFullScale*xtd)/bigCDIscale);
 			}
-			up.x=centerX+(long)(cdiRadius*fastsine(rotation))+(long)(dev*fastcosine(rotation));
-			up.y=centerY-(long)(cdiRadius*fastcosine(rotation))+(long)(dev*fastsine(rotation));
-			down.x=centerX+(long)(cdiRadius*fastsine(-rotation))+(long)(dev*fastcosine(-rotation));
-			down.y=centerY+(long)(cdiRadius*fastcosine(-rotation))-(long)(dev*fastsine(-rotation));;
+			long devXsin=dev*sin;
+			long devXcos=dev*cos;
+			up.x=centerX+cdiRadiusXsin+devXcos;
+			up.y=centerY-cdiRadiusXcos+devXsin;
+			down.x=centerX-cdiRadiusXsin+devXcos;
+			down.y=centerY+cdiRadiusXcos+devXsin;
 			_DrawLine(hDC, PS_ENDCAP_SQUARE, NIBLSCALE(2),up,down,cdiColor,rc);
 		}
 	}
 
 	//Draw airplane symbol
-	POINT a,b;
-	a.x=centerX;
-	a.y=centerY-NIBLSCALE(7);
-	b.x=centerX;
-	b.y=centerY+NIBLSCALE(7);
-	_DrawLine(hDC, PS_ENDCAP_SQUARE, NIBLSCALE(2),a,b,RGB_ORANGE,rc);
-	a.x=centerX-NIBLSCALE(8);
-	a.y=centerY-NIBLSCALE(3);
-	b.x=centerX+NIBLSCALE(8);
-	b.y=centerY-NIBLSCALE(3);
-	_DrawLine(hDC, PS_ENDCAP_SQUARE, NIBLSCALE(2),a,b,RGB_ORANGE,rc);
-	a.x=centerX-NIBLSCALE(3);
-	a.y=centerY+NIBLSCALE(4);
-	b.x=centerX+NIBLSCALE(3);
-	b.y=centerY+NIBLSCALE(4);
-	_DrawLine(hDC, PS_ENDCAP_SQUARE, NIBLSCALE(2),a,b,RGB_ORANGE,rc);
+	_DrawLine(hDC, PS_ENDCAP_SQUARE, NIBLSCALE(2),fusA,fusB,RGB_ORANGE,rc);
+	_DrawLine(hDC, PS_ENDCAP_SQUARE, NIBLSCALE(2),winA,winB,RGB_ORANGE,rc);
+	_DrawLine(hDC, PS_ENDCAP_SQUARE, NIBLSCALE(2),taiA,taiB,RGB_ORANGE,rc);
 
 	SelectObject(hDC, hbOld);
 	SelectObject(hDC, hpOld);
