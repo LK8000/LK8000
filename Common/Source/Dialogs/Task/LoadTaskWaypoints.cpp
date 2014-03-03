1792173f62a812b2ba76f90e961f2daf7c1f98df
Alberto Realis-Luc <admin@alus.it>
2014-03-03 23:45:08 +0100

    Fixing indentation: 4 spaces iso tab

--- Common/Source/Dialogs/Task/LoadTaskWaypoints.cpp
+++ Common/Source/Dialogs/Task/LoadTaskWaypoints.cpp
@@ -12,29 +12,25 @@
 
 // this is called only from Task LoadTaskWaypoints
 int FindOrAddWaypoint(WAYPOINT *read_waypoint, bool look_for_airfield) {
-  // this is an invalid pointer!
-  read_waypoint->Details = 0;
-  read_waypoint->Comment = 0;
-  read_waypoint->Name[NAME_SIZE-1] = 0; // prevent overrun if data is bogus
- 
-  int waypoint_index=-1;
-  if(look_for_airfield) waypoint_index = FindMatchingAirfield(read_waypoint);
-  else waypoint_index = FindMatchingWaypoint(read_waypoint);
-  if (waypoint_index == -1) {
-	// waypoint not found, so add it!
-    
-	// TODO bug: Set WAYPOINTFILECHANGED so waypoints get saved?
-	// NO, we dont save task waypoints inside WP files! 
-	WAYPOINT* new_waypoint = GrowWaypointList();
-	if (!new_waypoint) {
-		// error, can't allocate!
-		return false;
-	}
-	memcpy(new_waypoint, read_waypoint, sizeof(WAYPOINT));
-	// 100229 set no-save flag on
-	new_waypoint->FileNum=-1;
-	waypoint_index = NumberOfWayPoints-1;
-  }
-  return waypoint_index;
-}
+    // this is an invalid pointer!
+    read_waypoint->Details = 0;
+    read_waypoint->Comment = 0;
+    read_waypoint->Name[NAME_SIZE-1] = 0; // prevent overrun if data is bogus
 
+    int waypoint_index=-1;
+    if(look_for_airfield) waypoint_index = FindMatchingAirfield(read_waypoint);
+    else waypoint_index = FindMatchingWaypoint(read_waypoint);
+    if (waypoint_index == -1) { // waypoint not found, so add it!
+        // TODO bug: Set WAYPOINTFILECHANGED so waypoints get saved?
+        // NO, we dont save task waypoints inside WP files!
+        WAYPOINT* new_waypoint = GrowWaypointList();
+        if (!new_waypoint) { // error, can't allocate!
+            return false;
+        }
+        memcpy(new_waypoint, read_waypoint, sizeof(WAYPOINT));
+        // 100229 set no-save flag on
+        new_waypoint->FileNum=-1;
+        waypoint_index = NumberOfWayPoints-1;
+    }
+    return waypoint_index;
+}
--- Common/Source/Waypoints/FindMatchingWaypoint.cpp
+++ Common/Source/Waypoints/FindMatchingWaypoint.cpp
@@ -12,38 +12,30 @@
 
 
 int FindMatchingWaypoint(WAYPOINT *waypoint) {
-  if (!WayPointList) {
-    return -1;
-  }
-  unsigned int i;
-
-  for (i=NUMRESWP; i<NumberOfWayPoints; i++) {
-
-	// if different name, no match
-	if (_tcscmp(waypoint->Name, WayPointList[i].Name)!=0) continue;
-
-	// if same name, lat lon and flags must be the same in order to match
-	// a previously existing waypoint
-	if ((fabs(waypoint->Latitude-WayPointList[i].Latitude)<1.0e-6) 
-	&& (fabs(waypoint->Longitude-WayPointList[i].Longitude)<1.0e-6) &&
-	(waypoint->Flags == WayPointList[i].Flags) ) {
-		// name, lat,lon, flags are the same: same wp!
-		return i;
-	}
-  }
-  
+    if (!WayPointList) {
+        return -1;
+    }
+    unsigned int i;
+    for (i=NUMRESWP; i<NumberOfWayPoints; i++) {
+        if (_tcscmp(waypoint->Name, WayPointList[i].Name)!=0) continue; // if different name, no match
+        // if same name, lat lon and flags must be the same in order to match a previously existing waypoint
+        if ((fabs(waypoint->Latitude-WayPointList[i].Latitude)<1.0e-6) &&
+                (fabs(waypoint->Longitude-WayPointList[i].Longitude)<1.0e-6) &&
+                (waypoint->Flags == WayPointList[i].Flags) ) { // name, lat,lon, flags are the same: same wp!
+            return i;
+        }
+    }
   return -1;
 }
 
 int FindMatchingAirfield(WAYPOINT *waypoint) {
-	if(!WayPointList) return -1;
-	const double limit=0.00899928005; //1 Km expressed in deg
-	for(unsigned int i=NUMRESWP; i<NumberOfWayPoints; i++) { //for all WP in list
-		if(WayPointList[i].Style>=STYLE_AIRFIELDGRASS && //if it is any kind of airport/airfield
-				WayPointList[i].Style<=STYLE_AIRFIELDSOLID &&
-				(fabs(waypoint->Latitude-WayPointList[i].Latitude)<limit) && //and if coordinates within 1 Km range
-				(fabs(waypoint->Longitude-WayPointList[i].Longitude)<limit)) return i; //assume this as the desired WP
-	}
-	return -1;
+    if(!WayPointList) return -1;
+    const double limit=0.00899928005; //1 Km expressed in deg
+    for(unsigned int i=NUMRESWP; i<NumberOfWayPoints; i++) { //for all WP in list
+        if(WayPointList[i].Style>=STYLE_AIRFIELDGRASS && //if it is any kind of airport/airfield
+                WayPointList[i].Style<=STYLE_AIRFIELDSOLID &&
+                (fabs(waypoint->Latitude-WayPointList[i].Latitude)<limit) && //and if coordinates within 1 Km range
+                (fabs(waypoint->Longitude-WayPointList[i].Longitude)<limit)) return i; //assume this as the desired WP
+    }
+    return -1;
 }
-
