06a77fa3ea882a53cebef7a20d506ed0cc3474c8
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2014-03-06 22:23:49 +0100

    Fix simulator using correct sink rate adjusted for air density

--- Common/Source/LKSimulator.cpp
+++ Common/Source/LKSimulator.cpp
@@ -126,7 +126,7 @@ void LKSimulator(void) {
     // ONLY during flight, we will sink in the air
     if (FLYING && (IASMS>3) && (IASMS<MAXSPEED) ) {
 
-	double sinkias=-1*GlidePolar::SinkRate(TASMS);
+	double sinkias=-1*GlidePolar::SinkRate(IASMS)*AirDensityRatio(AltitudeToQNEAltitude(GPS_INFO.Altitude));
 	if (sinkias>10) sinkias=10; // set a limiter for sink rate
 	// StartupStore(_T(".... ias=%.0f sinkias=%.3f oldAlt=%.3f newAlt=%.3f\n"), 
 	// CALCULATED_INFO.IndicatedAirspeedEstimated*TOKPH, sinkias, GPS_INFO.Altitude, GPS_INFO.Altitude+sinkias);
