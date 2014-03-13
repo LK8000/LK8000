584db6c7093444fcc1eb07888b59d883e7160dc6
Paolo Ventafridda <venta@lk8000.it>
2014-03-13 13:44:25 +0100

    Unused code in devDigifly

--- Common/Source/Devices/devDigifly.cpp
+++ Common/Source/Devices/devDigifly.cpp
@@ -15,7 +15,6 @@ extern bool UpdateBaroSource(NMEA_INFO* pGPS, const short parserid, const PDevic
 extern double LowPassFilter(double y_last, double x_in, double fact);
 
 static BOOL PDGFTL1(PDeviceDescriptor_t d, TCHAR *String, NMEA_INFO *pGPS);
-static BOOL PDGFTTL(PDeviceDescriptor_t d, TCHAR *String, NMEA_INFO *pGPS);
 
 static BOOL DigiflyParseNMEA(PDeviceDescriptor_t d, TCHAR *String, NMEA_INFO *pGPS){
 
@@ -29,10 +28,6 @@ static BOOL DigiflyParseNMEA(PDeviceDescriptor_t d, TCHAR *String, NMEA_INFO *pG
   if(_tcsncmp(TEXT("$PDGFTL1"), String, 8)==0)
     {
       return PDGFTL1(d, &String[9], pGPS);
-    } 
-  if(_tcsncmp(TEXT("$PDGFTTL"), String, 8)==0)
-    {
-      return PDGFTTL(d, &String[9], pGPS);
     } 
 
   return FALSE;
@@ -191,84 +186,4 @@ static BOOL PDGFTL1(PDeviceDescriptor_t d, TCHAR *String, NMEA_INFO *pGPS)
   return TRUE;
 }
 
-//
-// Support for new 2011 Digifly TOURTELL protocol 
-// (Subset of TL1, pending upgrade in june 2011 for all devices)
-//
-static BOOL PDGFTTL(PDeviceDescriptor_t d, TCHAR *String, NMEA_INFO *pGPS)
-{
-/*
-	$PDGFTTL		     field     example
-	QNE 1013.25 altitude		0	2025  meters
-	QNH altitude			1	2000  meters
-	vario cm/s	 		2	 250  2.5m/s
-	IAS				4	45    kmh
-	netto vario			3	-14   dm/s
-	
-*/
-
-  TCHAR ctemp[80];
-  double vtas, vias;
-  double altqne, altqnh;
-  static bool initqnh=true;
-
-  NMEAParser::ExtractParameter(String,ctemp,0);
-  altqne = StrToDouble(ctemp,NULL);
-  NMEAParser::ExtractParameter(String,ctemp,1);
-  altqnh = StrToDouble(ctemp,NULL);
-
-  // AutoQNH will take care of setting an average QNH if nobody does it for a while
-  if (initqnh) {
-	// if digifly has qnh set by user qne and qnh are of course different
-	if (altqne != altqnh) {
-		QNH=FindQNH(altqne,altqnh);
-        CAirspaceManager::Instance().QnhChangeNotify(QNH);
-		StartupStore(_T(". Using Digifly QNH %f%s"),QNH,NEWLINE);
-		initqnh=false;
-	} else {
-		// if locally QNH was set, either by user of by AutoQNH, stop processing QNH from digifly
-		if ( (QNH <= 1012) || (QNH>=1014)) initqnh=false;
-		// else continue entering initqnh until somebody changes qnh in either digifly or lk8000
-	}
-  }
-  UpdateBaroSource( pGPS, 0,d,  AltitudeToQNHAltitude(altqne));
-
-  NMEAParser::ExtractParameter(String,ctemp,2);
-#if 1
-  pGPS->Vario = StrToDouble(ctemp,NULL)/100;
-#else
-  double newVario = StrToDouble(ctemp,NULL)/100;
-  pGPS->Vario = LowPassFilter(pGPS->Vario,newVario,0.1);
-#endif
-  pGPS->VarioAvailable = TRUE;
-
-
-  NMEAParser::ExtractParameter(String,ctemp,4);
-  if (ctemp[0] != '\0') {
-	pGPS->NettoVario = StrToDouble(ctemp,NULL)/10; // dm/s
-	pGPS->NettoVarioAvailable = TRUE;
-  } else
-	pGPS->NettoVarioAvailable = FALSE;
-
-
-  NMEAParser::ExtractParameter(String,ctemp,4);
-  if (ctemp[0] != '\0') {
-	// we store m/s  , so we convert it from kmh
-	vias = StrToDouble(ctemp,NULL)/3.6;
-
-	if (vias >1) {
-		vtas = vias*AirDensityRatio(pGPS->BaroAltitude);
-		pGPS->TrueAirspeed = vtas;
-		pGPS->IndicatedAirspeed = vias;
-		pGPS->AirspeedAvailable = TRUE;
-  	}
-  } else {
-	pGPS->AirspeedAvailable = FALSE;
-  }
-
-
-  TriggerVarioUpdate();
-
-  return TRUE;
-}
 
