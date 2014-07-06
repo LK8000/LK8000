14f131fd62968a30269fd32e218973dc35cb3bc1
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2014-07-06 15:54:18 +0200

    reduce memory and cpu usage for LKLanguage

--- Common/Source/LKLanguage.cpp
+++ Common/Source/LKLanguage.cpp
@@ -7,22 +7,10 @@
  */
 
 #include "externs.h"
-#include "InfoBoxLayout.h"
-#include "Logger.h"
-#include "WaveThread.h"
-#include "Message.h"
-#include "McReady.h"
-#include "InputEvents.h"
-#include "LKProfiles.h"
 #include "DoInits.h"
-
-#include <ctype.h>
-
-#include "utils/stringext.h"
 #include "utils/stl_utils.h"
 
-
-// #define DEBUG_GETTEXT	1
+//#define DEBUG_GETTEXT	1
 #define LKD_LANGUAGE	"_Language"
 #define MAX_HELP	1500	// complete help including several lines, and also for each single line
 				// Remember there is a limit in ReadULine, always careful with large strings
@@ -31,6 +19,7 @@
 #define MAX_MESSAGE_SIZE	150 // just for setting a limit
 
 bool LKLoadMessages(bool fillup);
+void LKUnloadMessage();
 
 // _@Hnnnn@
 // minimal: _@H1_  maximal: _@H1234_
@@ -46,38 +35,19 @@ const TCHAR *LKgethelptext(const TCHAR *TextIn) {
 
   if (TextIn == NULL) return TextIn;
   short tlen=_tcslen(TextIn);
-  if (tlen<5 || tlen>8) return TextIn;
-  if ( (TextIn[0]!='_') || (TextIn[1]!='@') || (TextIn[tlen-1]!='_') ) return TextIn;
+  if ( (tlen<5) || (TextIn[0]!='_') || (TextIn[1]!='@') || (TextIn[tlen-1]!='_') ) return TextIn;
 
+    // get the item index number, quick conversion from unicode
+    unsigned short inumber = 0;
+    for (short i = 0; i < tlen - 4; i++) {
+        inumber = (inumber * 10) + ((char)TextIn[3 + i] - '0');
+    }
 
   // get the item index number, quick conversion from unicode
-  char snum[6];
-  // char *pnum=(char *)&TextIn[3]; 
-
-  short i;
-  for (i=0; i<tlen-4; i++) {
-	/*
-	snum[i++]=*pnum; // advance to unicode 0
-	pnum++; // advance to next char
-	pnum++; // advance to next char
-	*/
-	snum[i] = (char)TextIn[3+i];
-  }
-  snum[i]='\0';
-  
-  unsigned short inumber=atoi(snum);
+ 
   #if DEBUG_GETTEXT
-  StartupStore(_T(".... Help item TextIn=<%s> snum= <%S> number=%d \n"),TextIn, snum, inumber);
+  StartupStore(_T(".... Help item TextIn=<%s> number=%d \n"),TextIn, inumber);
   #endif
-
-  if (inumber>9999) {
-	#if DEBUG_GETTEXT
-	StartupStore(_T(".... Help item snum= <%S> number=%d \n"),snum, inumber);
-	#endif
-	_stprintf(sHelp,_T("ERROR, wrong index number <%u> from XML: <%s>\r\n"),inumber,TextIn);
-	return (sHelp);
-  }
-
 
   // get the type character
   char ttype = TextIn[2];
@@ -106,8 +76,8 @@ const TCHAR *LKgethelptext(const TCHAR *TextIn) {
 
 	// search for beginning of code index   @000
 	bool found=false;
-	while (ReadULine(helpFile, sTmp, countof(sTmp))) {
-		int slen=_tcslen(sTmp); // includes cr or lf or both
+	while (ReadULine(helpFile, sTmp, array_size(sTmp))) {
+		unsigned slen=_tcslen(sTmp); // includes cr or lf or both
 		if (slen<3|| slen>8) {
 			#if DEBUG_GETTEXT
 			StartupStore(_T("... skip line <3||>8 : %s\n"),sTmp);
@@ -122,7 +92,7 @@ const TCHAR *LKgethelptext(const TCHAR *TextIn) {
 			continue;
 		}
 		if (sTmp[0]=='@') {
-			for (i=1; i<slen; i++) {
+			for (unsigned i=1; i<slen; i++) {
 				if ( sTmp[i] < '0' || sTmp[i] > '9' )  {
 					sTmp[i] = '\0';
 					break;
@@ -165,7 +135,7 @@ const TCHAR *LKgethelptext(const TCHAR *TextIn) {
 	// now load the help text for this index
 	_tcscpy(sHelp,_T(""));
 	int hlen=0;
-	while (ReadULine(helpFile, sTmp, countof(sTmp))) {
+	while (ReadULine(helpFile, sTmp, array_size(sTmp))) {
 
 		int slen=_tcslen(sTmp); // including cr or lf or both
 		if (slen==0 || sTmp[0]=='#') continue;
@@ -213,59 +183,40 @@ const TCHAR *LKgethelptext(const TCHAR *TextIn) {
 
 
 static TCHAR * LKMessages[MAX_MESSAGES+1];
-static short LKMessagesIndex[MAX_MESSAGES+1];
 
 //  Tokenized Language support for LK8000
 //  gettext is now a definition for LKGetText
 //  101208 
+
 const TCHAR *LKGetText(const TCHAR *TextIn) {
+    // quick preliminar checks
+    if (TextIn == NULL) return TextIn;
+    short tlen = _tcslen(TextIn);
+    if ((tlen < 5) || (TextIn[0] != '_') || (TextIn[1] != '@') || (TextIn[2] != 'M') || (TextIn[tlen - 1] != '_')) return TextIn;
 
-  // quick preliminar checks
-  if (TextIn == NULL) return TextIn;
-  short tlen=_tcslen(TextIn);
-  if (tlen<5 || tlen>8) return TextIn;
-  if ( (TextIn[0]!='_') || (TextIn[1]!='@') || (TextIn[tlen-1]!='_') ) return TextIn;
+    // get the item index number, quick conversion from unicode
+    unsigned short inumber = 0;
+    for (short i = 0; i < tlen - 4; i++) {
+        inumber = (inumber * 10) + ((char)TextIn[3 + i] - '0');
+    }
 
-  // get the item index number, quick conversion from unicode
-  char snum[6];
-  short i;
-  for (i=0; i<tlen-4; i++) {
-	snum[i] = (char)TextIn[3+i];
-  }
-  snum[i]='\0';
-  unsigned short inumber=atoi(snum);
-
-
-  #if DEBUG_GETTEXT
-  //StartupStore(_T(".... LKGettext item TextIn=<%s> snum= <%S> number=%d \n"),TextIn, snum, inumber);
-  #endif
-
-  // get the type character
-  char ttype = TextIn[2];
-
-  // Text messages inside C code
-  if (ttype=='M') { // Message
-	if (inumber> MAX_MESSAGES) return TextIn; // safe check
-	int pointer= LKMessagesIndex[inumber];
-        if (pointer<0 || pointer>MAX_MESSAGES) return TextIn;
-	return LKMessages[pointer];
-  }
-  return TextIn;
-
-
+    if (inumber > MAX_MESSAGES) return TextIn; // safe check
+    if(LKMessages[inumber]) {
+        return LKMessages[inumber];
+    }
+    return _T("");
 }
 
 //
 // Direct token access, with range check, faster than LKGetText of course
+// !!! this function is not threadsafe in case of Error;
 //
 TCHAR *MsgToken(const unsigned int inumber) {
-  unsigned int pointer; 
   if (inumber<=MAX_MESSAGES) {
-	pointer=LKMessagesIndex[inumber];
-        if (pointer<=MAX_MESSAGES) return (TCHAR *)LKMessages[pointer];
+    return (TCHAR *)LKMessages[inumber];
   }
   static TCHAR terr[30];
-  _stprintf(terr,_T("@M%u"),inumber);
+  _stprintf(terr,_T("_@M%u_"),inumber);
   return(terr);
 }
 
@@ -293,16 +244,10 @@ void LKReadLanguageFile() {
   // SetRegistryString(szRegistryLanguageFile, TEXT("\0")); // ?
 
   ZZIP_FILE *langFile = zzip_fopen(szFile1, "rb");
-	if (langFile == NULL) {
+  if (langFile == NULL) {
 	if (english) {
 		StartupStore(_T("--- CRITIC, NO ENGLISH LANGUAGE FILES!%s"),NEWLINE);
 		// critic point, no default language! BIG PROBLEM here!
-		for (unsigned short i=0; i<MAX_MESSAGES; i++) {
-			LKMessages[i]=NULL;
-			LKMessagesIndex[i]=-1;
-		}
-		return;
-		
 	} else {
 		StartupStore(_T("--- NO LANGUAGE FILE FOUND <%s>, retrying with ENGlish!%s"),szFile1,NEWLINE);
 		_tcscpy(szFile1,_T(""));
@@ -314,7 +259,7 @@ void LKReadLanguageFile() {
   bool found=false;
   TCHAR sTmp[200];
   TCHAR mylang[30];
-  while (ReadULine(langFile, sTmp, countof(sTmp))) {
+  while (ReadULine(langFile, sTmp, array_size(sTmp))) {
 	if (_tcslen(sTmp)<3) continue;
 	if ((sTmp[0]=='L')&&(sTmp[1]=='=')) {
 		_tcscpy(mylang,&sTmp[2]);
@@ -377,48 +322,20 @@ bool LKLoadMessages(bool fillup) {
   TCHAR sFile[MAX_PATH];
   TCHAR sPath[MAX_PATH];
   TCHAR suffix[20];
-  unsigned int i, j;
   #if DEBUG_GETTEXT
   int maxsize=0;
   #endif
 
   static bool doinit=true;
-  short mnumber=0;
-
-  #if TESTBENCH
-  short fillupstart=0;
-  #endif
 
   if (doinit) {
-	for (i=0; i<MAX_MESSAGES; i++) {
-		LKMessages[i]=NULL;
-		LKMessagesIndex[i]=-1;
-	}
+    std::fill(begin(LKMessages), end(LKMessages), (TCHAR*)NULL);
 	doinit=false;
   } else {
 	if (!fillup) {
 		// init data when reloading language files or changing it
 		// but not in fillup mode of course
-		for (i=0; i<MAX_MESSAGES; i++) {
-			if (LKMessages[i] != NULL) free(LKMessages[i]);
-			LKMessages[i]=NULL;
-			LKMessagesIndex[i]=-1;
-		}
-	} else {
-		// in fillup mode we need to add at the bottom of message array
-		for (i=0; i<MAX_MESSAGES; i++) {
-			if (LKMessages[i]!=NULL) ++mnumber;
-		}
-		if (mnumber == MAX_MESSAGES) {
-			#if TESTBENCH
-			StartupStore(_T("... Fillup language MSG already full\n"));
-			#endif
-			return false;
-		}
-		#if TESTBENCH
-		fillupstart=mnumber;
-		StartupStore(_T("... Fillup language MSG starting from pos.%d\n"),mnumber);
-		#endif
+        LKUnloadMessage();
 	}
   }
 
@@ -439,45 +356,39 @@ bool LKLoadMessages(bool fillup) {
 
   // search for beginning of code index, in the range _@M1_  _@M9999_ 
   TCHAR sTmp[300];
-  char snum[6];
   TCHAR scapt[MAX_MESSAGE_SIZE+1];
   TCHAR scaptraw[MAX_MESSAGE_SIZE+1];
 
   bool havewarned=false;
-  while (ReadULine(hFile, sTmp, countof(sTmp))) {
+  while (ReadULine(hFile, sTmp, array_size(sTmp))) {
 
 	unsigned int slen=_tcslen(sTmp); // includes cr or lf or both
-	if (slen<9) continue;
-	if ( (sTmp[0]!='_') || (sTmp[1]!='@') || (sTmp[2]!='M') ) {
-		#if DEBUG_GETTEXT
-		StartupStore(_T(".... MSG_ENG missing _@M line <%s>\n"),sTmp);
-		#endif
+	if ( (slen<9) || (sTmp[0]!='_') || (sTmp[1]!='@') || (sTmp[2]!='M') ) {
+        #if DEBUG_GETTEXT
+        if(slen>0 && sTmp[0]!='#') {
+            StartupStore(_T(".... MSG_ENG missing _@M line <%s>\n"),sTmp);
+        }
+        #endif
 		continue;
 	}
 
-	snum[0]=(char)sTmp[3];
-	snum[1]=(char)sTmp[4];
-	snum[2]=(char)sTmp[5];
-	snum[3]=(char)sTmp[6];
-	snum[4]=(char)'\0';
-	if (snum[3]=='_') snum[3]='\0';
-	if (snum[2]=='_') snum[2]='\0';
-	if (snum[1]=='_') snum[1]='\0';
-
-	unsigned short inumber;
-	inumber=atoi(snum);
+    // get the item index number, quick conversion from unicode
+    unsigned short inumber = 0;
+    for (unsigned int i = 0; i < slen - 4 && isdigit(sTmp[3 + i]); i++) {
+        inumber = (inumber * 10) + ((char)sTmp[3 + i] - '0');
+    }
 
 	if (inumber >=MAX_MESSAGES) {
 		if (!havewarned) {
 			StartupStore(_T("...... ERROR LOADING NON-COMPATIBLE MSG FILE!%s"),NEWLINE);
 			havewarned=true;
 		}
-		StartupStore(_T("...... MSG token <%d> over limit!%s"),inumber,NEWLINE);
+		StartupStore(_T("...... MSG token <%d> over limit! <%s>%s"),inumber,sTmp,NEWLINE);
 		continue;
 	}
 
 	int start=0;
-	for (i=3; i<slen; i++) {
+	for (unsigned i=3; i<slen; i++) {
 		if (sTmp[i]=='\"') {
 			start=i;
 			break;
@@ -491,7 +402,7 @@ bool LKLoadMessages(bool fillup) {
 		#endif
 		continue;
 	}
-	for (i=start+1; i<slen; i++) {
+	for (unsigned i=start+1; i<slen; i++) {
 		if (sTmp[i]=='\"') {
 			sTmp[i]='\0';
 			end=i;
@@ -513,20 +424,21 @@ bool LKLoadMessages(bool fillup) {
 		continue;
 	}
 	if (newlen==0) {
-        	#if DEBUG_GETTEXT
+        #if DEBUG_GETTEXT
 		StartupStore(_T(".... MSG_ENG TOKEN # %d : caption is empty, null text.\n"),inumber);
 		#endif
 		continue;
 	}
-        #if DEBUG_GETTEXT
+    #if DEBUG_GETTEXT
 	if (newlen>maxsize) maxsize=newlen;
-        #endif
+    #endif
 
 	// transcode special charcaters while loading from file
 	TCHAR tcode;
 	bool donetcode;
 	_tcscpy(scaptraw,&sTmp[start+1]);
-	for (i=0, j=0; i<_tcslen(scaptraw); i++) {
+    unsigned j=0;
+	for (unsigned i=0; i<_tcslen(scaptraw); i++) {
 		donetcode=false;
 		if (scaptraw[i] == '\\') {
 			if ( (i+1) <_tcslen(scaptraw)) {
@@ -552,7 +464,7 @@ bool LKLoadMessages(bool fillup) {
 	}
 	scapt[j]='\0';
 
-	if (LKMessagesIndex[inumber]!= -1) {
+	if (LKMessages[inumber]) {
 		// only for debugging translations
 		#if TESTBENCH
 		if (!fillup)
@@ -567,13 +479,12 @@ bool LKLoadMessages(bool fillup) {
 	if (fillup)
 		StartupStore(_T("... Fillup: message index %d is missing from translation\n"),inumber);
 	#endif
-	#endif
-	LKMessagesIndex[inumber]=mnumber;
-	LKMessages[mnumber] = (TCHAR *)malloc((_tcslen(scapt)+1)*sizeof(TCHAR));
-	LKASSERT(LKMessages[mnumber]!=NULL);
-	_tcscpy(LKMessages[mnumber],scapt);
-	mnumber++;
-	if (mnumber>=MAX_MESSAGES) {
+	#endif	
+
+	LKMessages[inumber] = (TCHAR *)malloc((_tcslen(scapt)+1)*sizeof(TCHAR));
+	LKASSERT(LKMessages[inumber]!=NULL);
+	_tcscpy(LKMessages[inumber],scapt);
+	if (inumber>=MAX_MESSAGES) {
 		#if TESTBENCH
 		StartupStore(_T("... TOO MANY MESSAGES, MAX %d%s"), MAX_MESSAGES, NEWLINE);
 		#endif
@@ -581,19 +492,6 @@ bool LKLoadMessages(bool fillup) {
 	}
 
   }
-
-  #if DEBUG_GETTEXT
-  StartupStore(_T("... LOADED %d MESSAGES, max size = %d\n"),mnumber-1,maxsize);
-  #endif
-  #if TESTBENCH
-  if (fillup) {
-	if ((mnumber-fillupstart-1)>0)
-		StartupStore(_T("... Fillup Loaded %d missing messages\n"),mnumber-fillupstart-1);
-	else 
-		StartupStore(_T("... Fillup no messages to load, translation OK\n"));
-  }
-  #endif
-
   zzip_fclose(hFile);
   return true;
 }
