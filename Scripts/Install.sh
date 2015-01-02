ba7db81229e2477f43dd87fe66abf53ee868b291
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2015-01-02 19:51:56 +0100

    update "Script/Install.sh"

--- Scripts/Install.sh
+++ Scripts/Install.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-cp -R ../Common/Distribution/LK8000/_Configuration ~/LK8000
-cp -R ../Common/Distribution/LK8000/_Polars ~/LK8000
+cp -Rv ../Common/Distribution/LK8000/* ~/LK8000
 
-cp -R ../Common/Distribution/LK8000/_System ~/LK8000/_System
-
-cp ../Common/Data/Language/DEFAULT_MENU.TXT ~/LK8000/_System
+cp -v ../Common/Data/Language/DEFAULT_MENU.TXT ~/LK8000/_System
 
 mkdir ~/LK8000/_Language
 
 echo "empty file" > ~/LK8000/_Language/_LANGUAGE
 echo "empty file" > ~/LK8000/_System/_Bitmaps/_BITMAPSH
 
-cp ../Common/Data/Language/ENG_HELP.TXT ~/LK8000/_Language/ENG_HELP.TXT
-cp ../Common/Data/Language/ENG_MSG.TXT ~/LK8000/_Language/ENG_MSG.TXT
-cp ../Common/Data/Language/ENGLISH.LNG ~/LK8000/_Language/ENGLISH.LNG
-cp ../Common/Data/Language/Translations/*.TXT ~/LK8000/_Language
-cp ../Common/Data/Language/Translations/*.LNG ~/LK8000/_Language
+cp -v ../Common/Data/Language/ENG_HELP.TXT ~/LK8000/_Language/ENG_HELP.TXT
+cp -v ../Common/Data/Language/ENG_MSG.TXT ~/LK8000/_Language/ENG_MSG.TXT
+cp -v ../Common/Data/Language/ENGLISH.LNG ~/LK8000/_Language/ENGLISH.LNG
+cp -v ../Common/Data/Language/Translations/*.TXT ~/LK8000/_Language
+cp -v ../Common/Data/Language/Translations/*.LNG ~/LK8000/_Language
 
 cp ../Common/Data/Sounds/*.WAV ~/LK8000/_System/_Sounds
 
-cp -R ../Distrib/LINUX/_System ~/LK8000/
+rm -fv ~/LK8000/_System/_Bitmaps/*.BMP
+
+cp -Rv ../Distrib/LINUX/* ~/LK8000/
