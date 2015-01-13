a3537e09d62baf956452f6f707ba53bc7310f87f
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2015-01-13 23:39:04 +0100

    LKBitmap::LoadFromResource : fix for X86_64 linux TARGET

--- Common/Source/Screen/LKBitmap.cpp
+++ Common/Source/Screen/LKBitmap.cpp
@@ -76,9 +76,9 @@ bool LKBitmap::LoadFromResource(const TCHAR* ResourceName) {
 #else
     const TCHAR* szID = ResourceName;
     TCHAR szTmp[10] = {};
-    if(!(((unsigned)ResourceName)&0xFFFF0000)) {
+    if((ptrdiff_t)ResourceName < (ptrdiff_t)std::numeric_limits<unsigned short>::max()) {
         // we have resource ID
-        _stprintf(szTmp, _T("%u"), (unsigned)ResourceName);
+        _stprintf(szTmp, _T("%u"), (unsigned short)(ptrdiff_t)ResourceName);        
         szID = szTmp;
     }
     if(szID) {
