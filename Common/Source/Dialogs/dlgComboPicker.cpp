d3f2c291f487d812bf2d2bf9f72b4ec5ff87b3a0
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2015-02-09 22:05:46 +0100

    dlgComboPicker : update item text position, vertical center.

--- Common/Source/Dialogs/dlgComboPicker.cpp
+++ Common/Source/Dialogs/dlgComboPicker.cpp
@@ -27,27 +27,29 @@ ComboList * ComboListPopup=NULL;
 static TCHAR sSavedInitialValue[ComboPopupITEMMAX];
 static int iSavedInitialDataIndex=-1;
 
-static void OnPaintComboPopupListItem(WindowControl * Sender, LKSurface& Surface){
+static void OnPaintComboPopupListItem(WindowControl * Sender, LKSurface& Surface) {
 
-  if(Sender) {
+    if (Sender) {
 
-    if ( ComboListPopup->ComboPopupDrawListIndex >= 0 && 
-            ComboListPopup->ComboPopupDrawListIndex < ComboListPopup->ComboPopupItemCount ) {
+        if (ComboListPopup->ComboPopupDrawListIndex >= 0 &&
+                ComboListPopup->ComboPopupDrawListIndex < ComboListPopup->ComboPopupItemCount) {
 
-        // Fill Background with Highlight color if Selected Item
-        if (!Sender->HasFocus() && ComboListPopup->ComboPopupItemIndex == ComboListPopup->ComboPopupDrawListIndex ) {
-            RECT rc = Sender->GetClientRect();
-            Surface.FillRect(&rc, LKBrush_Higlighted);
-        }    
+            // Fill Background with Highlight color if Selected Item
+            if (!Sender->HasFocus() && ComboListPopup->ComboPopupItemIndex == ComboListPopup->ComboPopupDrawListIndex) {
+                RECT rc = Sender->GetClientRect();
+                Surface.FillRect(&rc, LKBrush_Higlighted);
+            }
 
-        int w=Sender->GetWidth();
+            const int w = Sender->GetWidth();
+            const int h = Sender->GetHeight();
 
-        Surface.SetBackgroundTransparent();
-        Surface.SetTextColor(RGB_BLACK);
-        Surface.DrawTextClip(2*ScreenScale,
-                2*ScreenScale,
-                ComboListPopup->ComboPopupItemList[ComboListPopup->ComboPopupDrawListIndex]->StringValueFormatted,
-                w-ScreenScale*5);
+            const TCHAR* szText = ComboListPopup->ComboPopupItemList[ComboListPopup->ComboPopupDrawListIndex]->StringValueFormatted;
+
+            Surface.SetBackgroundTransparent();
+            Surface.SetTextColor(RGB_BLACK);
+            const int xText = 3 * ScreenScale;
+            const int yText = (h - Surface.GetTextHeight(szText)) / 2;
+            Surface.DrawTextClip(xText, yText, szText, w - ScreenScale * 5);
         }
     }
 }
