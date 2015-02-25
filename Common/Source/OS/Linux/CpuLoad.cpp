61197418501b1913d608d6714e17af54b1f098fa
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2015-02-25 21:37:49 +0100

    Cpu load : use real cpu usage instead of averaged number of processes in the system run queue
    previous method can be enabled using "USE_LOADAVG_CPU" define in option.h
    
    fix #600

--- Common/Source/OS/Linux/CpuLoad.cpp
+++ Common/Source/OS/Linux/CpuLoad.cpp
@@ -9,6 +9,8 @@
  */
 #include "externs.h"
 
+#ifdef USE_LOADAVG_CPU
+
 int CpuSummary() {
 
     double monoload[3];
@@ -23,3 +25,86 @@ int CpuSummary() {
     if (summary>999 || summary<0) summary=INVALID_VALUE;
     return summary;
 }
+
+#else
+
+#include <stdio.h>
+#include <unistd.h>
+
+/* Show overall CPU utilization of the system 
+ * http://phoxis.org/2013/09/05/finding-overall-and-per-core-cpu-utilization
+ */
+
+#define BUF_MAX 1024
+
+class GetCpuLoad_Singleton {
+public:
+
+    GetCpuLoad_Singleton() {
+        _fp = fopen("/proc/stat", "r");
+
+        if (_fp && read_fields()) {
+            total_tick = std::accumulate(std::begin(fields), std::end(fields), (uint64_t)0);
+            idle = fields[3]; /* idle ticks index */
+        }
+    }
+
+    ~GetCpuLoad_Singleton() {
+        if (_fp) {
+            fclose(_fp);
+        }
+    }
+
+    int operator()() {
+
+        if (_fp == NULL) {
+            return INVALID_VALUE;
+        }
+
+        if (!read_fields()) {
+            return INVALID_VALUE;
+        }
+        
+        total_tick_old = total_tick;
+        idle_old = idle;
+
+        if (!read_fields()) {
+            return INVALID_VALUE;
+        }
+        
+        total_tick = std::accumulate(std::begin(fields), std::end(fields), (uint64_t)0);
+        idle = fields[3];
+
+        del_total_tick = total_tick - total_tick_old;
+        del_idle = idle - idle_old;
+
+        return std::lrint(((del_total_tick - del_idle) / (double) del_total_tick) * 100);
+    }
+
+private:
+
+    bool read_fields() {
+        fseek(_fp, 0, SEEK_SET);
+        fflush(_fp);
+        if (fgets(buffer, BUF_MAX, _fp)) {
+            int retval = sscanf(buffer, "cpu %Lu %Lu %Lu %Lu %Lu %Lu %Lu %Lu %Lu %Lu",
+                    &fields[0], &fields[1], &fields[2], &fields[3], &fields[4],
+                    &fields[5], &fields[6], &fields[7], &fields[8], &fields[9]);
+
+            return (retval >= 4); /* Atleast 4 fields is to be read */
+        }
+        return false;
+    }
+
+    FILE* _fp;
+    char buffer[BUF_MAX];
+
+    uint64_t fields[10], total_tick, total_tick_old, idle, idle_old, del_total_tick, del_idle;
+};
+
+GetCpuLoad_Singleton GetGpuLoad;
+
+int CpuSummary() {
+    return GetGpuLoad();
+}
+#endif
