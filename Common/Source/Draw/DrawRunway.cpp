df3cbea3f48f363491c0a3c99a701a3d7c8f9211
Paolo Ventafridda <venta@lk8000.it>
2015-02-28 01:36:37 +0100

    Temporary fix for DrawRunway on all resolutions. Trying to rescale the waypoint circle, runway size, i spent some good time finding no real solution. I believe it is easier to write a de-orbiting program in RPN than fixing this function, which ought to be rewritten keeping in mind scalability. But so far, so good!

--- Common/Source/Draw/DrawRunway.cpp
+++ Common/Source/Draw/DrawRunway.cpp
@@ -11,7 +11,7 @@
 #include "DoInits.h"
 #include "LKObjects.h"
 #include <string.h>
-
+#include "ScreenGeometry.h"
 
 //
 // THIS FUNCTION is threadsafe only if called by dialogs using picto bool true
@@ -46,32 +46,70 @@ void MapWindow::DrawRunway(LKSurface& Surface, const WAYPOINT* wp, const RECT& r
 
   if (DoInit[MDI_MAPWPVECTORS])
   {
+    //
+    // How long and thick is the runway drawn, and how big is the circle.
+    // No rule possible, it depends on how we are dealing with many other
+    // custom things on screen, because LK is drawing differently depending
+    // on geometry, resolution, orientation. We dont split bits.
+    //
     switch(ScreenSize)
     {
-     case ss240x320: rwl = 9.0; rwb = 2.0;cir = 4.0; break;
-     case ss240x400: rwl = 9.0; rwb = 1.0;cir = 4.0; break;
-     case ss272x480: rwl = 9.0; rwb = 2.5;cir = 4.0; break;
-     case ss480x640: rwl = 6.0; rwb = 2.5;cir = 5.0; break;
-     case ss480x800: rwl = 6.0; rwb = 2.5;cir = 5.0; break;
-     case sslandscape: rwl = 6.0; rwb = 1.0;cir = 5.0; break;
-     case ss320x240: rwl = 9.0; rwb = 2.0;cir = 4.0; break;
-     case ss400x240: rwl = 9.0; rwb = 1.0;cir = 4.0; break;
-     case ss480x234: rwl = 9.0; rwb = 1.0;cir = 4.0; break;
-     case ss480x272: rwl = 9.0; rwb = 2.5;cir = 4.0; break;
-     case ss640x480: rwl = 6.0; rwb = 2.5;cir = 5.0; break;
-     case ss720x408: rwl = 6.0; rwb = 2.5;cir = 5.0; break;
-     case ss800x480: rwl = 6.0; rwb = 2.5;cir = 5.0; break;
-     case ss896x672: rwl = 6.0; rwb = 2.5;cir = 5.0; break;
+     case ss240x320: rwl = 9.0; rwb = 2.0;cir = 4.0; break; // 43
+     case ss240x400: rwl = 9.0; rwb = 1.0;cir = 4.0; break; // 53
+     case ss272x480: rwl = 9.0; rwb = 2.5;cir = 4.0; break; // 169
+     case ss480x640: rwl = 6.0; rwb = 2.5;cir = 5.0; break; // 43
+     case ss480x800: rwl = 6.0; rwb = 2.5;cir = 5.0; break; // 53
+     case sslandscape: rwl = 6.0; rwb = 1.0;cir = 5.0; break; // sslandscape is never assigned!
+     case ss320x240: rwl = 9.0; rwb = 2.0;cir = 4.0; break; // 43
+     case ss400x240: rwl = 9.0; rwb = 1.0;cir = 4.0; break; // 53
+     case ss480x234: rwl = 9.0; rwb = 1.0;cir = 4.0; break; // 21
+     case ss480x272: rwl = 9.0; rwb = 2.5;cir = 4.0; break; // 169
+     case ss640x480: rwl = 6.0; rwb = 2.5;cir = 5.0; break; // 43
+     case ss720x408: rwl = 6.0; rwb = 2.5;cir = 5.0; break; // 169
+     case ss800x480: rwl = 6.0; rwb = 2.5;cir = 5.0; break; // 53
+     case ss896x672: rwl = 6.0; rwb = 2.5;cir = 5.0; break; // 43
      case ssnone:
-	if (ScreenLandscape) {
-		rwl = 6.0; rwb=2.5; cir=4.0;
-	} else {
-		rwl = 9.0; rwb=2.5; cir=4.0;
-	}
-	break;
+
+         #define X ScreenSizeX==
+	 #define Y ScreenSizeY==
+         // Ok this doesnt look nice, but lets remember we are inside a DoInit.
+         // Generally the generic setup is good for everyone, but in case we 
+         // can make it custom for any resolution.
+         if (X 1024 && Y 768) {; rwl = 4.5; rwb = 2.5; cir = 5.0; } else
+         if (X 1014 && Y 758) {; rwl = 4.5; rwb = 2.5; cir = 5.0; } else
+
+         // .. and this is the rule of thumb, I could not find a better idea
+         // I think it is easier to redesign the drawing approach and make it
+         // really scalable. 
+         //
+         // This hack works for: 800x600 960x540 1280x720 (and relative portrait modes)
+         // These are the screen resolutions used by kobo, samsung s4 mini and s5 mini
+         // 
+         if (ScreenLandscape) {
+             if (ScreenSizeX<=480) {
+		 // testlk> Ok: t5     Bad for: -
+                 rwl = 9.0*(480.0/ScreenSizeX); rwb = 2.5*(480.0/ScreenSizeX); cir = 4.0;
+             } else {
+		 // testlk> Ok:  3,13,14,22    Bad: t4, t15, t23
+                 rwl = 6.0; rwb = 2.5; cir = 5.0;
+             }
+         } else {
+             if (ScreenSizeX<480) {
+		 // testlk> Ok: 10,19     Bad for: 20
+                 rwl = 6.0*(480.0/ScreenSizeX); rwb = 2.5*(480.0/ScreenSizeX); cir = 4.0;
+             } else {
+		 // testlk> Ok: 8,28     Bad for: -
+                 rwl = 6.0; rwb = 2.5; cir = 5.0;
+             }
+         }
+         break;
     }
 
-    // All values <=
+    // 
+    // These are the (absolute) scale thresholds for painting informations
+    // For example, paint just radio starting from 5.0km (3.6abs) etc.
+    // There is no rule possible, it matters only testing. 
+    //
     switch(ScreenSize)
     {
 
@@ -99,11 +137,14 @@ void MapWindow::DrawRunway(LKSurface& Surface, const WAYPOINT* wp, const RECT& r
 		break;
 
 	default:
-		scale_drawradio=2.6;
-		scale_bigfont=1.5;
-		scale_fullinfos=1.5;
-		break;
+
+                scale_drawradio=3.6;
+                scale_bigfont=1.5;
+                scale_fullinfos=1.5;
+                break;
     }
+
+
     DoInit[MDI_MAPWPVECTORS]=false;
   }
 
@@ -111,8 +152,13 @@ void MapWindow::DrawRunway(LKSurface& Surface, const WAYPOINT* wp, const RECT& r
   {
     if (picto)
        l = (int) (rwl);
-    else 
-       l = (int) (rwl * (1.0+ ((double)wp->RunwayLen/800.0-1.0)/4.0));
+    else {
+       // We cant rescale runways , no method found.
+       if (ScreenSize==ssnone)
+           l = (int) (rwl * (1.0+ (680.0/800.0-1.0)/4.0)); // wtf..!
+       else
+           l = (int) (rwl * (1.0+ ((double)wp->RunwayLen/800.0-1.0)/4.0));
+    }
 
     b = (int) (rwb/1.5 );
   } else
