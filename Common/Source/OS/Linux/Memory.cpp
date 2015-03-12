8b0ae767b119115eb5c4c269572f06852f978ebe
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2015-03-12 20:19:24 +0100

    Implement CheckFreeRam() for Linux platform.

--- Common/Source/OS/Linux/Memory.cpp
+++ Common/Source/OS/Linux/Memory.cpp
@@ -10,9 +10,14 @@
  */
 
 #include <stddef.h>
+#include <sys/sysinfo.h>
 
 
 size_t CheckFreeRam(void) {
+    struct sysinfo info = {};
+    if(sysinfo(&info) == 0) {
+        return info.freeram *(unsigned long long)info.mem_unit;
+    }
     return ~((size_t)0);
 }
 
