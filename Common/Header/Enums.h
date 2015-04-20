691916fb507c8113f7055932d666d6f8e62c00c0
Paolo Ventafridda <venta@lk8000.it>
2015-04-20 02:27:21 +0200

    Add ss800x600 and ss600x800 enums. Kobo will soon get custom setup for its resolution to make it better.

--- Common/Header/Enums.h
+++ Common/Header/Enums.h
@@ -23,6 +23,7 @@ typedef enum{
   ss272x480,
   ss480x640,
   ss480x800,
+  ss600x800,
   sslandscape, //  <landscape=portrait modes, >landscape=landscape modes
   ss320x240, 
   ss400x240,
@@ -32,6 +33,7 @@ typedef enum{
   ss720x408,
   ss800x480,
   ss896x672
+  ss800x600,
 }ScreenSize_t;
 
 typedef enum{
