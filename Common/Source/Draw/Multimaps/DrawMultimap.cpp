423311092f9549b2090f301691b9c89c4b835509
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2015-04-30 22:30:47 +0200

    DrawMultimap_SideTopSeparator : use rci Parameter instead of DrawRect for DrawLine

--- Common/Source/Draw/Multimaps/DrawMultimap.cpp
+++ Common/Source/Draw/Multimaps/DrawMultimap.cpp
@@ -4,23 +4,24 @@
    See CREDITS.TXT file for authors and copyrights
 
    $Id$
-*/
+ */
 #include "externs.h"
 #include "MapWindow.h"
 #include "Multimap.h"
 #include "Sideview.h"
 
-//
-// Simple thin black line separator between side and topview, if needed
-//
+/**
+ * @brief Draw simple thin black line separator between side and topview, if needed
+ * @param Surface : Surface to draw
+ * @param rci : TopView RECT, line is draw on bottom side of this RECT
+ */
 void MapWindow::DrawMultimap_SideTopSeparator(LKSurface& Surface, const RECT& rci) {
-  if ( DrawRect.bottom>0 && Current_Multimap_SizeY<SIZE4 ) {
-	POINT line[2];
-	line[0].x = rci.left;
-	line[1].x = rci.right;
-	line[0].y=DrawRect.bottom;
-	line[1].y=DrawRect.bottom;
-        Surface.DrawLine(PEN_SOLID, 1, line[0], line[1], RGB_BLACK,rci);
-  }
+    if (rci.bottom > 0 && Current_Multimap_SizeY < SIZE4) {
+        const POINT line[2] = {
+            { rci.left, rci.bottom - 1}, 
+            { rci.right, rci.bottom - 1}
+        };
+        Surface.DrawLine(PEN_SOLID, 1, line[0], line[1], RGB_BLACK, rci);
+    }
 }
 
