8da8b5e91e1702e16f43cf56f43667eacbdf23ba
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2015-07-28 20:55:54 +0200

    fix WndForm Layout child window can't be moved inside onPaint because on WinCE WM_PAINT is not send if Child window completly overlap Parent. work on V5 because all WndForm always have 1px wide line on top not overlaped by child
    fix #724

--- Common/Header/WindowControls.h
+++ Common/Header/WindowControls.h
@@ -538,8 +538,8 @@ class WindowControl : public WndCtrlBase {
     LKColor mColorBack;
     LKColor mColorFore;
     LKBrush mBrushBk;
+    LKBrush mBrushBorder;
 
-    PenReference mhPenBorder;
     PenReference mhPenSelector;
 
     FontReference mhFont;
@@ -550,11 +550,8 @@ class WindowControl : public WndCtrlBase {
     int mTag;
     bool mReadOnly;
 
-    int  mBorderSize;
-
     static int InstCount;
 
-    static PenReference hPenDefaultBorder;
     static PenReference hPenDefaultSelector;
 
   protected:
@@ -604,7 +601,7 @@ class WindowControl : public WndCtrlBase {
     bool SetReadOnly(bool Value);
 
     int  GetBorderKind(void);
-    int  SetBorderKind(int Value);
+    virtual int  SetBorderKind(int Value);
 
     FontReference GetFont(void) const {return(mhFont);};
     virtual void SetFont(FontReference Value);
@@ -625,8 +622,7 @@ class WindowControl : public WndCtrlBase {
         }
     }
 
-    PenReference GetBorderPen() const {return(mhPenBorder);};
-    void SetBorderPen(const LKPen& Pen) { mhPenBorder = Pen;}
+    void SetBorderColor(const LKColor& Color) { mBrushBorder.Create(Color);}
 
     PenReference GetSelectorPen(void) const {return(mhPenSelector);};
 
@@ -882,6 +878,8 @@ class WndForm:public WindowControl{
     void Show();
 
     void SetCaption(const TCHAR *Value);
+
+    int  SetBorderKind(int Value);
 
     LKColor SetForeColor(const LKColor& Value);
     LKColor SetBackColor(const LKColor& Value);
