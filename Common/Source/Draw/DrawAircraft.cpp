175f11a63dec5e4bcd11d451f878a1e55aeb1169
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2015-09-02 22:42:47 +0200

    refactoring aircraft symbol on moving map for glider & pg mode. (GA are already nice)

--- Common/Source/Draw/DrawAircraft.cpp
+++ Common/Source/Draw/DrawAircraft.cpp
@@ -4,162 +4,159 @@
    See CREDITS.TXT file for authors and copyrights
 
    $Id$
-*/
+ */
 
 #include "externs.h"
 #include "LKObjects.h"
 #include "Multimap.h"
 
+void MapWindow::DrawAircraft(LKSurface& Surface, const POINT& Orig) {
 
-void MapWindow::DrawAircraft(LKSurface& Surface, const POINT& Orig)
-{
+    const double angle = DisplayAircraftAngle + (DerivedDrawInfo.Heading - DrawInfo.TrackBearing);
 
-  if ( ISPARAGLIDER || ISCAR ) {
+    if (ISPARAGLIDER || ISCAR) {
 
-    #define NUMPARAPOINTS 3
+        POINT AircraftInner[] = {
+            { 0, -5},
+            {5, 9},
+            {-5, 9}
+        };
+        POINT AircraftOuter[] = {
+            { 0, -6},
+            {6, 10},
+            {-6, 10}
+        };
+        
+        BrushReference hbPAircraftSolid; 
+        BrushReference hbPAircraftSolidBg;
 
-    POINT Para[3] = {
-      { 0,-5},
-      {5,9},
-      {-5,9}
-    };
+        if (BlackScreen) {
+          hbPAircraftSolid = LK_WHITE_BRUSH;
+          hbPAircraftSolidBg = LKBrush_LightCyan;
+        } else {
+          hbPAircraftSolid = LK_WHITE_BRUSH;
+          hbPAircraftSolidBg = LKBrush_Blue;
+        }        
 
-    int pi;
-    BrushReference hbPAircraftSolid; 
-    BrushReference hbPAircraftSolidBg;
+        PolygonRotateShift(AircraftInner, array_size(AircraftInner), Orig.x, Orig.y, angle);
+        PolygonRotateShift(AircraftOuter, array_size(AircraftOuter), Orig.x, Orig.y, angle);
 
-    if (BlackScreen) {
-      hbPAircraftSolid = LKBrush_LightCyan;
-      hbPAircraftSolidBg = LKBrush_Blue;
+        const auto oldPen = Surface.SelectObject(LK_NULL_PEN);
+
+        const auto hbOld = Surface.SelectObject(hbPAircraftSolid);
+        Surface.Polygon(AircraftOuter, array_size(AircraftOuter));
+
+        Surface.SelectObject(hbPAircraftSolidBg);
+        Surface.Polygon(AircraftInner, array_size(AircraftInner));
+
+        Surface.SelectObject(oldPen);
+        Surface.SelectObject(hbOld);        
+
+    } else if (ISGAAIRCRAFT) {
+
+        POINT Aircraft[] = {
+            { 1, -6},
+            {2, -1},
+            {15, 0},
+            {15, 2},
+            {1, 2},
+            {0, 10},
+            {4, 11},
+            {4, 12},
+            {-4, 12},
+            {-4, 11},
+            {0, 10},
+            {-1, 2},
+            {-15, 2},
+            {-15, 0},
+            {-2, -1},
+            {-1, -6}
+        };
+
+        BrushReference hbAircraftSolid = LKBrush_Black;
+        BrushReference hbAircraftSolidBg = LKBrush_White;
+
+        const auto hbOld = Surface.SelectObject(hbAircraftSolidBg);
+        const auto hpOld = Surface.SelectObject(hpAircraft);
+
+        PolygonRotateShift(Aircraft, array_size(Aircraft), Orig.x, Orig.y, angle);
+
+        Surface.Polygon(Aircraft, array_size(Aircraft));
+
+        // draw it again so can get white border
+        Surface.SelectObject(LKPen_White_N2);
+        Surface.SelectObject(hbAircraftSolid);
+
+        for (unsigned i = 0; i < array_size(Aircraft); i++) {
+            Aircraft[i].x -= 1;
+            Aircraft[i].y -= 1;
+        }
+
+        Surface.Polygon(Aircraft, array_size(Aircraft));
+
+        Surface.SelectObject(hpOld);
+        Surface.SelectObject(hbOld);
+
+        return;
+
     } else {
-      hbPAircraftSolid = LKBrush_Blue;
-      hbPAircraftSolidBg = LKBrush_Grey;
+
+        // GLIDER AICRAFT NORMAL ICON
+
+        POINT AircraftInner[] = {
+            {1, -5},
+            {1, 0},
+            {14, 0},
+            {14, 1},
+            {1, 1},
+            {1, 8},
+            {4, 8},
+            {4, 9},
+            {-3, 9},
+            {-3, 8},
+            {0, 8},
+            {0, 1},
+            {-13, 1},
+            {-13, 0},
+            {0, 0},
+            {0, -5},
+            {1, -5},
+        };
+        POINT AircraftOuter[] = {
+            {2, -6},
+            {2, -1},
+            {15, -1},
+            {15, 2},
+            {2, 2},
+            {2, 7},
+            {5, 7},
+            {5, 10},
+            {-4, 10},
+            {-4, 7},
+            {-1, 7},
+            {-1, 2},
+            {-14, 2},
+            {-14, -1},
+            {-1, -1},
+            {-1, -6},
+            {2, -6},
+        };
+
+        PolygonRotateShift(AircraftInner, array_size(AircraftInner), Orig.x, Orig.y, angle);
+        PolygonRotateShift(AircraftOuter, array_size(AircraftOuter), Orig.x, Orig.y, angle);
+
+        const auto oldPen = Surface.SelectObject(LK_NULL_PEN);
+
+        const auto hbOld = Surface.SelectObject(LK_BLACK_BRUSH);
+        Surface.Polygon(AircraftOuter, array_size(AircraftOuter));
+
+        Surface.SelectObject(LK_WHITE_BRUSH);
+        Surface.Polygon(AircraftInner, array_size(AircraftInner));
+
+        Surface.SelectObject(oldPen);
+        Surface.SelectObject(hbOld);
+
     }
-
-    const auto hbPOld = Surface.SelectObject(hbPAircraftSolidBg);
-    const auto hpPOld = Surface.SelectObject(LKPen_White_N3);
-  
-    PolygonRotateShift(Para, NUMPARAPOINTS, Orig.x+1, Orig.y+1,
-                       DisplayAircraftAngle+
-                       (DerivedDrawInfo.Heading-DrawInfo.TrackBearing));
-
-    Surface.Polygon(Para, NUMPARAPOINTS);
-
-    // draw it again so can get white border
-    Surface.SelectObject(LKPen_Black_N1);
-    Surface.SelectObject(hbPAircraftSolid);
-
-    for(pi=0; pi<NUMPARAPOINTS; pi++)
-      {
-	Para[pi].x -= 1;  Para[pi].y -= 1;
-      }
-
-    Surface.Polygon(Para, NUMPARAPOINTS);
-
-    Surface.SelectObject(hpPOld);
-    Surface.SelectObject(hbPOld);
-
-    return;
-  }
-
-  if ( ISGAAIRCRAFT ) {
-
-#define NUMAIRCRAFTPOINTS 16
-
-    POINT Aircraft[NUMAIRCRAFTPOINTS] = {
-      { 1,-6},
-      {2,-1},
-      {15,0},
-      {15,2},
-      {1,2},
-      {0,10},
-      {4,11},
-      {4,12},
-      {-4,12},
-      {-4,11},
-      {0,10},
-      {-1,2},
-      {-15,2},
-      {-15,0},
-      {-2,-1},
-      {-1,-6}
-    };
-
-    int i;
-
-    BrushReference hbAircraftSolid = LKBrush_Black;
-    BrushReference hbAircraftSolidBg = LKBrush_White;
-
-    const auto hbOld = Surface.SelectObject(hbAircraftSolidBg);
-    const auto hpOld = Surface.SelectObject(hpAircraft);
-  
-    PolygonRotateShift(Aircraft, NUMAIRCRAFTPOINTS, Orig.x+1, Orig.y+1,
-                       DisplayAircraftAngle+
-                       (DerivedDrawInfo.Heading-DrawInfo.TrackBearing));
-
-    Surface.Polygon(Aircraft, NUMAIRCRAFTPOINTS);
-
-    // draw it again so can get white border
-    Surface.SelectObject(LKPen_White_N2);
-    Surface.SelectObject(hbAircraftSolid);
-
-    for(i=0; i<NUMAIRCRAFTPOINTS; i++)
-      {
-	Aircraft[i].x -= 1;  Aircraft[i].y -= 1;
-      }
-
-    Surface.Polygon(Aircraft, NUMAIRCRAFTPOINTS);
-
-    Surface.SelectObject(hpOld);
-    Surface.SelectObject(hbOld);
-    
-    return;
-
-  }
-
-      // GLIDER AICRAFT NORMAL ICON
-
-      POINT Aircraft[] = {
-	{1, -5},
-	{1, 0},
-	{14, 0}, 
-	{14, 1}, 
-	{1, 1},
-	{1, 8},
-	{4, 8},
-	{4, 9},
-	{-3, 9},
-	{-3, 8},
-	{0, 8},
-	{0, 1},
-	{-13, 1}, 
-	{-13, 0}, 
-	{0, 0},
-	{0, -5},
-	{1, -5},
-      };
-
-      int n = sizeof(Aircraft)/sizeof(Aircraft[0]);
-
-      double angle = DisplayAircraftAngle+
-	(DerivedDrawInfo.Heading-DrawInfo.TrackBearing);
-
-      PolygonRotateShift(Aircraft, n,
-			 Orig.x-1, Orig.y, angle);
-
-      const auto oldPen = Surface.SelectObject(hpAircraft); 
-      const auto hbOld = Surface.SelectObject(LK_WHITE_BRUSH);
-      Surface.Polygon(Aircraft, n);
-      
-      Surface.SelectObject(LKBrush_Black);
-      Surface.SelectObject(LKPen_White_N2);
-      
-      Surface.Polygon(Aircraft, n);
-
-      Surface.SelectObject(oldPen);
-      Surface.SelectObject(hbOld);
-
-
 }
 
 
