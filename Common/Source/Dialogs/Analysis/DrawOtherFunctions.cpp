6a895d5e879d8a8f6911f6395bc9da35cc7be533
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2015-12-23 21:41:45 +0100

    Statistics::DrawFilledLineGraph : fix Polygon

--- Common/Source/Dialogs/Analysis/DrawOtherFunctions.cpp
+++ Common/Source/Dialogs/Analysis/DrawOtherFunctions.cpp
@@ -195,19 +195,21 @@ void Statistics::DrawFilledLineGraph(LKSurface& Surface, const RECT& rc,
 				     LeastSquares* lsdata,
 				     const LKColor& color) {
 
-  POINT line[4];
+  static std::vector<RasterPoint> line;
+  line.resize(lsdata->sum_n + 3);
 
-  for (int i=0; i<lsdata->sum_n-1; i++) {
-    line[0].x = (int)((lsdata->xstore[i]-x_min)*xscale)+rc.left+BORDER_X;
-    line[0].y = (int)((y_max-lsdata->ystore[i])*yscale)+rc.top;
-    line[1].x = (int)((lsdata->xstore[i+1]-x_min)*xscale)+rc.left+BORDER_X;
-    line[1].y = (int)((y_max-lsdata->ystore[i+1])*yscale)+rc.top;
-    line[2].x = line[1].x;
-    line[2].y = rc.bottom-BORDER_Y;
-    line[3].x = line[0].x;
-    line[3].y = rc.bottom-BORDER_Y;
-    Surface.Polygon(line, 4);
+  for (int i=0; i<lsdata->sum_n; i++) {
+    line[i].x = ((lsdata->xstore[i]-x_min)*xscale)+rc.left+BORDER_X;
+    line[i].y = ((y_max-lsdata->ystore[i])*yscale)+rc.top;
   }
+  
+  line[lsdata->sum_n].x = line[lsdata->sum_n - 1].x;
+  line[lsdata->sum_n].y = rc.bottom-BORDER_Y;
+  line[lsdata->sum_n+1].x = line[0].x;
+  line[lsdata->sum_n+1].y = rc.bottom-BORDER_Y;
+  line[lsdata->sum_n+2] = line[0];
+  
+  Surface.Polygon(line.data(), line.size());
 }
 
 
