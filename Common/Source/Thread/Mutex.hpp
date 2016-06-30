3157bb2e902419d1e095036bf2874c9a057b3681
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2016-06-30 21:02:34 +0200

    add missed License Header

--- Common/Source/Thread/Mutex.hpp
+++ Common/Source/Thread/Mutex.hpp
@@ -1,6 +1,10 @@
-/* 
+/*
+ * LK8000 Tactical Flight Computer -  WWW.LK8000.IT
+ * Released under GNU/GPL License v.2
+ * See CREDITS.TXT file for authors and copyrights
+ * 
  * File:   Mutex.hpp
- * Author: user
+ * Author: Bruno de Lacheisserie
  *
  * Created on March 3, 2015, 2:20 PM
  */
