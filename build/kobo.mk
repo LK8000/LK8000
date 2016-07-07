9ec0eb77ad2ff0c324419cbada3272447ca554ec
Paolo Ventafridda <venta@lk8000.it>
2016-07-08 01:19:28 +0200

    Revert "makefile update : build 2 "update" archive for kobo"
    This reverts commit e0f881f4c9da0487a31a992f9ac5e345d0e8d91c.
    
    Sorry Bruno, my fault.

--- build/kobo.mk
+++ build/kobo.mk
@@ -94,6 +94,8 @@ $(Q)install -m 0644 $(SYSTEM_FILES) $(BIN)/$(1)/KoboRoot/opt/LK8000/share/_Syste
 $(Q)install -m 0644 $(BITMAP_FILES) $(BIN)/$(1)/KoboRoot/opt/LK8000/share/_System/_Bitmaps
 $(Q)install -m 0644 $(POLAR_FILES) $(BIN)/$(1)/KoboRoot/mnt/onboard/LK8000/_Polars
 $(Q)install -m 0644 $(LANGUAGE_FILES) $(BIN)/$(1)/KoboRoot/mnt/onboard/LK8000/_Language
+$(Q)install -m 0644 $(CONFIG_FILES) $(BIN)/$(1)/KoboRoot/mnt/onboard/LK8000/_Configuration
+$(Q)install -m 0644 $(WAYPOINT_FILES) $(BIN)/$(1)/KoboRoot/mnt/onboard/LK8000/_Waypoints
 endef
 
 
@@ -108,17 +110,6 @@ Kobo-install.zip: $(BIN)/std/.kobo/KoboRoot.tgz
 	$(Q)cd $(BIN)/std/ && zip -q $@ .kobo/KoboRoot.tgz
 	$(Q)cp $(BIN)/std/$@ $@
 
-Kobo-update-otg.zip: $(BIN)/otg-update/.kobo/KoboRoot.tgz
-	@$(NQ)echo "  Zip     $@"
-	$(Q)cd $(BIN)/otg-update/ && zip -q $@ .kobo/KoboRoot.tgz
-	$(Q)cp $(BIN)/otg-update/$@ $@
-
-
-Kobo-update.zip: $(BIN)/std-update/.kobo/KoboRoot.tgz
-	@$(NQ)echo "  Zip     $@"
-	$(Q)cd $(BIN)/std-update/ && zip -q $@ .kobo/KoboRoot.tgz
-	$(Q)cp $(BIN)/std-update/$@ $@
-
 
 # /mnt/onboard/.kobo/KoboRoot.tgz is a file that is picked up by
 # /etc/init.d/rcS, extracted to / on each boot; we can use it to
@@ -130,10 +121,6 @@ $(BIN)/std/.kobo/KoboRoot.tgz: $(OUTPUTS) $(KOBO_MENU_BIN) $(KOBO_POWER_OFF_BIN)
 	@$(NQ)echo "  TAR     $@"
 	
 	$(call build-kobofs,std)
-	
-	$(Q)install -m 0644 $(WAYPOINT_FILES) $(BIN)/std/KoboRoot/mnt/onboard/LK8000/_Waypoints
-	$(Q)install -m 0644 $(CONFIG_FILES) $(BIN)/std/KoboRoot/mnt/onboard/LK8000/_Configuration
-	
 	$(Q)install -m 0755 -d  $(BIN)/std/.kobo
 	$(Q)fakeroot tar czfC $@ $(BIN)/std/KoboRoot .
 
@@ -145,9 +132,6 @@ $(BIN)/otg/.kobo/KoboRoot.tgz: $(OUTPUTS) $(KOBO_MENU_BIN) $(KOBO_POWER_OFF_BIN)
 	
 	$(call build-kobofs,otg)
 
-	$(Q)install -m 0644 $(WAYPOINT_FILES) $(BIN)/otg/KoboRoot/mnt/onboard/LK8000/_Waypoints
-	$(Q)install -m 0644 $(CONFIG_FILES) $(BIN)/otg/KoboRoot/mnt/onboard/LK8000/_Configuration
-
 	$(Q)install -m 0755 -d  $(BIN)/otg/KoboRoot/lib/udev/rules.d
 	$(Q)install -m 0644 kobo/60-persistent-serial.rules $(BIN)/otg/KoboRoot/lib/udev/rules.d
 	$(Q)install -m 0755 -d  $(BIN)/otg/KoboRoot/mnt/onboard/LK8000/kobo
@@ -156,32 +140,6 @@ $(BIN)/otg/.kobo/KoboRoot.tgz: $(OUTPUTS) $(KOBO_MENU_BIN) $(KOBO_POWER_OFF_BIN)
 	$(Q)install -m 0755 -d  $(BIN)/otg/.kobo
 	$(Q)fakeroot tar czfC $@ $(BIN)/otg/KoboRoot .
 
-$(BIN)/std-update/.kobo/KoboRoot.tgz: $(OUTPUTS) $(KOBO_MENU_BIN) $(KOBO_POWER_OFF_BIN) \
-	$(SYSTEM_FILES) $(BITMAP_FILES) \
-	$(FONTS_FILES) $(POLAR_FILES) $(LANGUAGE_FILES) \
-	$(CONFIG_FILES)  kobo/inittab kobo/rcS
-	@$(NQ)echo "  TAR     $@"
-	
-	$(call build-kobofs,std-update)
-	
-	$(Q)install -m 0755 -d  $(BIN)/std-update/.kobo
-	$(Q)fakeroot tar czfC $@ $(BIN)/std-update/KoboRoot .
-
-$(BIN)/otg-update/.kobo/KoboRoot.tgz: $(OUTPUTS) $(KOBO_MENU_BIN) $(KOBO_POWER_OFF_BIN) \
-	$(SYSTEM_FILES) $(BITMAP_FILES) \
-	$(FONTS_FILES) $(POLAR_FILES) $(LANGUAGE_FILES) \
-	$(CONFIG_FILES)  kobo/inittab kobo/rcS kobo/60-persistent-serial.rules $(KOBO_KERNEL_SOURCES)
-	@$(NQ)echo "  TAR     $@"
-	
-	$(call build-kobofs,otg-update)
-
-	$(Q)install -m 0755 -d  $(BIN)/otg-update/KoboRoot/lib/udev/rules.d
-	$(Q)install -m 0644 kobo/60-persistent-serial.rules $(BIN)/otg-update/KoboRoot/lib/udev/rules.d
-	$(Q)install -m 0755 -d  $(BIN)/otg-update/KoboRoot/mnt/onboard/LK8000/kobo
-	$(Q)install -m 0644 $(KOBO_KERNEL_SOURCES) $(BIN)/otg-update/KoboRoot/mnt/onboard/LK8000/kobo
-
-	$(Q)install -m 0755 -d  $(BIN)/otg-update/.kobo
-	$(Q)fakeroot tar czfC $@ $(BIN)/otg-update/KoboRoot .
 
 $(KOBO_POWER_OFF_BIN) : $(KOBO_POWER_OFF_BIN)_ns
 	@$(NQ)echo "  STRIP   $@"
