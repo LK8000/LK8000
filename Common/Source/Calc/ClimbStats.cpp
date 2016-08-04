3f6476743acdce426b2d6a51cf08232fdb5cbb98
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2016-08-04 20:38:19 +0200

    use "climbAverageCalculator" for calculate "Vario Average 30s"

--- Common/Source/Calc/ClimbStats.cpp
+++ Common/Source/Calc/ClimbStats.cpp
@@ -7,100 +7,12 @@
 */
 
 #include "externs.h"
-#include "DoInits.h"
+#include "ClimbAverageCalculator.h"
 
-
-#ifdef NEWCLIMBAV
 ClimbAverageCalculator climbAverageCalculator;
 void Average30s(NMEA_INFO *Basic, DERIVED_INFO *Calculated)
 {
 	Calculated->Average30s = climbAverageCalculator.GetAverage(Basic->Time, Calculated->NavAltitude, 30);	
-}
-
-#endif
-
-void Average30s(NMEA_INFO *Basic, DERIVED_INFO *Calculated)
-{
-  static double LastTime = 0;
-  static double Altitude[30];
-  static double Vario[30];
-  int Elapsed, i;
-  long index = 0; 
-  double Gain;
-  static int num_samples = 0;
-  static bool lastCircling = false;
-
-  if (DoInit[MDI_AVERAGE30S]) {
-	LastTime = 0;
-	num_samples = 0;
-	lastCircling = false;
-	DoInit[MDI_AVERAGE30S]=false;
-  }
-
-  if(Basic->Time > LastTime)
-    {
-
-      if (Calculated->Circling != lastCircling) {
-        num_samples = 0;
-        // reset!
-      }
-      lastCircling = Calculated->Circling;
-
-      Elapsed = (int)(Basic->Time - LastTime);
-      for(i=0;i<Elapsed;i++)
-        {
-          index = (long)LastTime + i;
-          index %= 30;
-
-          Altitude[index] = Calculated->NavAltitude;
-	  if (Basic->VarioAvailable) {
-	    Vario[index] = Basic->Vario;
-	  } else {
-	    Vario[index] = Calculated->Vario;
-	  }
-
-          if (num_samples<30) {
-            num_samples ++;
-          }
-
-        }
-
-      double Vave = 0;
-      int j;
-      for (i=0; i< num_samples; i++) {
-        j = (index - i) % 30;
-        if (j<0) { 
-          j += 30;
-        }
-        Vave += Vario[j];
-      }
-      if (num_samples) {
-        Vave /= num_samples;
-      }
-
-      if (!Basic->VarioAvailable) {
-        index = ((long)Basic->Time - 1)%30;
-        Gain = Altitude[index];
-        
-        index = ((long)Basic->Time)%30;
-        Gain = Gain - Altitude[index];
-
-        Vave = Gain/30;
-      }
-      Calculated->Average30s = 
-        LowPassFilter(Calculated->Average30s,Vave,0.8);
-    }
-  else
-    {
-      if (Basic->Time<LastTime) {
-	// gone back in time
-	for (i=0; i<30; i++) {
-	  Altitude[i]= 0;
-	  Vario[i]=0;
-	}
-      }
-    }
-  LastTime = Basic->Time;
 }
 
 void AverageThermal(NMEA_INFO *Basic, DERIVED_INFO *Calculated)
