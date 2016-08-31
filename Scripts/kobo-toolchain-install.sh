a1b9468bd735b80c36aa328c8b5bfca326a67082
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2016-08-31 19:37:20 +0200

    update kobo-toolchain-install.sh  - libpng 1.6.25  - libfreetype 2.7  - boostlib 1.62.0
     - updated optimizer flags on all lib

--- Scripts/kobo-toolchain-install.sh
+++ Scripts/kobo-toolchain-install.sh
@@ -6,19 +6,19 @@
 # Created on Jan 1, 2015, 6:39:30 PM
 #
 
-mkdir /lk/tmp
-cd /lk/tmp
+mkdir ~/tmp
+cd ~/tmp
 
 # install toolchain
-wget http://max.kellermann.name/download/xcsoar/devel/x-tools/x-tools-arm-i386-2013-12-11.tar.xz
-tar xJfC x-tools-arm-i386-2013-12-11.tar.xz /home/user
-export PATH=/home/user/x-tools/arm-unknown-linux-gnueabi/bin:$PATH
+#wget http://max.kellermann.name/download/xcsoar/devel/x-tools/x-tools-arm-i386-2013-12-11.tar.xz
+#tar xJfC x-tools-arm-i386-2013-12-11.tar.xz /home/user
+#export PATH=/home/user/x-tools/arm-unknown-linux-gnueabi/bin:$PATH
 
 # install zlib ( 1.2.8 - 2013-04-28)
 wget http://zlib.net/zlib-1.2.8.tar.gz
 tar -xvzf zlib-1.2.8.tar.gz 
 cd zlib-1.2.8
-CC=arm-unknown-linux-gnueabi-gcc CFLAGS="-O3 -march=armv7-a -mfpu=neon -mfloat-abi=hard -ftree-vectorize" \
+CC=arm-unknown-linux-gnueabi-gcc CFLAGS="-O3 -march=armv7-a -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -funsafe-math-optimizations -funsafe-loop-optimizations" \
 ./configure --prefix=/opt/kobo/arm-unknown-linux-gnueabi
 make all
 sudo make install
@@ -29,35 +29,35 @@ wget http://liquidtelecom.dl.sourceforge.net/project/zziplib/zziplib13/0.13.62/z
 tar xvjf zziplib-0.13.62.tar.bz2 
 mkdir zzipbuild
 cd zzipbuild
-CFLAGS="-O2 -march=armv7-a -mfpu=neon -mfloat-abi=hard -ftree-vectorize" \
+CFLAGS="-O3 -march=armv7-a -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -funsafe-math-optimizations -funsafe-loop-optimizations" \
 ../zziplib-0.13.62/configure --host=arm-unknown-linux-gnueabi --target=arm-unknown-linux-gnueabi --prefix=/opt/kobo/arm-unknown-linux-gnueabi --with-zlib
 make all
 sudo PATH=/home/user/x-tools/arm-unknown-linux-gnueabi/bin:$PATH \
     make install
 cd ..
 
-# install boostlib ( 1.60.0 - 2015-12-17 )
-wget http://netcologne.dl.sourceforge.net/project/boost/boost/1.61.0/boost_1_61_0.tar.gz
-tar xzf boost_1_61_0.tar.gz
-cd boost_1_61_0
+# install boostlib ( 1.62.0 - 2016-09-28 )
+wget http://netcologne.dl.sourceforge.net/project/boost/boost/1.62.0/boost_1_62_0.tar.gz
+tar xzf boost_1_62_0.tar.gz
+cd boost_1_62_0
 ./bootstrap.sh
-echo "using gcc : arm : arm-unknown-linux-gnueabi-g++ : cxxflags=-march=armv7-a -mfpu=neon -mfloat-abi=hard -ftree-vectorize ;" > user-config.jam
+echo "using gcc : arm : arm-unknown-linux-gnueabi-g++ : cxxflags=-O3 -march=armv7-a -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -funsafe-math-optimizations -funsafe-loop-optimizations ;" > user-config.jam
 sudo PATH=/home/user/x-tools/arm-unknown-linux-gnueabi/bin:$PATH \
     ./bjam toolset=gcc target-os=linux variant=release link=shared runtime-link=shared --prefix=/opt/kobo/arm-unknown-linux-gnueabi --user-config=user-config.jam install
 cd ..
 
-# install libpng ( 1.6.21 - 2016-01-15 )
-wget http://sourceforge.net/projects/libpng/files/libpng16/1.6.22/libpng-1.6.22.tar.gz
-tar xzf libpng-1.6.22.tar.gz
+# install libpng ( 1.6.25 - 2016-09-01 )
+wget http://sourceforge.net/projects/libpng/files/libpng16/1.6.25/libpng-1.6.25.tar.gz
+tar xzf libpng-1.6.25.tar.gz
 mkdir libpng-build
 cd libpng-build
-../libpng-1.6.22/configure \
+../libpng-1.6.25/configure \
     --host=arm-unknown-linux-gnueabi \
     CC=arm-unknown-linux-gnueabi-gcc \
     AR=arm-unknown-linux-gnueabi-ar \
     STRIP=arm-unknown-linux-gnueabi-strip \
     RANLIB=arm-unknown-linux-gnueabi-ranlib \
-    CPPFLAGS="-O2 -march=armv7-a -mfpu=neon -mfloat-abi=hard -ftree-vectorize -I/opt/kobo/arm-unknown-linux-gnueabi/include" \
+    CPPFLAGS="-O3 -march=armv7-a -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -funsafe-math-optimizations -funsafe-loop-optimizations -I/opt/kobo/arm-unknown-linux-gnueabi/include" \
     LDFLAGS="-L/opt/kobo/arm-unknown-linux-gnueabi/lib"  \
     --prefix=/opt/kobo/arm-unknown-linux-gnueabi 
 make
@@ -65,18 +65,18 @@ sudo PATH=/home/user/x-tools/arm-unknown-linux-gnueabi/bin:$PATH \
     make install
 cd ..
 
-# install freetype2 ( 2.6.3 - 2016-02-09 )
-wget http://download.savannah.gnu.org/releases/freetype/freetype-2.6.3.tar.gz
-tar xzf freetype-2.6.3.tar.gz
+# install freetype2 ( 2.7 - 2016-09-08 )
+wget http://download.savannah.gnu.org/releases/freetype/freetype-2.7.tar.gz
+tar xzf freetype-2.7.tar.gz
 mkdir freetype-build
 cd freetype-build
-CFLAGS="-O2 -march=armv7-a -mfpu=neon -mfloat-abi=hard -ftree-vectorize" \
+CFLAGS="-O3 -march=armv7-a -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -funsafe-math-optimizations -funsafe-loop-optimizations" \
 LDFLAGS="-L/opt/kobo/arm-unknown-linux-gnueabi/lib"  \
-../freetype-2.6.3/configure \
+../freetype-2.7/configure \
     --host=arm-unknown-linux-gnueabi \
     --target=arm-unknown-linux-gnueabi \
     --prefix=/opt/kobo/arm-unknown-linux-gnueabi \
-    --without-harfbuzz --without-png \
+    --without-harfbuzz \
     PKG_CONFIG_LIBDIR=/opt/kobo/arm-unknown-linux-gnueabi/lib/pkgconfig
 make
 sudo PATH=/home/user/x-tools/arm-unknown-linux-gnueabi/bin:$PATH \
