172fb20e95eefca0cf61154fb93223b4560aea3a
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2016-11-29 23:49:45 +0100

    android studio project : no need symlinks for AndroidManifest.xml file.

--- android-studio/prepare.sh
+++ android-studio/prepare.sh
@@ -3,12 +3,8 @@
 
 
 rm -rf app/src/main/res
-rm -rf app/src/main/java
 
 mkdir -p app/src/main/res/drawable app/src/main/assets/language/ 
-
-ln -rfs ../android/AndroidManifest.xml app/src/main/AndroidManifest.xml
-ln -rfs ../android/custom_rules.xml app/src/main/custom_rules.xml
 
 ln -rfs  ../android/res/values app/src/main/res/values
 ln -rfs  ../android/res/xml app/src/main/res/xml
