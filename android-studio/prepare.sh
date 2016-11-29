cddd0c894804145593ec39e3f0a73f96fabf7207
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2016-11-29 23:46:29 +0100

    android studio project : no need symlinks for cpp source file.

--- android-studio/prepare.sh
+++ android-studio/prepare.sh
@@ -26,6 +26,3 @@ ln -rfs  ../android/res/values app/src/main/res/values
 ln -rfs  ../android/res/xml app/src/main/res/xml
 ln -rfs  ../Common/Data/Bitmaps/LKicon.png app/src/main/res/drawable/icon.png
 ln -rfs  ../Common/Data/Bitmaps/LKicon.png app/src/main/res/drawable/notification_icon.png
-
-
-ln -rfs  ../Common app/src/main/cpp/Common
