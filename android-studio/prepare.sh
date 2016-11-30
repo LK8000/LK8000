b7e5aa8b0334cc1c470bd2dbb506f8e12489c553
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2016-11-30 16:49:18 +0100

    Add DEFAULT_MENU.TXT to android assets (2)

--- android-studio/prepare.sh
+++ android-studio/prepare.sh
@@ -4,7 +4,7 @@
 
 rm -rf app/src/main/res
 
-mkdir -p app/src/main/res/drawable app/src/main/assets/language/ 
+mkdir -p app/src/main/res/drawable
 
 ln -rfs  ../Common/Data/Bitmaps/LKicon.png app/src/main/res/drawable/icon.png
 ln -rfs  ../Common/Data/Bitmaps/LKicon.png app/src/main/res/drawable/notification_icon.png
