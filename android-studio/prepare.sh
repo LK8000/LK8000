0fd3012b92f49992311437d47388cdc5abfdfa2d
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2016-11-30 15:41:45 +0100

    android studio project : no need symlinks for "res/values" and "res/xml" directory

--- android-studio/prepare.sh
+++ android-studio/prepare.sh
@@ -6,7 +6,5 @@ rm -rf app/src/main/res
 
 mkdir -p app/src/main/res/drawable app/src/main/assets/language/ 
 
-ln -rfs  ../android/res/values app/src/main/res/values
-ln -rfs  ../android/res/xml app/src/main/res/xml
 ln -rfs  ../Common/Data/Bitmaps/LKicon.png app/src/main/res/drawable/icon.png
 ln -rfs  ../Common/Data/Bitmaps/LKicon.png app/src/main/res/drawable/notification_icon.png
