ae3cc3dc85c060ead59f5425e4cc2b82d7104212
Paolo Ventafridda <venta@lk8000.it>
2017-01-08 23:24:50 +0100

    Define LK_REFERENCE_DPI as 140 inside ScreenGeometry . First step before creating globals and functions for fast pixel rescaling

--- Common/Header/ScreenGeometry.h
+++ Common/Header/ScreenGeometry.h
@@ -12,6 +12,7 @@
 #ifndef SCREENGEO_H
 #define SCREENGEO_H
 
+
 #define SCREEN_GEOMETRY_INVALID  0
 
 //
@@ -29,6 +30,12 @@
 // In case of Unknown geometry we use L: 480x272 (272)  P: 272x480 (480)
 //
 #define SCREEN_GEOMETRY_COUNT    6
+
+// Many graphic functions in LK were tuned around low DPI devices, with the most tuned being 
+// 480x272 at around 110 DPI. 800x480 was also managed properly at 186 DPI.
+// Here we fix a reference DPI to rescale pixel sizes upon.
+#define LK_REFERENCE_DPI  140
+
 
 extern unsigned short GetScreenGeometry(unsigned int x, unsigned int y);
 extern double GetScreen0Ratio(void);
