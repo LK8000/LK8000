4d679833c3eda5b1b079228118bb72093dfa52d4
Paolo Ventafridda <venta@lk8000.it>
2017-03-06 04:22:36 +0100

    Led buttons flip-flopping

--- Common/Source/WindowControls.cpp
+++ Common/Source/WindowControls.cpp
@@ -2088,6 +2088,7 @@ void WndButton::Paint(LKSurface& Surface){
   Surface.DrawPushButton(rc, mDown);
 
 
+  static bool flipflop=true; // remember to flip only after showing a focused item!
   if (mLedMode) {
      RECT lrc={rc.left+(LEDBUTTONBORDER-1),rc.bottom-(LEDBUTTONBORDER+DLGSCALE(mLedSize)),
                rc.right-LEDBUTTONBORDER,rc.bottom-LEDBUTTONBORDER};
@@ -2096,14 +2097,25 @@ void WndButton::Paint(LKSurface& Surface){
         case LEDMODE_REDGREEN:
            if (IsDithered())
               lcol=(mLedOnOff?LEDCOLOR_LGREEN:LEDCOLOR_ORANGE);
-           else
-              lcol=(mLedOnOff?LEDCOLOR_GREEN:LEDCOLOR_RED);
+           else {
+              if (HasFocus()) {
+                 lcol=(flipflop?LEDCOLOR_YELLOW:LEDCOLOR_BLACK);
+                 flipflop=!flipflop;
+              } else
+                lcol=(mLedOnOff?LEDCOLOR_GREEN:LEDCOLOR_RED);
+           }
            break;
         case LEDMODE_OFFGREEN:
            if (IsDithered())
               lcol=(mLedOnOff?LEDCOLOR_LGREEN:LEDCOLOR_BLUE);
-           else
-              lcol=(mLedOnOff?LEDCOLOR_GREEN:LEDCOLOR_DGREEN);
+           else {
+              if (HasFocus()) {
+                 lcol=(flipflop?LEDCOLOR_YELLOW:LEDCOLOR_DGREEN);
+                 // only flip after showing focused item!
+                 flipflop=!flipflop;
+              } else
+                 lcol=(mLedOnOff?LEDCOLOR_GREEN:LEDCOLOR_DGREEN);
+           }
            break;
         case LEDMODE_MANUAL:
            lcol=mLedColor;
