6c42227026185a98cfacb123785ef15e940ad63f
Paolo Ventafridda <venta@lk8000.it>
2017-03-07 03:55:40 +0100

    Revert "Led flipflopping only on OPENGL"
    This reverts commit f84b35c1562ca37d60a6a09e37d02d3376949ece.

--- Common/Source/WindowControls.cpp
+++ Common/Source/WindowControls.cpp
@@ -2087,11 +2087,8 @@ void WndButton::Paint(LKSurface& Surface){
 
   Surface.DrawPushButton(rc, mDown);
 
-  // Flip-flopping works only with OPENGL because dialogs are repainted every second
-  // Selected item is painted yellow in any case.
-  // Remember to flip only after showing a focused item!
-  // 
-  static bool flipflop=true; 
+
+  static bool flipflop=true; // remember to flip only after showing a focused item!
   if (mLedMode) {
      RECT lrc={rc.left+(LEDBUTTONBORDER-1),rc.bottom-(LEDBUTTONBORDER+DLGSCALE(mLedSize)),
                rc.right-LEDBUTTONBORDER,rc.bottom-LEDBUTTONBORDER};
@@ -2103,9 +2100,7 @@ void WndButton::Paint(LKSurface& Surface){
            else {
               if (HasFocus()) {
                  lcol=(flipflop?LEDCOLOR_YELLOW:LEDCOLOR_BLACK);
-                 #ifdef ENABLE_OPENGL
                  flipflop=!flipflop;
-                 #endif
               } else
                 lcol=(mLedOnOff?LEDCOLOR_GREEN:LEDCOLOR_RED);
            }
@@ -2116,9 +2111,8 @@ void WndButton::Paint(LKSurface& Surface){
            else {
               if (HasFocus()) {
                  lcol=(flipflop?LEDCOLOR_YELLOW:LEDCOLOR_DGREEN);
-                 #ifdef ENABLE_OPENGL
+                 // only flip after showing focused item!
                  flipflop=!flipflop;
-                 #endif
               } else
                  lcol=(mLedOnOff?LEDCOLOR_GREEN:LEDCOLOR_DGREEN);
            }
