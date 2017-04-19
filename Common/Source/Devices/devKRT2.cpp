42c4bbd9b26531808b58f9146c16626c37234d11
AlphaLima <U.Heynen@web.de>
2017-04-19 21:26:53 +0200

    KRT2 driver fix fix wrong kHz to channel conversion

--- Common/Source/Devices/devKRT2.cpp
+++ Common/Source/Devices/devKRT2.cpp
@@ -85,8 +85,8 @@ int  SetKRT2Station(uint8_t *Command ,int Active_Passive, double fFrequency, TCH
 unsigned int len = 8;
 unsigned int i;
 unsigned char MHz= (unsigned char) fFrequency;
-unsigned char kHz= (unsigned char) (fFrequency *1000.0 - MHz *1000  + 0.5);
-int Chan = kHz/5;
+unsigned int kHz= (unsigned int) (fFrequency *1000.0 - MHz *1000  + 0.5);
+unsigned char Chan = (unsigned char)(kHz/5);
 
 char Airfield[10]={"   ---   "};
 
@@ -284,14 +284,14 @@ BOOL KRT2RadioMode(PDeviceDescriptor_t d, int mode) {
 
 
 BOOL KRT2RequestAllData(PDeviceDescriptor_t d) {
-  TCHAR  szTmp[255];
+/*  TCHAR  szTmp[255];
 
   LockComm();
   if(d != NULL)
     if(!d->Disabled)
       if (d->Com)
         d->Com->WriteString(szTmp);
-  UnlockComm();
+  UnlockComm();*/
   return(TRUE);
 }
 
@@ -384,7 +384,7 @@ int processed=0;
 static int iDetected = 0;
 static bool bFound = false;
 
-
+static int counter =0;
 
 int i;
 LKASSERT(szCommand !=NULL);
@@ -392,6 +392,7 @@ LKASSERT(d !=NULL);
 
     if(szCommand[0] == 'S')
     {
+      if(uiKRT2DebugLevel) StartupStore(_T("KRT2 heartbeat: #%i %s"),counter++ ,NEWLINE);
       d->Com->WriteString(_T("x"));
       if(bFound == false)
       {
