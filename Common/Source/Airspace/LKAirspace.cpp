383b7b0defcbf0a92e46452acfcb2e7c5b294b6c
AlphaLima <U.Heynen@gmx.de>
2018-07-12 22:01:53 +0200

    limit dynamic memory allocation (for the airspace name) to the realy needed size

--- Common/Source/Airspace/LKAirspace.cpp
+++ Common/Source/Airspace/LKAirspace.cpp
@@ -744,9 +744,8 @@ void CAirspaceBase::ResetWarnings() {
 void CAirspaceBase::Init(const TCHAR *name, const int type, const AIRSPACE_ALT &base, const AIRSPACE_ALT &top, bool flyzone, const TCHAR *comment) {
     CopyTruncateString(_name, NAME_SIZE, name);
 
-int iLen =   min(READLINE_LENGTH,(int) _tcslen(comment)+1);
- iLen = READLINE_LENGTH;
-// iLen *=sizeof(TCHAR);
+int iLen =   min(READLINE_LENGTH,(int) _tcslen(comment)+3);
+
   if(comment != NULL)
   {
 #ifdef DYN_MEM_COMMENT
@@ -763,7 +762,7 @@ int iLen =   min(READLINE_LENGTH,(int) _tcslen(comment)+1);
 
 	if( _comment  != NULL)
 	{
-	  CopyTruncateString(_comment,min( READLINE_LENGTH, (int) _tcslen(comment)+1), comment);
+	  CopyTruncateString(_comment,min( READLINE_LENGTH, (int) _tcslen(comment)+2), comment);
 //	  StartupStore(TEXT("new _comment: %s %u %s"), _comment, _tcslen(_comment), NEWLINE);
 	}
   }
