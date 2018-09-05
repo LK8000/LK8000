4cd7e9b974d8d4937f70b83f241d383304f58474
Ulli Heynen <U.Heynen@web.de>
2018-09-05 21:23:04 +0200

    to work with 10000 as well

--- Common/Source/Dialogs/dlgWaypointEdit.cpp
+++ Common/Source/Dialogs/dlgWaypointEdit.cpp
@@ -456,7 +456,7 @@ static void GetValues(WndForm* wf) {
 		wp = (WndProperty*)wf->FindByName(TEXT("prpLongitudeDDDD"));
 		if (wp) {
 		  mm = wp->GetDataField()->GetAsInteger();
-		  while((mm) > 10000)
+		  while((mm) >= 10000)
 		    mm /=10;
 		}
 		num = dd+mm/10000;
@@ -507,7 +507,7 @@ static void GetValues(WndForm* wf) {
 		wp = (WndProperty*)wf->FindByName(TEXT("prpLatitudeDDDD"));
 		if (wp) {
 		  mm = wp->GetDataField()->GetAsInteger();
-		  while((mm) > 10000)
+		  while((mm) >= 10000)
 		    mm /=10;
 		}
 		num = dd+mm/10000;
