b5a11fbb0e6c3f5cbec7c34cd59d03f4a7b9d03d
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2018-10-06 15:23:57 +0200

    add missing license header

--- Common/Source/Draw/ScreenProjection.cpp
+++ Common/Source/Draw/ScreenProjection.cpp
@@ -1,6 +1,10 @@
-/* 
+/*
+ * LK8000 Tactical Flight Computer -  WWW.LK8000.IT
+ * Released under GNU/GPL License v.2
+ * See CREDITS.TXT file for authors and copyrights
+ *
  * File:   ScreenProjection.cpp
- * Author: bruno
+ * Author: Bruno de Lacheisserie
  * 
  * Created on December 30, 2015, 1:32 AM
  */
