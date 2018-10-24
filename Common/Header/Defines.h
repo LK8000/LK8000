8d7391124760ce0b1ba8fcb0f25c469f681a3afc
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2018-10-24 20:29:06 +0200

    Version 7.0

--- Common/Header/Defines.h
+++ Common/Header/Defines.h
@@ -96,8 +96,8 @@
 #endif
 
 #define LKFORK		"LK8000"
-#define LKVERSION	"6"
-#define LKRELEASE	"1u1"
+#define LKVERSION	"7"
+#define LKRELEASE	"0"
 
 #define LKDATADIR	"LK8000"
 #define LKPROFILE	"DEFAULT_PROFILE.prf"
