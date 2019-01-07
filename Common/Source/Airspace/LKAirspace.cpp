4fdc3dda7280cc973a8c18c9d4e7eb92e542da28
AlphaLima <U.Heynen@web.de>
2019-01-07 22:02:30 +0100

    or even better...

--- Common/Source/Airspace/LKAirspace.cpp
+++ Common/Source/Airspace/LKAirspace.cpp
@@ -3217,7 +3217,7 @@ void CAirspaceManager::GetAirspaceAltText(TCHAR *buffer, int bufferlen, const AI
             break;
         case abAGL:
             if (alt->AGL <= 0)
-                _sntprintf(intbuf,BUF_LEN,TEXT("%s"), MsgToken(2387)); //_@M2387_ "GND"
+            	CopyTruncateString(intbuf, BUF_LEN, MsgToken(2387)); // _@M2387_ "GND"
             else {
                 Units::FormatUserAltitude(alt->AGL, sUnitBuffer, sizeof (sUnitBuffer) / sizeof (sUnitBuffer[0]));
                 Units::FormatAlternateUserAltitude(alt->AGL, sAltUnitBuffer, sizeof (sAltUnitBuffer) / sizeof (sAltUnitBuffer[0]));
@@ -3255,7 +3255,7 @@ void CAirspaceManager::GetSimpleAirspaceAltText(TCHAR *buffer, int bufferlen, co
             break;
         case abAGL:
             if (alt->AGL <= 0)
-                _sntprintf(intbuf,BUF_LEN, TEXT("%s"),MsgToken(2387)); // _@M2387_ "GND"
+            	CopyTruncateString(intbuf, BUF_LEN, MsgToken(2387)); // _@M2387_ "GND"
             else {
                 Units::FormatUserAltitude(alt->AGL, sUnitBuffer, sizeof (sUnitBuffer) / sizeof (sUnitBuffer[0]));
                 _stprintf(intbuf, TEXT("%s AGL"), sUnitBuffer);
