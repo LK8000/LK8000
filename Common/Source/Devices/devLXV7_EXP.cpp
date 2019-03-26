e7df3ed94cecf342a6b968892d4fd551731bfa45
AlphaLima <U.Heynen@web.de>
2019-03-26 21:44:29 +0100

    rebase

--- Common/Source/Devices/devLXV7_EXP.cpp
+++ Common/Source/Devices/devLXV7_EXP.cpp
@@ -2,7 +2,7 @@
    LK8000 Tactical Flight Computer -  WWW.LK8000.IT
    Released under GNU/GPL License v.2
    See CREDITS.TXT file for authors and copyrights
- 
+
    $Id$
 */
 //_____________________________________________________________________includes_
