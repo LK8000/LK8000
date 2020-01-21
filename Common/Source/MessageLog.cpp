0a886954cc52ee96a84540cf671df4cc36511712
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2020-01-21 19:52:34 +0100

    remove unused FailStore()

--- Common/Source/MessageLog.cpp
+++ Common/Source/MessageLog.cpp
@@ -49,59 +49,6 @@ void DebugStore(const char *Str, ...)
 #endif
 }
 
-
-#if 0 // 130207 no more unsed in V4
-void FailStore(const TCHAR *Str, ...)
-{
-  TCHAR buf[MAX_PATH];
-  va_list ap;
-
-  va_start(ap, Str);
-  _vstprintf(buf, Str, ap);
-  va_end(ap);
-
-  FILE *stream=NULL;
-  static TCHAR szFileName[MAX_PATH];
-  static bool initialised = false;
-
-  if (!initialised) {
-	LocalPath(szFileName, TEXT(LKF_FAILLOG));
-	stream = _tfopen(szFileName, TEXT("ab+"));
-	if (stream) {
-		fclose(stream);
-	}
-	initialised = true;
-  }
-  stream = _tfopen(szFileName,TEXT("ab+"));
-  if (stream == NULL) {
-	StartupStore(_T("------ FailStore failed, cannot open <%s>%s"), szFileName, NEWLINE);
-	return;
-  }
-  fprintf(stream, "------%s%04d%02d%02d-%02d:%02d:%02d [%09u] FailStore Start, Version %s%s (%s %s) FreeRam=%ld %s",SNEWLINE,
-	GPS_INFO.Year,GPS_INFO.Month,GPS_INFO.Day, GPS_INFO.Hour,GPS_INFO.Minute,GPS_INFO.Second,
-	(unsigned int)GetTickCount(),LKVERSION, LKRELEASE,
-	"",
-#if WINDOWSPC >0
-	"PC",
-#else
-	#ifdef PNA
-	"PNA",
-	#else
-	"PDA",
-	#endif
-#endif
-
-CheckFreeRam(),SNEWLINE);
-  fprintf(stream, "Message: %S%s", buf, SNEWLINE);
-  fprintf(stream, "GPSINFO: Latitude=%f Longitude=%f Altitude=%f Speed=%f %s",
-	GPS_INFO.Latitude, GPS_INFO.Longitude, GPS_INFO.Altitude, GPS_INFO.Speed, SNEWLINE);
-
-  fclose(stream);
-  StartupStore(_T("------ %s%s"),buf,NEWLINE);
-}
-#endif
-
-
 void StartupStore(const TCHAR *Str, ...)
 {
   static Mutex mutex;
