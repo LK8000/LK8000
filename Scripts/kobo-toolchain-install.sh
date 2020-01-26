818076161ec619d83942624d5e5a29835882424e
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2020-01-26 17:35:29 +0100

    upgrade kobo depends  - zziplib   0.13.69  - boostlib  1.69.0  - libpng    1.6.37  - freetype2 2.10.1

--- Scripts/kobo-toolchain-install.sh
+++ Scripts/kobo-toolchain-install.sh
@@ -21,8 +21,6 @@ fi
 NB_CORES=$(grep -c '^processor' /proc/cpuinfo)
 export MAKEFLAGS="-j$((NB_CORES+1)) -l${NB_CORES}"
 
-USER_PATH=$PATH
-
 [ ! -d ${BUILD_DIR} ] && mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
@@ -34,33 +32,29 @@ tar -xvzf zlib-1.2.11.tar.gz
 cd zlib-1.2.11
 CC=$TC-gcc CFLAGS="-O3 -march=armv7-a -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -funsafe-math-optimizations -funsafe-loop-optimizations" \
 ./configure --prefix=$TARGET_DIR
-make all
-sudo make install
+make all && make install
 cd ..
 
-# install zziplib ( 0.13.62 - 2012-03-11)
-[ ! -f zziplib-0.13.62.tar.bz2 ] && wget https://freefr.dl.sourceforge.net/project/zziplib/zziplib13/0.13.62/zziplib-0.13.62.tar.bz2
-[ -d zziplib-0.13.62 ] && rm -rf zziplib-0.13.62
+# install zziplib ( 0.13.69 - 2018-03-17)
+[ ! -f v0.13.69.tar.gz ] && wget https://github.com/gdraheim/zziplib/archive/v0.13.69.tar.gz
+[ -d zziplib-0.13.69 ] && rm -rf zziplib-0.13.69
 [ -d zzipbuild ] && rm -rf zzipbuild
-tar xvjf zziplib-0.13.62.tar.bz2 
+tar -xvzf v0.13.69.tar.gz 
 mkdir zzipbuild
 cd zzipbuild
 CFLAGS="-O3 -march=armv7-a -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -funsafe-math-optimizations -funsafe-loop-optimizations" \
-../zziplib-0.13.62/configure --host=$TC --target=$TC --prefix=$TARGET_DIR --with-zlib
-make all
-sudo PATH=$USER_PATH:$PATH \
-    make install
+../zziplib-0.13.69/configure --host=$TC --target=$TC --prefix=$TARGET_DIR --with-zlib
+make all && make install
 cd ..
 
-# install boostlib ( 1.66.0 - 2017-12-18 )
-[ ! -f boost_1_66_0.tar.gz ] && wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz
-[ -d boost_1_66_0 ] && sudo rm -rf boost_1_66_0
-tar xzf boost_1_66_0.tar.gz
-cd boost_1_66_0
+# install boostlib ( 1.69.0 - 2018-12-12 )
+[ ! -f boost_1_69_0.tar.gz ] && wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
+[ -d boost_1_69_0 ] && rm -rf boost_1_69_0
+tar xzf boost_1_69_0.tar.gz
+cd boost_1_69_0
 ./bootstrap.sh
 echo "using gcc : arm : $TC-g++ : cxxflags=-O3 -march=armv7-a -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -funsafe-math-optimizations -funsafe-loop-optimizations ;" > user-config.jam
-sudo PATH=$USER_PATH:$PATH \
-    ./bjam toolset=gcc-arm \
+./bjam toolset=gcc-arm \
            variant=release \
            link=shared \
            runtime-link=shared \
@@ -68,18 +62,25 @@ sudo PATH=$USER_PATH:$PATH \
            --without-python \
            --without-context \
            --without-coroutine \
+           --without-fiber \
+           --address-model=32 \
+           --without-mpi \
+           --without-graph_parallel \
+           -sZLIB_SOURCE="$BUILD_DIR/zlib-1.2.11" \
+           -sZLIB_INCLUDE="$TARGET_DIR\include" \
+           -sZLIB_LIBPATH="$TARGET_DIR\lib" \
            --user-config=user-config.jam \
            install
 cd ..
 
-# install libpng ( 1.6.34 - 2017-09-29 )
-[ ! -f libpng-1.6.34.tar.gz ] && wget http://sourceforge.net/projects/libpng/files/libpng16/1.6.34/libpng-1.6.34.tar.gz
-[ -d libpng-1.6.34 ] && rm -rf libpng-1.6.34
+# install libpng ( 1.6.37 - 2019-04-15 )
+[ ! -f libpng-1.6.37.tar.gz ] && wget http://sourceforge.net/projects/libpng/files/libpng16/1.6.37/libpng-1.6.37.tar.gz
+[ -d libpng-1.6.37 ] && rm -rf libpng-1.6.37
 [ -d libpng-build ] && rm -rf libpng-build
-tar xzf libpng-1.6.34.tar.gz
+tar xzf libpng-1.6.37.tar.gz
 mkdir libpng-build
 cd libpng-build
-../libpng-1.6.34/configure \
+../libpng-1.6.37/configure \
     --host=$TC \
     CC=$TC-gcc \
     AR=$TC-ar \
@@ -89,29 +90,25 @@ cd libpng-build
     LDFLAGS="-L$TARGET_DIR/lib" \
     --prefix=$TARGET_DIR \
     --enable-arm-neon
-make
-sudo PATH=$USER_PATH:$PATH \
-    make install
+make && make install
 cd ..
 
-# install freetype2 ( 2.9 - 2018-01-08 )
-[ ! -f freetype-2.9.tar.gz ] && wget https://download.savannah.gnu.org/releases/freetype/freetype-2.9.tar.gz
-[ -d freetype-2.9 ] && rm -rf freetype-2.9
+# install freetype2 ( 2.10.1 - 2019-07-01 )
+[ ! -f freetype-2.10.1.tar.gz ] && wget https://download.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.gz
+[ -d freetype-2.10.1 ] && rm -rf freetype-2.10.1
 [ -d freetype-build ] && rm -rf freetype-build
-tar xzf freetype-2.9.tar.gz
+tar xzf freetype-2.10.1.tar.gz
 mkdir freetype-build
 cd freetype-build
 CFLAGS="-O3 -march=armv7-a -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -funsafe-math-optimizations -funsafe-loop-optimizations" \
 LDFLAGS="-L$TARGET_DIR/lib"  \
-../freetype-2.9/configure \
+../freetype-2.10.1/configure \
     --host=$TC \
     --target=$TC \
     --prefix=$TARGET_DIR \
     --without-harfbuzz \
     PKG_CONFIG_LIBDIR=$TARGET_DIR/lib/pkgconfig
-make
-sudo PATH=$USER_PATH:$PATH \
-    make install
+make && make install
 cd ..
 
 # install Geographiclib ( 1.50.1 -  2019-12-13 )
@@ -127,7 +124,5 @@ LDFLAGS="-L$TARGET_DIR/lib"  \
     --host=$TC \
     --prefix=$TARGET_DIR \
     PKG_CONFIG_LIBDIR=$TARGET_DIR/lib/pkgconfig
-make
-sudo PATH=$USER_PATH:$PATH \
-    make install
+make && make install
 cd ..
