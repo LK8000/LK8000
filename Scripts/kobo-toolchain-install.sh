6a7ba6e7aebd2e815d56b8636bba53d3db7f158d
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2020-01-26 17:24:07 +0100

    update kobo-toolchain-install.sh to allow re-run without re-download package source.

--- Scripts/kobo-toolchain-install.sh
+++ Scripts/kobo-toolchain-install.sh
@@ -13,19 +13,24 @@ set -ex
 [ -z "$BUILD_DIR" ] && BUILD_DIR=$HOME/tmp
 [ -z "$TARGET_DIR" ] && TARGET_DIR=/opt/kobo/arm-unknown-linux-gnueabi
 
+if [ command -v ${TC}-gcc >/dev/null ]; then
+    echo "error : ${TC} toolchain not available"
+    exit 1
+fi
+
+NB_CORES=$(grep -c '^processor' /proc/cpuinfo)
+export MAKEFLAGS="-j$((NB_CORES+1)) -l${NB_CORES}"
+
 USER_PATH=$PATH
 
-mkdir "$BUILD_DIR"
-cd "$BUILD_DIR"
+[ ! -d ${BUILD_DIR} ] && mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
 
-# install toolchain
-#wget http://max.kellermann.name/download/xcsoar/devel/x-tools/x-tools-arm-i386-2013-12-11.tar.xz
-#tar xJfC x-tools-arm-i386-2013-12-11.tar.xz /home/user
-#export PATH=/home/user/x-tools/arm-unknown-linux-gnueabi/bin:$PATH
 
 # install zlib ( 1.2.11 - 2017-01-15)
-wget http://zlib.net/zlib-1.2.11.tar.gz
-tar -xvzf zlib-1.2.11.tar.gz 
+[ ! -f zlib-1.2.11.tar.gz ] && wget http://zlib.net/zlib-1.2.11.tar.gz
+[ -d zlib-1.2.11 ] && rm -rf zlib-1.2.11
+tar -xvzf zlib-1.2.11.tar.gz
 cd zlib-1.2.11
 CC=$TC-gcc CFLAGS="-O3 -march=armv7-a -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -funsafe-math-optimizations -funsafe-loop-optimizations" \
 ./configure --prefix=$TARGET_DIR
@@ -34,7 +39,9 @@ sudo make install
 cd ..
 
 # install zziplib ( 0.13.62 - 2012-03-11)
-wget https://freefr.dl.sourceforge.net/project/zziplib/zziplib13/0.13.62/zziplib-0.13.62.tar.bz2
+[ ! -f zziplib-0.13.62.tar.bz2 ] && wget https://freefr.dl.sourceforge.net/project/zziplib/zziplib13/0.13.62/zziplib-0.13.62.tar.bz2
+[ -d zziplib-0.13.62 ] && rm -rf zziplib-0.13.62
+[ -d zzipbuild ] && rm -rf zzipbuild
 tar xvjf zziplib-0.13.62.tar.bz2 
 mkdir zzipbuild
 cd zzipbuild
@@ -46,13 +53,14 @@ sudo PATH=$USER_PATH:$PATH \
 cd ..
 
 # install boostlib ( 1.66.0 - 2017-12-18 )
-wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz
+[ ! -f boost_1_66_0.tar.gz ] && wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz
+[ -d boost_1_66_0 ] && sudo rm -rf boost_1_66_0
 tar xzf boost_1_66_0.tar.gz
 cd boost_1_66_0
 ./bootstrap.sh
 echo "using gcc : arm : $TC-g++ : cxxflags=-O3 -march=armv7-a -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -funsafe-math-optimizations -funsafe-loop-optimizations ;" > user-config.jam
 sudo PATH=$USER_PATH:$PATH \
-    ./bjam toolset=gcc \
+    ./bjam toolset=gcc-arm \
            variant=release \
            link=shared \
            runtime-link=shared \
@@ -65,7 +73,9 @@ sudo PATH=$USER_PATH:$PATH \
 cd ..
 
 # install libpng ( 1.6.34 - 2017-09-29 )
-wget http://sourceforge.net/projects/libpng/files/libpng16/1.6.34/libpng-1.6.34.tar.gz
+[ ! -f libpng-1.6.34.tar.gz ] && wget http://sourceforge.net/projects/libpng/files/libpng16/1.6.34/libpng-1.6.34.tar.gz
+[ -d libpng-1.6.34 ] && rm -rf libpng-1.6.34
+[ -d libpng-build ] && rm -rf libpng-build
 tar xzf libpng-1.6.34.tar.gz
 mkdir libpng-build
 cd libpng-build
@@ -85,7 +95,9 @@ sudo PATH=$USER_PATH:$PATH \
 cd ..
 
 # install freetype2 ( 2.9 - 2018-01-08 )
-wget https://download.savannah.gnu.org/releases/freetype/freetype-2.9.tar.gz
+[ ! -f freetype-2.9.tar.gz ] && wget https://download.savannah.gnu.org/releases/freetype/freetype-2.9.tar.gz
+[ -d freetype-2.9 ] && rm -rf freetype-2.9
+[ -d freetype-build ] && rm -rf freetype-build
 tar xzf freetype-2.9.tar.gz
 mkdir freetype-build
 cd freetype-build
@@ -103,7 +115,9 @@ sudo PATH=$USER_PATH:$PATH \
 cd ..
 
 # install Geographiclib ( 1.50.1 -  2019-12-13 )
-wget https://netcologne.dl.sourceforge.net/project/geographiclib/distrib/GeographicLib-1.50.1.tar.gz
+[ ! -f GeographicLib-1.50.1.tar.gz ] && wget https://netcologne.dl.sourceforge.net/project/geographiclib/distrib/GeographicLib-1.49.tar.gz
+[ -d GeographicLib-1.50.1 ] && rm -rf GeographicLib-1.50.1
+[ -d GeographicLib-build ] && rm -rf GeographicLib-build
 tar xzf GeographicLib-1.50.1.tar.gz
 mkdir GeographicLib-build
 cd GeographicLib-build
