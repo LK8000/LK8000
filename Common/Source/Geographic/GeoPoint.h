0d29723c3b206171ce2b796c53579a39712d58ad
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2020-03-01 17:09:11 +0100

    Add AGeoPoint struct, extension of GeoPoint for altitude

--- Common/Source/Geographic/GeoPoint.h
+++ Common/Source/Geographic/GeoPoint.h
@@ -9,13 +9,17 @@
  * Created on October 21, 2016, 12:59 AM
  */
 
-#ifndef GEOGRAPHIC_GEOPOINT_H
-#define GEOGRAPHIC_GEOPOINT_H
+#ifndef _GEOGRAPHIC_GEOPOINT_H_
+#define _GEOGRAPHIC_GEOPOINT_H_
+
+#include <type_traits>
 
 struct GeoPoint {
 
     GeoPoint() = default;
-    GeoPoint(double lat, double lon) : latitude(lat), longitude(lon) {}
+    
+    constexpr GeoPoint(double lat, double lon) 
+            : latitude(lat), longitude(lon) {}
 
     GeoPoint Direct(double bearing, double distance) const;
 
@@ -45,4 +49,18 @@ struct GeoPoint {
     double longitude;
 };
 
-#endif /* GEOGRAPHIC_GEOPOINT_H */
+static_assert(std::is_trivial<GeoPoint>::value, "type is not trivial");
+
+struct AGeoPoint: public GeoPoint {
+
+    AGeoPoint() = default;
+
+    constexpr AGeoPoint(const GeoPoint& p, double alt)
+            : GeoPoint(p), altitude(alt) {}
+
+    double altitude;
+};
+
+static_assert(std::is_trivial<AGeoPoint>::value, "type is not trivial");
+
+#endif /* _GEOGRAPHIC_GEOPOINT_H_ */
