a419ae8b8a588842fc7ab586677b81f97ea37700
gereic <57770169+gereic@users.noreply.github.com>
2020-03-22 20:09:01 +0100

    Update Parser.h

--- Common/Header/Parser.h
+++ Common/Header/Parser.h
@@ -139,6 +139,7 @@ typedef struct _NMEA_INFO
   bool FLARMTRACE_bBuffFull;
   int  FLARMTRACE_iLastPtr;
   FANET_WEATHER FANET_Weather[MAXFANETWEATHER];
+  FANET_NAME FanetName[MAXFANETDEVICES];
 #ifdef DEVICE_SERIAL
   int HardwareId;
   int SerialNumber;
