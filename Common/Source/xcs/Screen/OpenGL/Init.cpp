b2c47a4f3829e7874fa1e31bf5048c6bd2f9bb72
Roel Baardman <roel.baardman@gmail.com>
2020-04-11 02:21:04 +0200

    [xcs patch] OpenGL/Init: Fix software screen rotation
    This solves a regression introduced in 67a9a19.
    Correctly multiply the rotation matrix defined by software screen rotation with other transformations, instead of overwriting it.
    
    # Conflicts:
    #	Common/Source/xcs/Screen/OpenGL/Init.cpp

--- Common/Source/xcs/Screen/OpenGL/Init.cpp
+++ Common/Source/xcs/Screen/OpenGL/Init.cpp
@@ -372,7 +372,7 @@ OpenGL::SetupViewport(UnsignedPoint2D size)
                                   glm::vec3(0, 0, 1));
   OrientationSwap(size, display_orientation);
 #endif
-  projection_matrix = glm::ortho<float>(0, size.x, size.y, 0, -1, 1);
+  projection_matrix *= glm::ortho<float>(0, size.x, size.y, 0, -1, 1);
   UpdateShaderProjectionMatrix();
 #else
   glMatrixMode(GL_PROJECTION);
