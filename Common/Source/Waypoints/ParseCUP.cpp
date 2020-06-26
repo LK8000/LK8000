a0344ce015cbc10b6d0f39e80631b62be6ff5790
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2020-06-26 20:12:16 +0200

    update cup file waypoints loading for compatiblity with Seeyou v10.32
    issue #1466

--- Common/Source/Waypoints/ParseCUP.cpp
+++ Common/Source/Waypoints/ParseCUP.cpp
@@ -19,6 +19,36 @@ enum class CSVState {
     QuotedQuote,
 };
 
+std::map<tstring, size_t> CupStringToHeader(const TCHAR *row) {
+  const std::map<tstring, tstring> alias = {
+    { _T("Title"),       _T("name") },
+    { _T("Code"),        _T("code") },
+    { _T("Country"),     _T("country") },
+    { _T("Latitude"),    _T("lat") },
+    { _T("Longitude"),   _T("lon") },
+    { _T("Elevation"),   _T("elev") },
+    { _T("Style"),       _T("style") },
+    { _T("Direction"),   _T("rwdir") },
+    { _T("Length"),      _T("rwlen") },
+    { _T("Frequency"),   _T("freq") },
+    { _T("Description"), _T("desc") }
+  };
+
+  std::map<tstring, size_t> header;
+  const auto entries = CupStringToFieldArray(row);
+  for (size_t i = 0; i < entries.size(); ++i) {
+    const tstring& text = entries[i];
+    tstring lower_text;
+    std::transform(text.begin(), text.end(), std::back_inserter(lower_text), ::tolower);
+    auto it = alias.find(lower_text);
+    if (it != alias.end()) {
+      lower_text = it->second;
+    }
+    header[lower_text] = i;
+  }
+  return header;
+}
+
 std::vector<tstring> CupStringToFieldArray(const TCHAR *row) {
 
     std::vector<tstring> fields = {_T("")};
@@ -72,8 +102,33 @@ std::vector<tstring> CupStringToFieldArray(const TCHAR *row) {
     return fields;
 }
 
+namespace {
+  class cup_line {
+  public:
+    cup_line(const std::map<tstring, size_t>& _Headers, std::vector<tstring>&& _Entries) 
+          : Headers(_Headers), Entries(std::forward<std::vector<tstring>>(_Entries)) {}
+
+    const tstring& operator[](const TCHAR* Name) {
+      auto it = Headers.find(Name);
+      if(it != Headers.end()) {
+        return Entries[it->second];
+      }
+      return empty;
+    }
+
+    size_t size() {
+      return Entries.size();
+    }
+
+  private:
+    const tstring empty = _T("");
+    const std::map<tstring, size_t>& Headers;
+    const std::vector<tstring> Entries;
+  };
+}
+
 //#define CUPDEBUG
-bool ParseCUPWayPointString(const TCHAR *String,WAYPOINT *Temp)
+bool ParseCUPWayPointString(const std::map<tstring, size_t>& cup_header, const TCHAR *String,WAYPOINT *Temp)
 {
   int flags=0;
   bool ishome=false; // 100310
@@ -84,35 +139,35 @@ bool ParseCUPWayPointString(const TCHAR *String,WAYPOINT *Temp)
   Temp->Number = WayPointList.size();
   Temp->FileNum = globalFileNum;
 
-  std::vector<tstring> Entries =   CupStringToFieldArray(String);
+  cup_line Entries(cup_header, CupStringToFieldArray(String));
 
   if(Entries.size() < 11) {
     return false;
   }
 
   // ---------------- NAME ----------------
-  _sntprintf(Temp->Name,NAME_SIZE, _T("%s"), Entries[0].c_str() );
+  _sntprintf(Temp->Name,NAME_SIZE, _T("%s"), Entries[_T("name")].c_str());
   #ifdef CUPDEBUG
   StartupStore(_T("   CUP NAME=<%s>%s"),Temp->Name,NEWLINE);
   #endif
 
 
   // ---------------- CODE ------------------
-  _sntprintf(Temp->Code,CUPSIZE_CODE, _T("%s"), Entries[1].c_str() );
+  _sntprintf(Temp->Code,CUPSIZE_CODE, _T("%s"),  Entries[_T("code")].c_str() );
   #ifdef CUPDEBUG
   StartupStore(_T("   CUP CODE=<%s>%s"),Temp->Code,NEWLINE);
   #endif
 
 
   // ---------------- COUNTRY ------------------
-  _sntprintf(Temp->Country,CUPSIZE_COUNTRY, _T("%s"), Entries[2].c_str() );
+  _sntprintf(Temp->Country,CUPSIZE_COUNTRY, _T("%s"),  Entries[_T("country")].c_str() );
   #ifdef CUPDEBUG
   StartupStore(_T("   CUP COUNTRY=<%s>%s"),Temp->Country,NEWLINE);
   #endif
 
 
   // ---------------- LATITUDE  ------------------
-  Temp->Latitude = CUPToLat(Entries[3].c_str() );
+  Temp->Latitude = CUPToLat( Entries[_T("lat")].c_str() );
 
   if((Temp->Latitude > 90) || (Temp->Latitude < -90)) {
 	return false;
@@ -123,7 +178,7 @@ bool ParseCUPWayPointString(const TCHAR *String,WAYPOINT *Temp)
 
 
   // ---------------- LONGITUDE  ------------------
-  Temp->Longitude  = CUPToLon(Entries[4].c_str());
+  Temp->Longitude  = CUPToLon( Entries[_T("lon")].c_str());
   #ifdef CUPDEBUG
   StartupStore(_T("   CUP LONGITUDE=<%f>%s"),Temp->Longitude,Entries[4].c_str());
   #endif
@@ -136,7 +191,7 @@ bool ParseCUPWayPointString(const TCHAR *String,WAYPOINT *Temp)
 
 
   // ---------------- ELEVATION  ------------------
-  Temp->Altitude = ReadAltitude(Entries[5].c_str());
+  Temp->Altitude = ReadAltitude( Entries[_T("elev")].c_str());
   #ifdef CUPDEBUG
   StartupStore(_T("   CUP ELEVATION=<%f>%s"),Temp->Altitude,NEWLINE);
   #endif
@@ -146,7 +201,7 @@ bool ParseCUPWayPointString(const TCHAR *String,WAYPOINT *Temp)
 
 
   // ---------------- STYLE  ------------------
-  Temp->Style = (int)_tcstol(Entries[6].c_str(),NULL,10);
+  Temp->Style = (int)_tcstol( Entries[_T("style")].c_str(),NULL,10);
   switch(Temp->Style) {
 	case STYLE_AIRFIELDGRASS:	// airfield grass
 	case STYLE_GLIDERSITE:		// glider site
@@ -169,22 +224,23 @@ bool ParseCUPWayPointString(const TCHAR *String,WAYPOINT *Temp)
   #endif
 
   // ---------------- RWY DIRECTION   ------------------
-  if ((Entries[7].length()) == 1)
-	Temp->RunwayDir=-1;
-  else
-	Temp->RunwayDir = (int)AngleLimit360(_tcstol(Entries[7].c_str(), NULL, 10));
+  const tstring& rwdir =  Entries[_T("rwdir")];
+  if ((rwdir.length()) == 1) {
+    Temp->RunwayDir=-1;
+  } else {
+    Temp->RunwayDir = (int)AngleLimit360(_tcstol(rwdir.c_str(), NULL, 10));
+  }
   #ifdef CUPDEBUG
   StartupStore(_T("   CUP RUNWAY DIRECTION=<%d>%s"),Temp->RunwayDir,NEWLINE);
   #endif
 
 
   // ---------------- RWY LENGTH   ------------------
-
-  if (Entries[8].length() == 1)
-	Temp->RunwayLen = -1;
-  else
-  {
-    Temp->RunwayLen = (int)ReadLength(Entries[8].c_str());
+  const tstring& rwlen =  Entries[_T("rwlen")];
+  if (rwlen.length() == 1) {
+    Temp->RunwayLen = -1;
+  } else {
+    Temp->RunwayLen = (int)ReadLength(rwlen.c_str());
   }
   #ifdef CUPDEBUG
   StartupStore(_T("   CUP RUNWAY LEN=<%d>%s"),Temp->RunwayLen,NEWLINE);
@@ -193,7 +249,8 @@ bool ParseCUPWayPointString(const TCHAR *String,WAYPOINT *Temp)
 
 
   // ---------------- AIRPORT FREQ   ------------------
-  _sntprintf(Temp->Freq,CUPSIZE_FREQ, _T("%s"), Entries[9].c_str() );
+  const tstring& freq =  Entries[_T("freq")];
+  _sntprintf(Temp->Freq,CUPSIZE_FREQ, _T("%s"), freq.c_str() );
 
   #ifdef CUPDEBUG
   StartupStore(_T("   CUP FREQ=<%s>%s"),Temp->Freq,NEWLINE);
@@ -201,17 +258,17 @@ bool ParseCUPWayPointString(const TCHAR *String,WAYPOINT *Temp)
 
 
   // ---------------- COMMENT   ------------------
-  SetWaypointComment(*Temp, Entries[10].c_str());
+  SetWaypointComment(*Temp,  Entries[_T("desc")].c_str());
   #ifdef CUPDEBUG
   StartupStore(_T("   CUP COMMENT=<%s>%s"),Temp->Comment,NEWLINE);
   #endif
 
   if(Temp->Altitude <= 0) {
-	WaypointAltitudeFromTerrain(Temp);
+    WaypointAltitudeFromTerrain(Temp);
   }
 
   if (Temp->Details) {
-	free(Temp->Details);
+    free(Temp->Details);
   }
 
   return true;
