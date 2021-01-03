cab0aeabec91bc4273bcbbee23382b776ce780b4
AlphaLima <U.Heynen@web.de>
2021-01-03 08:00:55 +0100

    made compatible to all toolchains

--- Common/Source/Draw/Multimaps/TopView.cpp
+++ Common/Source/Draw/Multimaps/TopView.cpp
@@ -257,7 +257,7 @@ _nomoredeclutter:
   // M3 has sideview always on, so wont apply here, and no need to check
   if (Current_Multimap_SizeY==SIZE4) {
     RECT rc = rct;
-    rc.Grow(0, BottomSize);
+    rc.bottom += BottomSize;
 	DrawMapScale(Surface,rc,_Proj);
   }
 
