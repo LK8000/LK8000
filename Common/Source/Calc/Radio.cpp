f078620b56047ea266f2d8b2b1d809451fb11e2c
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2021-01-05 23:46:41 +0100

    Radio.h/Radio.cpp : fix typo and code indent

--- Common/Source/Calc/Radio.cpp
+++ Common/Source/Calc/Radio.cpp
@@ -13,84 +13,80 @@
 
 BOOL ValidFrequency(double Freq)
 {
-BOOL Valid =FALSE;
-int Frac = 	(int)(Freq*1000.0+0.05) - 100*((int) (Freq *10.0+0.05));
+	BOOL Valid =FALSE;
+	int Frac = 	(int)(Freq*1000.0+0.05) - 100*((int) (Freq *10.0+0.05));
 
-  if(Freq >= 118.0)
-    if(Freq <= 137.0)
-      switch(Frac)
-      {
-        case 0:
-        case 25:
-        case 50:
-        case 75:
-          Valid = TRUE;
-        break;
+	if(Freq >= 118.0 && Freq <= 137.0) {
+		switch(Frac)
+		{
+			case 0:
+			case 25:
+			case 50:
+			case 75:
+				Valid = TRUE;
+			break;
 
-        case 5:
-        case 10:
-        case 15:
-        case 30:
-        case 35:
-        case 40:
-        case 55:
-        case 60:
-        case 65:
-        case 80:
-        case 85:
-        case 90:
-          if(RadioPara.Enabled8_33)
-            Valid = TRUE;
-        break;
+			case 5:
+			case 10:
+			case 15:
+			case 30:
+			case 35:
+			case 40:
+			case 55:
+			case 60:
+			case 65:
+			case 80:
+			case 85:
+			case 90:
+				if(RadioPara.Enabled8_33)
+					Valid = TRUE;
+			break;
 
-        default:
-        break;
-      }
+			default:
+			break;
+		}
+	}
 
-
-  return Valid;
+	return Valid;
 }
 
 double  ExtractFrequency(const TCHAR *text)
 {
-	if(text == NULL)
+	if(text == nullptr)
 		return 0.0;
-double fFreq = 0.0;
-int iTxtlen = (int)_tcslen(text);
-int i,Mhz=0,kHz=0;
 
-	for (i=0; i < iTxtlen; i++)
+	double fFreq = 0.0;
+	size_t iTxtlen = _tcslen(text);
+
+	for (size_t i = 0; i < (iTxtlen - 3); i++)
 	{
 		if (text[i] == '1')
 		{
-			Mhz  = _tcstol(&text[i], nullptr, 10);
-			if(Mhz >= 118)
-				if(Mhz <= 138)
-					if((i+3) < iTxtlen)
-					{
-						if((text[i+3] == '.') || (text[i+3] == ','))
-						{
-							kHz =0;
-							if((i+4) < iTxtlen)
-								if((text[i+4] >= '0') && (text[i+4] <= '9'))
-									kHz += (text[i+4]-'0') * 100;
+			int Mhz  = _tcstol(&text[i], nullptr, 10);
+			if(Mhz >= 118 && Mhz <= 138) 
+			{
+				if((text[i+3] == '.') || (text[i+3] == ','))
+				{
+					int kHz = 0;
+					if((i+4) < iTxtlen)
+						if((text[i+4] >= '0') && (text[i+4] <= '9'))
+							kHz += (text[i+4]-'0') * 100;
 
-							if((i+5) < iTxtlen)
-								if((text[i+5] >= '0') && (text[i+5] <= '9'))
-									kHz += (text[i+5]-'0') * 10;
+					if((i+5) < iTxtlen)
+						if((text[i+5] >= '0') && (text[i+5] <= '9'))
+							kHz += (text[i+5]-'0') * 10;
 
-							if((i+6) < iTxtlen)
-								if((text[i+6] >= '0') && (text[i+6] <= '9'))
-									kHz += (text[i+6]-'0');
+					if((i+6) < iTxtlen)
+						if((text[i+6] >= '0') && (text[i+6] <= '9'))
+							kHz += (text[i+6]-'0');
 
-							fFreq = (double) Mhz+ (double)kHz/1000.0f;
-							return fFreq;
-						}
-					}
+					fFreq = (double)Mhz + (double)kHz / 1000.0;
+				}
+			}
 		}
 	}
 
- return fFreq;
+	return fFreq;
 }
 
 /**
