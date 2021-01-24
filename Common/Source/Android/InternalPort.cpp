031f7dbf8bed7c4e5cc7df8cbbacef3495004d36
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2021-01-24 17:50:06 +0100

    Android : add vario sound for device with internal baro sensor

--- Common/Source/Android/InternalPort.cpp
+++ Common/Source/Android/InternalPort.cpp
@@ -13,29 +13,55 @@
 
 
 
-InternalPort::InternalPort(int idx, const tstring& sName) : NullComPort(idx, sName), internal_sensors() {
+InternalPort::InternalPort(int idx, const tstring& sName) : NullComPort(idx, sName) { }
 
-}
-
-InternalPort::~InternalPort() {
-
-};
+InternalPort::~InternalPort() { }
 
 bool InternalPort::Initialize() {
 
     internal_sensors = InternalSensors::create(Java::GetEnv(), context, GetPortIndex());
     if (internal_sensors) {
-// TODO: Allow user to specify whether they want certain sensors.
+        // TODO: Allow user to specify whether they want certain sensors.
         internal_sensors->subscribeToSensor(InternalSensors::TYPE_PRESSURE);
+
+        thread_status = std::thread([&] {
+            thread_run();
+        });
+
         return NullComPort::Initialize();
     }
     return false;
 }
 
 bool InternalPort::Close() {
-    delete internal_sensors;
-    internal_sensors = nullptr;
+    InternalSensors* p = internal_sensors;
+    {
+        std::unique_lock<std::mutex> lock(mutex_status);
+        internal_sensors = nullptr;
+        cv_status.notify_all();
+    }
+    delete p;
+
+    thread_status.join();
 
     return NullComPort::Close();
 }
 
+void InternalPort::CancelWaitEvent() {
+    cv_status.notify_all();
+}
+
+void InternalPort::thread_run() {
+    std::unique_lock<std::mutex> lock(mutex_status);
+    while (internal_sensors) {
+        cv_status.wait(lock);
+        if (!internal_sensors) {
+            continue;
+        }
+        PDeviceDescriptor_t d = devGetDeviceOnPort(GetPortIndex());
+        if (d && d->IsBaroSource && d->IsBaroSource(d)) {
+            internal_sensors->InitialiseVarioSound();
+        }
+    }
+    internal_sensors->DeinitialiseVarioSound();
+}
