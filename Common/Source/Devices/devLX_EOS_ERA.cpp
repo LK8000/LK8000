13d1536ab66be3685dd2587fa0f7c4935e83a63f
AlphaLima <U.Heynen@web.de>
2022-01-20 22:00:53 +0100

    increase timeout, 500ms timeout is to optimistic on some devices.

--- Common/Source/Devices/devLX_EOS_ERA.cpp
+++ Common/Source/Devices/devLX_EOS_ERA.cpp
@@ -811,7 +811,7 @@ BOOL DevLX_EOS_ERA::DeclareTask(PDeviceDescriptor_t d,
     // Send complete declaration to logger
     int orgRxTimeout;
     StartupStore(_T(". EOS/ERA SetRxTimeout%s "), NEWLINE);
-    status = SetRxTimeout(d, 500, orgRxTimeout, errBufSize, errBuf);
+    status = SetRxTimeout(d, 2000, orgRxTimeout, errBufSize, errBuf);
     int attemps = 0;
     char RecBuf[4096] = "";
 
