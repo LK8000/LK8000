a3995561b7642b65e071b2097ebaea2123bfb709
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2022-01-23 19:19:04 +0100

    devLK8EX1.h : remove unimplemented function LK8EX1LinkTimeout

--- Common/Source/Devices/devLK8EX1.h
+++ Common/Source/Devices/devLK8EX1.h
@@ -20,6 +20,5 @@ DeviceRegister_t LK8EX1Register(void){
 
 BOOL LK8EX1ParseNMEA(PDeviceDescriptor_t d, TCHAR *String, NMEA_INFO *pGPS);
 BOOL LK8EX1IsBaroSource(PDeviceDescriptor_t d);
-BOOL LK8EX1LinkTimeout(PDeviceDescriptor_t d);
 
 #endif
