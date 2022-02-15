LK8000/LK8000

commit 0493953ee79341ecff641371411599e3b739fc00

devLX_EOS_ERA : workaround for turnpoint name charset conversion
device send turnpoint name with the charset of file loaded

in most case it could be ANSI or UTF8, to avoid artifact, we try to detect charset from string and convert to the right charset (UTF8 or UTF16 depending of platform)

Common/Source/Devices/devLX_EOS_ERA.cpp
@@ -39,6 +39,8 @@
 #include "Thread/Mutex.hpp"
 #include "Thread/Cond.hpp"
 #include "Radio.h"
+#include "utils/charset_helper.h"
+
 
 unsigned int uiEOSDebugLevel = 1;
 extern bool UpdateQNH(const double newqnh);
@@ -2058,19 +2060,32 @@ BOOL DevLX_EOS_ERA::GetTarget(PDeviceDescriptor_t d, const TCHAR* sentence, NMEA
     return false;
   }
 
-TCHAR  szTmp[MAX_NMEA_LEN];
+  TCHAR  szTmp[MAX_NMEA_LEN];
 
-double fTmp;
   NMEAParser::ExtractParameter(sentence,szTmp,3);
+  ////////////////////////////////////////////////////// 
+  // workaround to fix turnpoint charset
+#ifdef UNICODE  
+  // 1 - copy back TCHAR to char
+  char  szName[MAX_NMEA_LEN];
+  for (size_t i = 0; szTmp[0]; ++i) {
+    szName[i] = szTmp[i];
+  }
+#else
+  // TCHAR is alias to char, no need copy back to char.
+  char* szName = szTmp;
+#endif
+  // 2 - detect and fix chatset
+  tstring tname = from_unknown_charset(szName);
+  ////////////////////////////////////////////////////// 
 
-
-    if(Alternate2 == RESWP_EXT_TARGET) // pointing to external target?
-      Alternate2 = -1;                 // clear external =re-enable!
+  if(Alternate2 == RESWP_EXT_TARGET) // pointing to external target?
+    Alternate2 = -1;                 // clear external =re-enable!
 
   _tcscpy(WayPointList[RESWP_EXT_TARGET].Name, _T("^") );
-  _tcscat(WayPointList[RESWP_EXT_TARGET].Name, szTmp );
+  _tcscat(WayPointList[RESWP_EXT_TARGET].Name, tname.c_str() );
 
-
+  double fTmp;
   ParToDouble(sentence, 4, &fTmp);
   WayPointList[RESWP_EXT_TARGET].Latitude= fTmp/60000;
   ParToDouble(sentence, 5, &fTmp);

commit 3543c0d6831bf05abc069dee1a0aa158aab8f080

devLX_EOS_ERA : fix infinite loop on Win32 platform
introduced in commit 0493953e

Common/Source/Devices/devLX_EOS_ERA.cpp
@@ -2068,7 +2068,7 @@ BOOL DevLX_EOS_ERA::GetTarget(PDeviceDescriptor_t d, const TCHAR* sentence, NMEA
 #ifdef UNICODE  
   // 1 - copy back TCHAR to char
   char  szName[MAX_NMEA_LEN];
-  for (size_t i = 0; szTmp[0]; ++i) {
+  for (size_t i = 0; szTmp[i]; ++i) {
     szName[i] = szTmp[i];
   }
 #else