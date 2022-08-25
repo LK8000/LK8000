e012e9d22cf2340bbe26188bd430b94ed860c373
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2022-08-25 19:24:29 +0200

    WithLock(...): use std::invoke

--- Common/Source/Thread/Mutex.hpp
+++ Common/Source/Thread/Mutex.hpp
@@ -12,6 +12,7 @@
 #ifndef _THREAD_MUTEX_HPP_
 #define	_THREAD_MUTEX_HPP_
 
+#include <functional>
 #include "Poco/Mutex.h"
 #include "Poco/ScopedLock.h"
 #include "Poco/ScopedUnlock.h"
@@ -31,12 +32,15 @@ using ScopeLock = Poco::ScopedLock<Mutex>;
 using ScopeUnlock = Poco::ScopedUnlock<Mutex>;
 
 /**
- * Call the Function within Mutex lock.
+ * Invoke a callable object within Mutex lock.
  */
-template<class _Mutex, class Function>
-decltype(auto) WithLock(_Mutex& m, Function&& f) {
+template<typename _Mutex, typename _Callable, typename ..._Args>
+std::invoke_result_t<_Callable, _Args...> 
+WithLock(_Mutex& m, _Callable&& fn, _Args&& ...args) 
+noexcept(std::is_nothrow_invocable_v<_Callable, _Args...>)
+{
     Poco::ScopedLock<_Mutex> lock(m);
-    return f();
+    return std::invoke(std::forward<_Callable>(fn), std::forward<_Args>(args)...);
 }
 
 #endif	/* _THREAD_MUTEX_HPP_ */
