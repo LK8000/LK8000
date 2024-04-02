8a192b49a5751f0b17acd29194e41aab5b5e392f
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2024-04-02 19:56:34 +0200

    MessageLog : use "unique_file_ptr"

--- Common/Source/MessageLog.cpp
+++ Common/Source/MessageLog.cpp
@@ -10,6 +10,7 @@
 #include <stdio.h>      /* printf */
 #include <stdarg.h>     /* va_list, va_start, va_arg, va_end */
 #include "utils/stringext.h"
+#include "utils/unique_file_ptr.h"
 #include "OS/Memory.h"
 
 #ifdef ANDROID
@@ -18,11 +19,10 @@
 
 constexpr size_t MAX_LOG_SIZE = 1024*1024; // 1MB
 
-void DebugStore(const char *Str, ...)
-{
+void DebugStore(const char* fmt, ...) {
 #ifndef NDEBUG
 
-  FILE *stream = nullptr;
+  unique_file_ptr stream;
 
   static Mutex mutex;
   ScopeLock Lock(mutex);
@@ -31,30 +31,29 @@ void DebugStore(const char *Str, ...)
   static bool initialised = false;
   if (!initialised) {
     LocalPath(szFileName, TEXT(LKF_DEBUG));
-    stream = _tfopen(szFileName,TEXT("w"));
+    stream = make_unique_file_ptr(szFileName,TEXT("w"));
     initialised = true;
   } else {
-    stream = _tfopen(szFileName,TEXT("a+"));
+    stream = make_unique_file_ptr(szFileName,TEXT("w"));
   }
 
   if(stream) {
-      va_list ap;
-      va_start(ap, Str);
-      vfprintf(stream, Str, ap);
-      va_end(ap);
-      fclose(stream);
+      va_list args;
+      va_start(args, fmt);
+      vfprintf(stream.get(), fmt, args);
+      va_end(args);
   }
 #endif
 }
 
-void StartupStoreV(const TCHAR *Str, va_list ap)
+void StartupStoreV(const TCHAR* fmt, va_list args)
 {
   static Mutex mutex;
   ScopeLock Lock(mutex);
 
   TCHAR buf[1024]; // 2 kByte for unicode, 1kByte for utf-8
 
-  _vsntprintf(buf, std::size(buf), Str, ap);
+  _vsntprintf(buf, std::size(buf), fmt, args);
 
   buf[std::size(buf) -1] = _T('\0'); // grant string is null terminated.
 
@@ -84,8 +83,8 @@ void StartupStoreV(const TCHAR *Str, va_list ap)
     initialised = true;
   }
 
-  FILE* startupStoreFile = _tfopen(szFileName, TEXT("ab+"));
-  if (startupStoreFile != NULL) {
+  auto startupStoreFile = make_unique_file_ptr(szFileName, TEXT("ab+"));
+  if (startupStoreFile) {
 #ifdef UNICODE
     /* each codepoints can be encoded in one to four bytes.
     * worst case : ( <number of codepoint> x 4 ) + <size of '\0'>
@@ -94,14 +93,13 @@ void StartupStoreV(const TCHAR *Str, va_list ap)
     char sbuf[buff_size]; 
     size_t i = to_utf8(buf, sbuf, buff_size);
 #else
-    char* sbuf = buf; // string is already utf-8 encoded, no need to convert.
+    const char* sbuf = buf; // string is already utf-8 encoded, no need to convert.
     size_t i = strlen(sbuf);
 #endif
 
     if (i > 0) {
-      fprintf(startupStoreFile, "[%09u] %s" SNEWLINE, MonotonicClockMS(), sbuf);
+      fprintf(startupStoreFile.get(), "[%09u] %s" SNEWLINE, MonotonicClockMS(), sbuf);
     }
-    fclose(startupStoreFile);
   }
 }
 
