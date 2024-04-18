e9e4eb9a8b2be1ee2e712ae7c49789419f8e39e2
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2024-04-18 22:16:33 +0200

    LiveTrack24 : use `std::mt19937` to generate random string

--- Common/Source/Tracking/LiveTrack24.cpp
+++ Common/Source/Tracking/LiveTrack24.cpp
@@ -13,6 +13,7 @@
 #include <time.h>
 #include <zlib.h>
 #include <string>
+#include <random>
 #include "externs.h"
 #include "Tracking.h"
 #include "LiveTrack24.h"
@@ -103,20 +104,23 @@ std::string toString(const T& value) {
 	return oss.str();
 }
 
-static std::string random_string( size_t length )
-{
-  auto randchar = []() -> char
-  {
-    const char charset[] =
-        "0123456789"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
-    const size_t max_index = (sizeof(charset) - 1);
-    return charset[ std::rand() % max_index ];
-  };
-  std::string str(length,0);
-  std::generate_n( str.begin(), length, randchar );
-  return str;
+static std::string random_string(size_t length) {
+	constexpr std::string_view charset =
+		"0123456789"
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcdefghijklmnopqrstuvwxyz";
+
+	std::random_device rd;   // a seed source for the random number engine
+	std::mt19937 gen(rd());  // mersenne_twister_engine seeded with rd()
+	std::uniform_int_distribution<unsigned> distrib(0, charset.size() - 1);
+
+	auto randchar = [&]() -> char {
+		return charset[distrib(gen)];
+	};
+
+	std::string str(length, 0);
+	std::generate_n(str.begin(), length, randchar);
+	return str;
 }
 static std::string  DeviceID =  random_string(10);
 
