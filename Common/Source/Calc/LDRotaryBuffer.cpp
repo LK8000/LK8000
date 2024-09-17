1eab652edf975b8ad654f78b1b383bf5d87cc648
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2024-09-17 19:42:35 +0200

    LDRotaryBuffer : factorize code

--- Common/Source/Calc/LDRotaryBuffer.cpp
+++ Common/Source/Calc/LDRotaryBuffer.cpp
@@ -8,15 +8,23 @@
 
 #include "externs.h"
 #include "McReady.h"
+#include "utils/unique_file_ptr.h"
 
-//#define DEBUG_ROTARY 1
+#define DEBUG_ROTARY
+
+template<typename... Args>
+static void Log(const char* fmt, Args&&... args) {
+#ifdef DEBUG_ROTARY
+	auto fp = make_unique_file_ptr("Rotary_Debug.TXT", "a");
+	if (fp) {
+		fprintf(fp.get(), fmt, std::forward<Args>(args)...);
+	}
+#endif
+}
+
 
 bool InitLDRotary(ldrotary_s *buf) {
-short i, bsize;
-#ifdef DEBUG_ROTARY
-char ventabuffer[200];
-FILE *fp;
-#endif
+	short i, bsize;
 	TestLog(_T("... Init LDRotary @%s"), WhatTimeIsIt());
 
 	switch (AverEffTime) {
@@ -68,15 +76,13 @@ FILE *fp;
 	buf->start=-2;
 	buf->size=bsize;
 	buf->valid=false;
-#ifdef DEBUG_ROTARY
-	sprintf(ventabuffer,"InitLdRotary size=%d\r\n",buf->size);
-	if ((fp=fopen("DEBUG.TXT","a"))!= NULL)
-                    {;fprintf(fp,"%s\n",ventabuffer);fclose(fp);}
-#endif
+
+	Log("InitLdRotary size=%d\r\n", buf->size);
 
 	Rotary_Speed=0;
 	Rotary_Distance=0;
-  return false;
+
+	return false;
 }
 
 
@@ -84,14 +90,11 @@ FILE *fp;
 // Called by LD  in Calc thread
 //
 void InsertLDRotary(ldrotary_s *buf, double distance, NMEA_INFO *Basic, DERIVED_INFO *Calculated) {
-static unsigned short errs=0;
+	static unsigned short errs=0;
 #ifdef TESTBENCH
-static bool zeroerrs=true;
+	static bool zeroerrs=true;
 #endif
-#ifdef DEBUG_ROTARY
-char ventabuffer[200];
-FILE *fp;
-#endif
+
 	if (LKSW_ResetLDRotary) {
 		TestLog(_T("... LD ROTARY SWITCH RESET @%s"), WhatTimeIsIt());
 		LKSW_ResetLDRotary=false;
@@ -100,11 +103,7 @@ FILE *fp;
 
         // For IGC Replay, we are never OnGround, see Calc/TakeoffLanding.cpp
 	if (Calculated->OnGround) {
-#ifdef DEBUG_ROTARY
-		sprintf(ventabuffer,"OnGround, ignore LDrotary\r\n");
-		if ((fp=fopen("DEBUG.TXT","a"))!= NULL)
-			    {;fprintf(fp,"%s\n",ventabuffer);fclose(fp);}
-#endif
+		Log("OnGround, ignore LDrotary\r\n");
 		return;
 	}
 
@@ -113,27 +112,19 @@ FILE *fp;
 	}
 
 	if (Calculated->Circling) {
-#ifdef DEBUG_ROTARY
-		sprintf(ventabuffer,"Circling, ignore LDrotary\r\n");
-		if ((fp=fopen("DEBUG.TXT","a"))!= NULL)
-			    {;fprintf(fp,"%s\n",ventabuffer);fclose(fp);}
-#endif
+		Log("Circling, ignore LDrotary\r\n");
 		return;
 	}
 
 	if (distance<3 || distance>150) { // just ignore, no need to reset rotary
-                #ifdef TESTBENCH
-                if (distance==0 && zeroerrs) {
-                   StartupStore(_T("... InsertLDRotary distance error=%f @%s%s"),distance,WhatTimeIsIt(),NEWLINE);
-                   zeroerrs=false;
-                }
-                #endif
-		if (errs==9) {
-#ifdef DEBUG_ROTARY
-			sprintf(ventabuffer,"Rotary reset after exceeding errors\r\n");
-			if ((fp=fopen("DEBUG.TXT","a"))!= NULL)
-				    {;fprintf(fp,"%s\n",ventabuffer);fclose(fp);}
+#ifdef TESTBENCH
+		if (distance==0 && zeroerrs) {
+			StartupStore(_T("... InsertLDRotary distance error=%f @%s%s"),distance,WhatTimeIsIt(),NEWLINE);
+			zeroerrs=false;
+		}
 #endif
+		if (errs==9) {
+			Log("Rotary reset after exceeding errors\r\n");
 			TestLog(_T("... LDROTARY RESET, distance errors"));
 			InitLDRotary(&rotaryLD);
 			errs=10; // an no more here until errs reset with valid data
@@ -141,11 +132,8 @@ FILE *fp;
 
 		}
 		if (errs<9) errs++;  // make it up to 9
-#ifdef DEBUG_ROTARY
-		sprintf(ventabuffer,"(errs=%d) IGNORE INVALID distance=%d altitude=%d\r\n",errs,(int)(distance),(int)(Calculated->NavAltitude));
-		if ((fp=fopen("DEBUG.TXT","a"))!= NULL)
-			    {;fprintf(fp,"%s\n",ventabuffer);fclose(fp);}
-#endif
+
+		Log("(errs=%d) IGNORE INVALID distance=%d altitude=%d\r\n",errs,(int)(distance),(int)(Calculated->NavAltitude));
 		return;
 	}
 	errs=0;
@@ -169,11 +157,7 @@ _noautoreset:
     buf->prevaltitude = iround(Calculated->NavAltitude*100);
 
 	if (++buf->start >=buf->size) {
-#ifdef DEBUG_ROTARY
-		sprintf(ventabuffer,"*** rotary reset and VALID=TRUE ++bufstart=%d >=bufsize=%d\r\n",buf->start, buf->size);
-		if ((fp=fopen("DEBUG.TXT","a"))!= NULL)
-			    {;fprintf(fp,"%s\n",ventabuffer);fclose(fp);}
-#endif
+		Log("*** rotary reset and VALID=TRUE ++bufstart=%d >=bufsize=%d\r\n", buf->start, buf->size);
 		buf->start=0;
 		buf->valid=true; // flag for a full usable buffer
 	}
@@ -209,11 +193,8 @@ _noautoreset:
 			buf->ias[buf->start] = (int)(Calculated->IndicatedAirspeedEstimated*100);
 		}
 	}
-#ifdef DEBUG_ROTARY
-	sprintf(ventabuffer,"insert buf[%d/%d], distance=%d totdist=%d\r\n",buf->start, buf->size-1, buf->distance[buf->start], buf->totaldistance);
-	if ((fp=fopen("DEBUG.TXT","a"))!= NULL)
-		    {;fprintf(fp,"%s\n",ventabuffer);fclose(fp);}
-#endif
+	Log("insert buf[%d/%d], distance=%d totdist=%d\r\n",
+			buf->start, buf->size-1, buf->distance[buf->start], buf->totaldistance);
 }
 
 //#define DEBUG_EQMC 1
@@ -224,20 +205,11 @@ _noautoreset:
 double CalculateLDRotary(ldrotary_s *buf, NMEA_INFO *Basic, DERIVED_INFO *Calculated ) {
 
 	double eff;
-#ifdef DEBUG_ROTARY
-	short bcold;
-	char ventabuffer[200];
-	FILE *fp;
-#endif
 	double averias;
 	double avertas;
 
 	if ( Calculated->Circling || Calculated->OnGround || !Calculated->Flying ) {
-#ifdef DEBUG_ROTARY
-		sprintf(ventabuffer,"Not Calculating, on ground or circling, or not flying\r\n");
-		if ((fp=fopen("DEBUG.TXT","a"))!= NULL)
-			    {;fprintf(fp,"%s\n",ventabuffer);fclose(fp);}
-#endif
+		Log("Not Calculating, on ground or circling, or not flying\r\n");
 		#if DEBUG_EQMC
 		StartupStore(_T("... Circling, grounded or not flying, EqMc -1 (---)\n"));
 		#endif
@@ -246,11 +218,7 @@ double CalculateLDRotary(ldrotary_s *buf, NMEA_INFO *Basic, DERIVED_INFO *Calcul
 	}
 
 	if ( buf->start <0) {
-#ifdef DEBUG_ROTARY
-		sprintf(ventabuffer,"Calculate: invalid buf start<0\r\n");
-		if ((fp=fopen("DEBUG.TXT","a"))!= NULL)
-			    {;fprintf(fp,"%s\n",ventabuffer);fclose(fp);}
-#endif
+		Log("Calculate: invalid buf start<0\r\n");
 		#if DEBUG_EQMC
 		StartupStore(_T("... Invalid buf start <0, EqMc -1 (---)\n"));
 		#endif
@@ -326,120 +294,13 @@ double CalculateLDRotary(ldrotary_s *buf, NMEA_INFO *Basic, DERIVED_INFO *Calcul
 	}
 	eff= ((double)bc.totaldistance) / ((double)bc.totalaltitude);
 
-#ifdef DEBUG_ROTARY
-	if (bc.valid && bc.start < (bc.size-1))
-		bcold=bc.start+1;
-	else
-		bcold=0;
-
-	sprintf(ventabuffer,"bcstart=%d bcold=%d altnew=%d altold=%d altdiff=%d totaldistance=%d eff=%f\r\n",
-		bc.start, bcold,
-		bc.altitude[bc.start], bc.altitude[bcold], bc.totalaltitude, bc.totaldistance, eff);
-	if ((fp=fopen("DEBUG.TXT","a"))!= NULL)
-                    {;fprintf(fp,"%s\n",ventabuffer);fclose(fp);}
-#endif
+	int bcold = ((bc.valid && bc.start < (bc.size-1)) ?  bc.start + 1 : 0);
+	Log("bcstart=%d bcold=%d altnew=%d altold=%d altdiff=%d totaldistance=%d eff=%f\r\n",
+			bc.start, bcold, bc.altitude[bc.start], bc.altitude[bcold], 
+			bc.totalaltitude, bc.totaldistance, eff);
 
 	if (eff>MAXEFFICIENCYSHOW) eff=INVALID_GR;
 
 	return(eff);
 
 }
-
-#if 0
-// Example code used for rotary buffering
-bool InitFilterBuffer(ifilter_s *buf, short bsize) {
-short i;
-	if (bsize <3 || bsize>RASIZE) return false;
-	for (i=0; i<RASIZE; i++) buf->array[i]=0;
-	buf->start=-1;
-	buf->size=bsize;
-	return true;
-}
-
-void InsertRotaryBuffer(ifilter_s *buf, int value) {
-	if (++buf->start >=buf->size) {
-		buf->start=0;
-	}
-	buf->array[buf->start]=value;
-}
-
-int FilterFast(ifilter_s *buf, int minvalue, int maxvalue) {
-
-  ifilter_s bc;
-  memcpy(&bc, buf, sizeof(ifilter_s));
-
-  short i,nc,iter;
-  int s, *val;
-  float aver=0.0, oldaver, low=minvalue, high=maxvalue, cutoff;
-
-  for (iter=0; iter<MAXITERFILTER; iter++) {
-	 for (i=0, nc=0, s=0; i<bc.size; i++) {
-		val=&bc.array[i];
-		if (*val >=low && *val <=high) { s+=*val; nc++; }
-	  }
-	  if (nc==0) { aver=0.0; break; }
-	  oldaver=aver; aver=((float)s/nc);
-	  //printf("Sum=%d count=%d Aver=%0.3f (old=%0.3f)\n",s,nc,aver,oldaver);
-	  if (oldaver==aver) break;
-	  cutoff=aver/50; // 2%
-	  low=aver-cutoff;
-	  high=aver+cutoff;
-  }
-  //printf("Found: aver=%d (%0.3f) after %d iterations\n",(int)aver, aver, iter);
-  return ((int)aver);
-
-}
-
-int FilterRotary(ifilter_s *buf, int minvalue, int maxvalue) {
-
-  ifilter_s bc;
-  memcpy(&bc, buf, sizeof(ifilter_s));
-
-  short i,curs,nc,iter;
-  int s, val;
-  float aver, low, high, cutoff;
-
-  low  = minvalue;
-  high = maxvalue;
-
-  for (iter=0; iter<MAXITERFILTER; iter++) {
-	 for (i=0, nc=0, s=0,curs=bc.start; i<bc.size; i++) {
-
-		val=bc.array[curs];
-		if (val >=low && val <=high) {
-			s+=val;
-			nc++;
-		}
-		if (++curs >= bc.size ) curs=0;
-	  }
-	  if (nc==0) {
-		aver=0.0;
-		break;
-	  }
-	  aver=((float)s/nc);
-	  //printf("Sum=%d count=%d Aver=%0.3f\n",s,nc,aver);
-
-	  cutoff=aver/50; // 2%
-	  low=aver-cutoff;
-	  high=aver+cutoff;
-  }
-
-  //printf("final: aver=%d\n",(int)aver);
-  return ((int)aver);
-
-}
-#endif
-/*
-main(int argc, char *argv[])
-{
-  short i;
-  ifilter_s buf;
-  InitFilterBuffer(&buf,20);
-  int values[20] = { 140,121,134,119,116,118,121,122,120,124,119,117,116,130,122,119,110,118,120,121 };
-  for (i=0; i<20; i++) InsertRotaryBuffer(&buf, values[i]);
-  FilterFast(&buf, 70,200 );
-  buf.start=10;
-  for (i=0; i<20; i++) InsertRotaryBuffer(&buf, values[i]);
-  FilterFast(&buf, 70,200 );
-}
-*/
