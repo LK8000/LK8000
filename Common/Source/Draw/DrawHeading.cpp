89d485d88f768f194791f9164aaae5b2fe687d48
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2025-04-18 00:13:53 +0200

    DrawHeading : remove invalid cast

--- Common/Source/Draw/DrawHeading.cpp
+++ Common/Source/Draw/DrawHeading.cpp
@@ -17,19 +17,27 @@ void MapWindow::DrawHeading(LKSurface& Surface, const POINT& Orig, const RECT& r
     if(DrawInfo.NAVWarning) return; // 100214
     if(mode.Is(MapWindow::Mode::MODE_CIRCLING)) return;
 
-    // Reduce the rectangle for a better effect
-    const PixelRect ClipRect = { rc.left+NIBLSCALE(5), rc.top+NIBLSCALE(5), rc.right-NIBLSCALE(5), rc.bottom-NIBLSCALE(5) };
+    using PointScalarT = decltype(Orig.x);
+    const auto margin = NIBLSCALE<PointScalarT>(5);
 
-    int tmp = isqrt4((ClipRect.GetSize().cx*ClipRect.GetSize().cx) + (ClipRect.GetSize().cy*ClipRect.GetSize().cy));
+    // Reduce the rectangle for a better effect
+    const PixelRect ClipRect = {
+        rc.left + margin,
+        rc.top + margin,
+        rc.right - margin,
+        rc.bottom - margin
+    };
+
+    // max length of the line (diagonal of the Screen minus the borders)
+    PointScalarT tmp = std::sqrt((ClipRect.GetSize().cx * ClipRect.GetSize().cx) + (ClipRect.GetSize().cy * ClipRect.GetSize().cy));
     POINT p2;
     if (  MapWindow::mode.autoNorthUP() ||   !( DisplayOrientation == TRACKUP || DisplayOrientation == NORTHCIRCLE || DisplayOrientation == TARGETCIRCLE || DisplayOrientation == TARGETUP)) {   // NorthUP
-        p2.y= Orig.y - (int)(tmp*fastcosine(DrawInfo.TrackBearing));
-        p2.x= Orig.x + (int)(tmp*fastsine(DrawInfo.TrackBearing));
+      p2.y = Orig.y - tmp * fastcosine(DrawInfo.TrackBearing);
+      p2.x = Orig.x + tmp * fastsine(DrawInfo.TrackBearing);
     } else {  // TrackUP
-        p2.x=Orig.x;
-        p2.y=Orig.y - (int)tmp;
+      p2.x = Orig.x;
+      p2.y = Orig.y - tmp;
     }
-
 
     Surface.DrawLine(PEN_SOLID, NIBLSCALE(1), Orig, p2, BlackScreen ? RGB_INVDRAW : RGB_BLACK, ClipRect); // 091109
 }
