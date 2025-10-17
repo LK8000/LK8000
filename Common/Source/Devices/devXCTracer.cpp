43278c96c0940bc45e302d818188e37e16e528bc
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2025-10-17 22:43:12 +0200

    XCTracer Driver : ignore GPS data when device is not the active gps

--- Common/Source/Devices/devXCTracer.cpp
+++ Common/Source/Devices/devXCTracer.cpp
@@ -19,7 +19,9 @@
 #include "nmeaistream.h"
 #include "Comm/ExternalWind.h"
 #include "devLK8EX1.h"
+#include "devGeneric.h"
 
+namespace {
 /**
  * Helper functions to parse and check an input field
  * Should these be added as methods to Class CSVLine ?
@@ -31,8 +33,7 @@
  * @param max Maximum value to be accepted
  * @return true if parsed OK and within range
  */
-static bool
-ReadCheckedRange(const char *String, unsigned &value_r, unsigned min, unsigned max) {
+bool ReadCheckedRange(const char *String, unsigned &value_r, unsigned min, unsigned max) {
     if (!String || strlen(String) == 0) {
       return false; // empty or empty string
     }
@@ -50,8 +51,7 @@ ReadCheckedRange(const char *String, unsigned &value_r, unsigned min, unsigned m
 }
 
 /* same helper as above with params of type double */
-static bool
-ReadCheckedRange(const char *String, double &value_r, double min, double max) {
+bool ReadCheckedRange(const char *String, double &value_r, double min, double max) {
     if (!String || strlen(String) == 0) {
       return false; // empty or empty string
     }
@@ -66,8 +66,7 @@ ReadCheckedRange(const char *String, double &value_r, double min, double max) {
     return true;
 }
 
-static bool
-ReadChecked(const char *String, double &value_r) {
+bool ReadChecked(const char *String, double &value_r) {
     if (!String || strlen(String) == 0) {
         return false; // empty or empty string
     }
@@ -83,7 +82,7 @@ ReadChecked(const char *String, double &value_r) {
  * $XCTRC,year,month,day,hour,minute,second,centisecond,latitude,longitude,altitude,speedoverground,
  *      course,climbrate,res,res,res,rawpressure,batteryindication*checksum
  */
-static BOOL XTRC(DeviceDescriptor_t* d, char **params, size_t nparams, NMEA_INFO *pGPS) {
+BOOL XTRC(DeviceDescriptor_t* d, char **params, size_t nparams, NMEA_INFO *pGPS) {
 
     if (nparams < 19) {
         return FALSE;
@@ -109,30 +108,33 @@ static BOOL XTRC(DeviceDescriptor_t* d, char **params, size_t nparams, NMEA_INFO
 
     if (valid_fields == 3 + 4 + 2) {
 
-        pGPS->Year = year;
-        pGPS->Month = month;
-        pGPS->Day = day;
+        d->nmeaParser.gpsValid = ((latitude != 0.0) || (longitude != 0.0));
 
-        pGPS->Hour = hour;
-        pGPS->Minute = minute;
-        pGPS->Second = second;
+        if (d->nmeaParser.activeGPS) {
+            pGPS->Year = year;
+            pGPS->Month = month;
+            pGPS->Day = day;
 
+            pGPS->Hour = hour;
+            pGPS->Minute = minute;
+            pGPS->Second = second;
 
-        static int startday = -1;
-        pGPS->Time = TimeModify(pGPS, startday) + centisecond / 100.0;
-        // TODO : check if TimeHasAdvanced check is needed (cf. Parser.cpp) 
+            static int startday = -1;
+            pGPS->Time = TimeModify(pGPS, startday) + centisecond / 100.0;
+            // TODO : check if TimeHasAdvanced check is needed (cf. Parser.cpp)
 
-        pGPS->Latitude = latitude;
-        pGPS->Longitude = longitude;
+            pGPS->Latitude = latitude;
+            pGPS->Longitude = longitude;
 
-        pGPS->NAVWarning = ( (latitude == 0.0) && (longitude == 0.0) );
-        pGPS->SatellitesUsed = -1;
+            pGPS->NAVWarning = !d->nmeaParser.gpsValid;
+            pGPS->SatellitesUsed = -1;
 
-        pGPS->Altitude = StrToDouble(params[10], nullptr); // altitude
+            pGPS->Altitude = StrToDouble(params[10], nullptr);  // altitude
 
-        pGPS->Speed = StrToDouble(params[11], nullptr); // speedoverground
-        if (pGPS->Speed > 1.0) {
-            pGPS->TrackBearing = AngleLimit360(StrToDouble(params[12], nullptr)); // course
+            pGPS->Speed = StrToDouble(params[11], nullptr);  // speedoverground
+            if (pGPS->Speed > 1.0) {
+                pGPS->TrackBearing = AngleLimit360(StrToDouble(params[12], nullptr));  // course
+            }
         }
 
         double Vario = StrToDouble(params[13], nullptr); // climbrate
@@ -141,27 +143,24 @@ static BOOL XTRC(DeviceDescriptor_t* d, char **params, size_t nparams, NMEA_INFO
         const double abs_press = StrToDouble(params[17], nullptr); // rawpressure
         UpdateBaroSource(pGPS, d, StaticPressureToQNHAltitude(abs_press * 100));
 
-
-        pGPS->ExtBatt1_Voltage = strtol(params[18], nullptr, 10) + 1000; // batteryindication
-
-    } else {
-        
-        pGPS->NAVWarning = true;
-        
+        // battery indication
+        if (d->OnBatteryLevel) {
+          d->OnBatteryLevel(*d, *pGPS, strtol(params[18], nullptr, 10));
+        }
     }
 
-    d->nmeaParser.gpsValid = !pGPS->NAVWarning;
     d->nmeaParser.connected = true;
     if (d->nmeaParser.gpsValid) {
         d->nmeaParser.lastGpsValid.Update();
-        TriggerGPSUpdate();
+        if (d->nmeaParser.activeGPS) {
+            TriggerGPSUpdate();
+        }
     }
 
     return TRUE;
 }
 
-bool LXWP0(DeviceDescriptor_t* d, char **params, size_t nparams, NMEA_INFO *pGPS)
-{
+bool LXWP0(DeviceDescriptor_t* d, char **params, size_t nparams, NMEA_INFO *pGPS) {
     // $LXWP0,logger_stored, airspeed, airaltitude,
     //   v1[0],v1[1],v1[2],v1[3],v1[4],v1[5], hdg, windspeed*CS<CR><LF>
     //
@@ -181,27 +180,19 @@ bool LXWP0(DeviceDescriptor_t* d, char **params, size_t nparams, NMEA_INFO *pGPS
     }
 
     double QneAltitude = 0;
-    double TrueAirSpeed = 0;
-    bool AirspeedAvailable = false;
-
-    if (ReadChecked(params[2], TrueAirSpeed))
-    {
-        TrueAirSpeed = Units::From(unKiloMeterPerHour, TrueAirSpeed);
-        AirspeedAvailable = TRUE;
-        pGPS->TrueAirspeed = TrueAirSpeed;
-        pGPS->AirspeedAvailable = AirspeedAvailable;
-    }
-
     if (ReadChecked(params[3], QneAltitude)) {
-        UpdateBaroSource(pGPS, d, QNEAltitudeToQNHAltitude(QneAltitude));
-    } else {
-        if (AirspeedAvailable) {
-            QneAltitude = QNHAltitudeToQNEAltitude(pGPS->Altitude);
-        }
+      UpdateBaroSource(pGPS, d, QNEAltitudeToQNHAltitude(QneAltitude));
+    }
+    else {
+      QneAltitude = QNHAltitudeToQNEAltitude(pGPS->Altitude);
     }
 
-    if (AirspeedAvailable) {
+    double TrueAirSpeed = 0;
+    if (ReadChecked(params[2], TrueAirSpeed)) {
+        TrueAirSpeed = Units::From(unKiloMeterPerHour, TrueAirSpeed);
+        pGPS->TrueAirspeed = TrueAirSpeed;
         pGPS->IndicatedAirspeed = IndicatedAirSpeed(TrueAirSpeed, QneAltitude);
+        pGPS->AirspeedAvailable = TRUE;
     }
 
     for(int i = 4; i <= 9; ++i) {
@@ -219,28 +210,33 @@ bool LXWP0(DeviceDescriptor_t* d, char **params, size_t nparams, NMEA_INFO *pGPS
         UpdateExternalWind(*pGPS, *d, Units::From(unKiloMeterPerHour, wind_speed), wind_dir + 180);
     }
 
-    return(true);
+    return true;
 } // LXWP0()
 
-static BOOL XCTracerParseNMEA(DeviceDescriptor_t* d, const char *String, NMEA_INFO *_INFO) {
+BOOL XCTracerParseNMEA(DeviceDescriptor_t* d, const char *String, NMEA_INFO *pGPS) {
+    using namespace std::string_view_literals;
+
     char ctemp[MAX_NMEA_LEN];
     char * params[MAX_NMEA_PARAMS];
 
     size_t n_params = NMEAParser::ValidateAndExtract(String, ctemp, params);
-    if (n_params>0) {
-        if(strcmp(params[0], "$XCTRC") == 0) {
-            return XTRC(d, params, n_params, _INFO);
+    if (n_params > 0) {
+        if(params[0] == "$XCTRC"sv) {
+            return XTRC(d, params, n_params, pGPS);
         }
-        if(strcmp(params[0], "$LXWP0") == 0) {
-            return LXWP0(d, params, n_params, _INFO);
+        if(params[0] == "$LXWP0"sv) {
+            return LXWP0(d, params, n_params, pGPS);
         }
-        if(LK8EX1ParseNMEA(d, String, _INFO)) {
+        if(LK8EX1ParseNMEA(d, String, pGPS)) {
           return TRUE;
         }
     }
     return FALSE;
 }
 
+}  // namespace
+
 void XCTracerInstall(DeviceDescriptor_t* d) {
+    genInstall(d);
     d->ParseNMEA = XCTracerParseNMEA;
 }
