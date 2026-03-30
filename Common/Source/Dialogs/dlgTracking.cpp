40ecf6b990739c3357a25b11dc1ae8475c0875f1
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2026-03-30 23:16:21 +0200

    Add licensing and author information to dlgTracking files

--- Common/Source/Dialogs/dlgTracking.cpp
+++ Common/Source/Dialogs/dlgTracking.cpp
@@ -1,3 +1,12 @@
+/*
+ * LK8000 Tactical Flight Computer -  WWW.LK8000.IT
+ * Released under GNU/GPL License v.2 or later
+ * See CREDITS.TXT file for authors and copyrights
+ *
+ * File:   dlgTracking.cpp
+ * Author: Bruno de Lacheisserie
+ *
+ */
 #include <memory>
 #include "dlgTracking.h"
 #include "dlgTools.h"
--- Common/Source/Dialogs/dlgTracking.h
+++ Common/Source/Dialogs/dlgTracking.h
@@ -1,3 +1,12 @@
+/*
+ * LK8000 Tactical Flight Computer -  WWW.LK8000.IT
+ * Released under GNU/GPL License v.2 or later
+ * See CREDITS.TXT file for authors and copyrights
+ *
+ * File:   dlgTracking.h
+ * Author: Bruno de Lacheisserie
+ *
+ */
 #pragma once
 #include "Compiler.h"
 #include "WindowControls.h"
