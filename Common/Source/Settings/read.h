4db713b3d8f95a64dcbf2aa520b5d3420b1f9485
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2026-05-16 23:10:57 +0200

    Refactor read.h to use concepts for type constraints in read_value functions

--- Common/Source/Settings/read.h
+++ Common/Source/Settings/read.h
@@ -9,102 +9,105 @@
 #ifndef _SETTINGS_READ_H_
 #define _SETTINGS_READ_H_
 
+#include <concepts>
 #include <type_traits>
 #include <cstdlib>
 #include <cstring>
 #include <assert.h>
 #include "utils/stringext.h"
 
-
 namespace settings {
-  namespace detail {
+namespace detail {
 
-    /**
-     * any signed numeric type
-     */
-    template<typename T>
-    using signed_number =
-            std::enable_if_t<(std::is_arithmetic_v<T> && std::is_signed_v<T> &&
-                            !std::is_enum_v<T>), T>;
+/**
+ * any signed numeric type
+ */
+template <typename T>
+concept signed_number =
+    std::is_arithmetic_v<T> && std::is_signed_v<T> && !std::is_enum_v<T>;
 
-    /**
-     * any unsigned numeric type ( including enum )
-     */
-    template<typename T>
-    using unsigned_number =
-            std::enable_if_t<(std::is_arithmetic_v<T> && !std::is_signed_v<T>) ||
-                            std::is_enum_v<T>, T>;
+/**
+ * any unsigned numeric type ( including enum )
+ */
+template <typename T>
+concept unsigned_number =
+    (std::is_arithmetic_v<T> && !std::is_signed_v<T>) || std::is_enum_v<T>;
 
-    /**
-     * read 'signed number' type
-     */
-    template<typename T>
-    void read_value(const char *curvalue, signed_number<T> &lookupvalue) {
-      lookupvalue = static_cast<T>(strtol(curvalue, nullptr, 10));
-    }
+template <typename T>
+concept cstring_assignable = std::assignable_from<T&, const char*>;
 
-    /**
-     * read 'unsigned number' type ( including enum )
-     */
-    template<typename T>
-    void read_value(const char *curvalue, unsigned_number<T> &lookupvalue) {
-      lookupvalue = static_cast<T>(strtoul(curvalue, nullptr, 10));
-    }
-
-    /**
-     * read utf8 string
-     */
-    template<size_t size>
-    void read_value(const char *curvalue, char (&lookupvalue)[size]) {
-      assert(strlen(curvalue) < size); // saved pref is longer than variable used to store value.
-      strncpy(lookupvalue, curvalue, size - 1);
-    }
-
-    /**
-     * read unicode string
-     */
-    template <size_t size>
-    void read_value(const char *curvalue, wchar_t (&lookupvalue)[size]) {
-      from_utf8(curvalue, lookupvalue, size);
-    }
-
-    /**
-     * generic : all type having operator=(const char*)  
-     */
-    template<typename T>
-    void read_value(const char *curvalue, T& lookupvalue) {
-        lookupvalue = curvalue;
-    }
-  }
-
-  /**
-   * default read specialisation
-   * @return true if 'curname' is equal to 'lookupname'
-   */
-  template<typename T>
-  bool read(const char *curname, const char *curvalue, const char *lookupname,
-                T &lookupvalue) {
-
-    if (strcmp(curname, lookupname)) {
-      return false;
-    }
-    detail::read_value<T>(curvalue, lookupvalue);
-    return true;
-  }
-
-  /**
-   * read specialisation for 'T[size]' pref (aka TCHAR[size]
-   * @return true if 'curname' is equal to 'lookupname'
-   */
-  template<typename T, size_t size>
-  bool read(const char *curname, const char *curvalue, const char *lookupname,
-                T (&lookupvalue)[size]) {
-
-    if (strcmp(curname, lookupname)) {
-      return false;
-    }
-    detail::read_value<size>(curvalue, lookupvalue);
-    return true;
-  }
+/**
+ * read 'signed number' type
+ */
+template <signed_number T>
+void read_value(const char* curvalue, T& lookupvalue) {
+  lookupvalue = static_cast<T>(std::strtol(curvalue, nullptr, 10));
 }
+
+/**
+ * read 'unsigned number' type ( including enum )
+ */
+template <unsigned_number T>
+void read_value(const char* curvalue, T& lookupvalue) {
+  lookupvalue = static_cast<T>(std::strtoul(curvalue, nullptr, 10));
+}
+
+/**
+ * read utf8 string
+ */
+template <size_t size>
+void read_value(const char* curvalue, char (&lookupvalue)[size]) {
+  assert(strlen(curvalue) <
+         size);  // saved pref is longer than variable used to store value.
+  strncpy(lookupvalue, curvalue, size - 1);
+  lookupvalue[size - 1] = '\0';
+}
+
+/**
+ * read unicode string
+ */
+template <size_t size>
+void read_value(const char* curvalue, wchar_t (&lookupvalue)[size]) {
+  from_utf8(curvalue, lookupvalue, size);
+}
+
+/**
+ * generic : all type having operator=(const char*)
+ */
+template <cstring_assignable T>
+void read_value(const char* curvalue, T& lookupvalue) {
+  lookupvalue = curvalue;
+}
+
+}  // namespace detail
+
+/**
+ * default read specialisation
+ * @return true if 'curname' is equal to 'lookupname'
+ */
+template <typename T>
+bool read(const char* curname, const char* curvalue, const char* lookupname,
+          T& lookupvalue) {
+  if (strcmp(curname, lookupname)) {
+    return false;
+  }
+  detail::read_value<T>(curvalue, lookupvalue);
+  return true;
+}
+
+/**
+ * read specialisation for 'T[size]' pref (aka TCHAR[size]
+ * @return true if 'curname' is equal to 'lookupname'
+ */
+template <typename T, size_t size>
+bool read(const char* curname, const char* curvalue, const char* lookupname,
+          T (&lookupvalue)[size]) {
+  if (strcmp(curname, lookupname)) {
+    return false;
+  }
+  detail::read_value<size>(curvalue, lookupvalue);
+  return true;
+}
+
+}  // namespace settings
 #endif
