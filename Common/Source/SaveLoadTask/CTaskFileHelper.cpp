8260427f6623d6c2a690c0f7dfc088ee4145b0ef
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2026-06-05 00:12:41 +0200

    Fix TimeGates loading from lkt file #1703

--- Common/Source/SaveLoadTask/CTaskFileHelper.cpp
+++ Common/Source/SaveLoadTask/CTaskFileHelper.cpp
@@ -341,7 +341,7 @@ void CTaskFileHelper::LoadOptions(const xml_node* node) {
                 }
             }
             LoadRules(nodeOpt->first_node("rules"));
-            LoadTimeGate(node->first_node("time-gate"));
+            LoadTimeGate(nodeOpt->first_node("time-gate"));
         }
     }
 }
