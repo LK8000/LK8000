a0e3d7bf41ecf351de12976b15bd5f03c7230056
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2026-07-19 21:45:48 +0200

    TeamCodeCalculation.h : fix missing include

--- Common/Header/TeamCodeCalculation.h
+++ Common/Header/TeamCodeCalculation.h
@@ -1,5 +1,7 @@
 #ifndef	TEAMCALCULATION_H
-#define	TEAMCALCULATION_H
+#define TEAMCALCULATION_H
+
+#include "tchar.h"
 
 void GetTeamCode(TCHAR *code, double bearing, double range);
 void CalcTeammateBearingRange(double ownDist, double ownBear, TCHAR *TeamMateCode,  double *distToMate, double *bearToMate);
