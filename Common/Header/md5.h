85cdf25e8a1a0227bb16242999fca4b6f6a5d9da
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2026-09-07 00:03:25 +0200

    Enhance MD5 update methods to support various container types

--- Common/Header/md5.h
+++ Common/Header/md5.h
@@ -9,40 +9,65 @@
 #define MD5_H
 
 #include "md5internal.h"
-#include <string>
+#include <type_traits>
+#include <ranges>
+#include "OS/ByteOrder.hpp"
+
+namespace md5_internal {
+
+template <typename T>
+concept byte_array = std::ranges::contiguous_range<T> &&
+                     sizeof(std::ranges::range_value_t<T>) == 1;
+
+template <typename T>
+concept other_container = std::ranges::range<T> && !byte_array<T>;
+
+}  // namespace md5_internal
 
 class MD5_Base {
-protected:
+ protected:
   md5_ctx context;
 
-public:
-
+ public:
   MD5_Base();
-  MD5_Base(unsigned int key1, unsigned int key2, unsigned int key3, unsigned int key4);
-  
+  MD5_Base(unsigned key1, unsigned key2, unsigned key3, unsigned key4);
+
   void Init();
-  void Init(unsigned int key1, unsigned int key2, unsigned int key3, unsigned int key4);
-  
+  void Init(unsigned key1, unsigned key2, unsigned key3, unsigned key4);
+
   // MD5 block update operation. Continues an MD5 message-digest
   // operation, processing another message block, and updating the
   // context.
   void Update(const void* input, size_t size);
 
-  template<typename T, std::enable_if_t<std::is_arithmetic_v<T>>* = nullptr>
+  // Arithmetic types (char, int, float, etc.)
+  template <typename T>
+    requires std::is_arithmetic_v<T>
   void Update(const T& data) {
+    static_assert(IsLittleEndian(), "Big-Endian Arch is not supported");
     Update(&data, sizeof(data));
   }
 
-  void Update(const std::string& data) {
-    Update(data.data(), data.size());
+  // Contiguous ranges (std::string, std::vector, std::array, std::span, etc.)
+  template <md5_internal::byte_array T>
+  void Update(const T& data) {
+    auto bytes = std::as_bytes(std::span(data));
+    Update(bytes.data(), bytes.size());
+  }
+
+  // Non-contiguous ranges (std::list, std::set, etc.)
+  template <md5_internal::other_container T>
+  void Update(const T& data) {
+    for (const auto& item : data) {
+      Update(item);
+    }
   }
 };
 
-class MD5 : public MD5_Base
-{
-public:
-  MD5() = default;
-  explicit MD5(const MD5_Base& md5) : MD5_Base(md5) { }
+class MD5 : public MD5_Base {
+ public:
+  using MD5_Base::MD5_Base;
+  explicit MD5(const MD5_Base& md5) : MD5_Base(md5) {}
 
   // MD5 finalization. Ends an MD5 message-digest operation, writing the
   // the message digest and zeroizing the context.
@@ -50,4 +75,3 @@ public:
 };
 
 #endif
-
--- Common/Source/utils/md5.cpp
+++ Common/Source/utils/md5.cpp
@@ -9,24 +9,20 @@
 #include "stl_utils.h"
 #include "md5.h"
 
-
-MD5_Base::MD5_Base()
-{
-    Init();
+MD5_Base::MD5_Base() {
+  Init();
 }
 
-MD5_Base::MD5_Base(unsigned int key1, unsigned int key2, unsigned int key3, unsigned int key4)
-{
+MD5_Base::MD5_Base(unsigned key1, unsigned key2, unsigned key3, unsigned key4) {
   Init(key1, key2, key3, key4);
 }
 
-void MD5_Base::Init()
-{
+void MD5_Base::Init() {
   md5_init_ctx(&context);
 }
 
-void MD5_Base::Init(unsigned int key1, unsigned int key2, unsigned int key3, unsigned int key4)
-{
+void MD5_Base::Init(unsigned key1, unsigned key2, unsigned key3,
+                    unsigned key4) {
   md5_init_ctx(&context);
   context.A = key1;
   context.B = key2;
@@ -37,19 +33,19 @@ void MD5_Base::Init(unsigned int key1, unsigned int key2, unsigned int key3, uns
 // MD5 block update operation. Continues an MD5 message-digest
 // operation, processing another message block, and updating the
 // context.
-void MD5_Base::Update(const void* input, size_t size)
-{
+void MD5_Base::Update(const void* input, size_t size) {
   md5_process_bytes(input, size, &context);
 }
 
 // MD5 finalization. Ends an MD5 message-digest operation,
 // zeroizing the context and return the message digest.
-std::string MD5::Final()
-{
+std::string MD5::Final() {
+  // clang-format off
   static constexpr char Digit[] = {
-      '0', '1', '2', '3', '4', '5', '6', '7',
-      '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
-    };
+    '0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+  };
+  // clang-format on
 
   unsigned char resbuf[16];
   md5_finish_ctx(&context, resbuf);
@@ -57,7 +53,7 @@ std::string MD5::Final()
   std::string out;
   out.reserve(32);
   auto out_iterator = std::back_inserter(out);
-  for ( char c : resbuf) {
+  for (char c : resbuf) {
     out_iterator = Digit[(c & 0xF0) >> 4];
     out_iterator = Digit[(c & 0x0F) >> 0];
   }
@@ -66,6 +62,7 @@ std::string MD5::Final()
 
 #ifndef DOCTEST_CONFIG_DISABLE
 #include <doctest/doctest.h>
+#include <span>
 
 TEST_CASE("MD5 hash") {
   MD5_Base hash;
@@ -79,6 +76,21 @@ TEST_CASE("MD5 hash") {
 
   hash.Update('4');
   CHECK_EQ(MD5(hash).Final(), "16d7a4fca7442dda3ad93c9a726597e4");
+
+  hash.Update(std::array<int, 3>({1, 2, 3}));
+  CHECK_EQ(MD5(hash).Final(), "50db5630f2359b1757233ea553d7cbb4");
+
+  {
+    int data[] = {1, 2, 3};
+    hash.Update(data);
+  }
+  CHECK_EQ(MD5(hash).Final(), "8b49aff2f33ffcaa01dd56bcc588e0a8");
+
+  {
+    uint8_t data[] = {1, 2, 3};
+    hash.Update(data);
+  }
+  CHECK_EQ(MD5(hash).Final(), "91373e78c2e9865af64f2245ebc7908d");
 }
 
 #endif
