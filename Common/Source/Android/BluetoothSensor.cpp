16e92ad27f812f1a51d624e4f043ad9619573803
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2026-09-14 00:11:40 +0200

    BluetoothSensor: Add empty data check in `HeartRateMeasurement` method

--- Common/Source/Android/BluetoothSensor.cpp
+++ Common/Source/Android/BluetoothSensor.cpp
@@ -292,6 +292,9 @@ void BluetoothSensor::BatteryLevel(const std::vector<uint8_t>& data) {
 }
 
 void BluetoothSensor::HeartRateMeasurement(const std::vector<uint8_t>& data) {
+  if (data.empty()) {
+    return;
+  }
   auto bpm = [&]() -> uint32_t {
     if (data[0] & 0x01) {
       return characteristic_value<uint16_t>(data).get(1);
